8b69a3dadd7b12e81cb4a5a651126b755158d5c8
rick carlino <rick.carlino@gmail.com>
2014-04-10 06:27:53 -0700

    Finished devise work, need landing page with user actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,9 @@ class User
   # field :unconfirmed_email,    type: String # Only if using reconfirmable
 
   ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+  # field :failed_attempts, type: Integer, default: 0 
+  # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    type: String
+  # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
 end
