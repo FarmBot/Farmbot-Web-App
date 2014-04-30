6c5ac4290f6ebe0ec888877be4b6fd7d75d076b1
rick carlino <rick.carlino@gmail.com>
2014-04-30 07:10:28 -0700

    Opps. Assets again.

--- spec/features/user_management/registration_spec.rb
+++ spec/features/user_management/registration_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe 'User Registration' do
   it 'creates a new user account' do
-    visit '/'
+    visit root_path
+    fill_in 'user_name', with: 'ricky_ricardo'
+    fill_in 'user_password', with: 'password123'
+    fill_in 'user_password_confirmation', with: 'password123'
+    click 'Next Step'
     expect(page).to have_content 'Farmbot'
   end
 end
