fec9c7aa0e5333a6f5a648280c510420275bdcfe
rick carlino <rick.carlino@gmail.com>
2014-05-07 06:57:54 -0700

    Started adding factoryGirl, specs. Fixed up some layouts and views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,5 @@ Dss::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
 end
---
+++ spec/features/user_management/session_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'User Session' do
+  xit 'logs the user in' do
+    visit new_user_session_path
+  end
+end
