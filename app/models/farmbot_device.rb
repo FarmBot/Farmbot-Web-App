e4dc8c8d6007dad96b5af6c596f40b45604ff393
rick carlino <rick.carlino@gmail.com>
2014-05-10 20:00:13 -0700

    Minor Code Pruning

--- app/models/farmbot_device.rb
+++ app/models/farmbot_device.rb
@@ -1,3 +1,5 @@
+# FarmbotDevice models all data related to an actual FarmBot in the real world.
+# This is useful for caching things like SkyNey IDs, owner users, work logs, etc
 class FarmbotDevice
   include Mongoid::Document
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,43 +3,25 @@ class User
 
   has_many :farmbot_devices, dependent: :destroy
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  field :name, type: String
+  validates_uniqueness_of :name
+  validates_presence_of :name
 
-  ## Database authenticatable
-  field :email,              type: String, default: ""
+  field :email, type: String, default: ""
+  validates_uniqueness_of :email
+  validates_presence_of :email
+
+  # BEGIN DEVISE CRAP ==========================================================
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :trackable, :validatable #, :omniauthable
   field :encrypted_password, type: String, default: ""
-
-  ## Recoverable
   field :reset_password_token,   type: String
   field :reset_password_sent_at, type: Time
-
-  ## Rememberable
   field :remember_created_at, type: Time
-
-  ## Trackable
   field :sign_in_count,      type: Integer, default: 0
   field :current_sign_in_at, type: Time
   field :last_sign_in_at,    type: Time
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
-
-  # The username.
-  field :name, type: String
-  validates_uniqueness_of :name
-  validates_presence_of :name
+  # END DEVISE CRAP ============================================================
 end
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 
-  # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String
-  # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
