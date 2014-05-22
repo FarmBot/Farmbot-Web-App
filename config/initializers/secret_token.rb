fc4eb7063596516bb8a54d4912d8fdb57e650b8d
rick carlino <rick.carlino@gmail.com>
2014-05-22 06:40:14 -0700

    Updating mongoid.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,13 +9,12 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Dss::Application.config.secret_key_base = `rake secret`
-# if Rails.env == 'production'
-#   Dss::Application.config.secret_key_base = ENV['secret_key_base']
-# # else
-#   # Going to keep a hard coded one here to make life easy for folks who want to
-#   # run it in development / submit PRs. Less config. RC.
-#   # Dss::Application.config.secret_key_base = '452b4491c1cdd7315a0d787be2f7668ea'\
-#   # '8307a64733488151a43b62bdd76a6eb819cf973a9261b1500ef5745faf54171c7015f5ab7fd'\
-#   # '43449a5e06e6c1215e4e'
-# end
+if Rails.env == 'production'
+  Dss::Application.config.secret_key_base = ENV['secret_key_base']
+else
+  # Going to keep a hard coded one here to make life easy for folks who want to
+  # run it in development / submit PRs. Less config. RC.
+  Dss::Application.config.secret_key_base = '452b4491c1cdd7315a0d787be2f7668ea'\
+  '8307a64733488151a43b62bdd76a6eb819cf973a9261b1500ef5745faf54171c7015f5ab7fd'\
+  '43449a5e06e6c1215e4e'
+end
