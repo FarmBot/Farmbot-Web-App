ddea8d4d4aa1d098efbf7df03d4765cc8aa79408
rick carlino <rick.carlino@gmail.com>
2014-06-19 06:34:00 -0700

    Dokku troubleshooting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    # 'resource' parameter for if we end up adding more
-    # devise resources (eg: admin users, etc.)
     dashboard_path
   end
 end
