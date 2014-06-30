88cf08a6d2b1c26415991b1b9a51975990a6b8db
Rick Carlino <rick.carlino@gmail.com>
2014-06-30 06:59:41 -0700

    Syntax correction for devise token

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,7 @@ Devise.setup do |config|
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
 
-  config.secret_key = env['DEVISE_SECRET']
+  config.secret_key = ENV['DEVISE_SECRET']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
