d5d22983efa20044845b4bb66a25b2bab5895191
Rick Carlino <rick.carlino@gmail.com>
2014-12-29 07:38:33 -0600

    Added ng-sortable via rails-assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require skynet
 //= require angular/angular
 //= require angular/angular-route
+//= require ng-sortable
 //= require ng-rails-csrf
 //= require farmbot_app/farmbot
 //= require_tree ./farmbot_app
