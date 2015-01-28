5760890497f77b01fe898f98863a57b36b3b8ac1
Rick Carlino <rick.carlino@gmail.com>
2015-01-28 15:33:31 -0600

    Even more specs to verify backend sorting code

--- spec/mutations/steps/update_spec.rb
+++ spec/mutations/steps/update_spec.rb
@@ -6,6 +6,7 @@ describe Steps::Update do
   let(:sequence) { FactoryGirl.create(:sequence, user: user) }
 
   it 'automatically populates position field via black magic.' do
+    # This feature is a crime against humanity. Sorry :(
     pink   = sequence.steps.first
     red    = Step.create(sequence: sequence, message_type: 'pin_write')
     orange = Step.create(sequence: sequence, message_type: 'pin_write')
@@ -14,6 +15,7 @@ describe Steps::Update do
     expectation = [pink, red, orange, green, blue]
     expect(sequence.steps.sort).to eq(expectation)
 
+    # Move last ==> first
     Steps::Update.run!(step_params: {position: 1}, step: blue)
     expect(blue.reload.position).to eq(1)
     expect(blue.next_steps).to_not include(blue)
@@ -22,10 +24,27 @@ describe Steps::Update do
     expect(blue.next_steps).to include(red)
     expect(blue.next_steps).to include(orange)
     expect(blue.next_steps).to include(green)
-    expectation = [blue, pink, red, orange, green,]
-                    .map(&:reload)
-                    .map(&:position)
-                    .sort
-    expect(sequence.steps.pluck(:position).sort).to eq(expectation)
+
+    expect(sequence.steps.find_by(position: 2)).to eq(pink)
+    expect(sequence.steps.find_by(position: 3)).to eq(red)
+    expect(sequence.steps.find_by(position: 4)).to eq(orange)
+    expect(sequence.steps.find_by(position: 5)).to eq(green)
+    # Current order is: blue, pink, red, orange, green
+
+    # Move orange ==> pink
+    # Expected order: blue, orange, pink, red, green
+    Steps::Update.run!(step_params: {position: 2}, step: orange)
+    expect(orange.next_steps).to_not include(blue)
+    expect(orange.next_steps).to_not include(orange)
+    expect(orange.next_steps.count).to eq(3)
+    expect(orange.next_steps).to include(pink)
+    expect(orange.next_steps).to include(red)
+    expect(orange.next_steps).to include(green)
+
+    expect(sequence.steps.find_by(position: 1)).to eq(blue)
+    expect(sequence.steps.find_by(position: 2)).to eq(orange)
+    expect(sequence.steps.find_by(position: 3)).to eq(pink)
+    expect(sequence.steps.find_by(position: 4)).to eq(red)
+    expect(sequence.steps.find_by(position: 5)).to eq(green)
   end
 end
