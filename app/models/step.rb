38b383b5c13f6e1d427c7a5f4c9bf27891f0034b
Rick Carlino <rick.carlino@gmail.com>
2015-01-29 11:19:07 -0600

    tweaks for PR

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,6 +1,5 @@
 class Step
   include Mongoid::Document
-  include Mongoid::Timestamps
 
   MESSAGE_TYPES = %w(single_command read_status pin_write move_abs move_rel)
 
@@ -11,7 +10,11 @@ class Step
 
   field :command, type: Hash, default: {}
   field :position, type: Integer, default: -> do
-    (self.try(:sequence).try(:steps).try(:size) || 1) - 1 || 0
+    if sequence && sequence.steps.present?
+      sequence.steps.size - 1
+    else
+      0
+    end
   end
   validates :position, presence: true
   # TODO : Move this into a refinement =========================================
@@ -26,16 +29,6 @@ class Step
     new_steps.insert(position, self)
     new_steps.each_with_index do |step, index|
       step.position = index
-      step.save!
-    end
-  end
-
-  def untangle!
-    passed_self = false
-    all_steps.each_with_index do |step, i|
-      passed_self = true && next if step == self
-      step.position = i
-      step.position += 1 if passed_self
       step.save!
     end
   end
--- app/serializers/step_serializer.rb
+++ app/serializers/step_serializer.rb
@@ -1,5 +1,5 @@
 class StepSerializer < ActiveModel::Serializer
-  attributes :_id, :message_type, :command, :sequence_id, :position, :updated_at
+  attributes :_id, :message_type, :command, :sequence_id, :position
 
   # TODO: This is almost certainly wrong. I shouldn't need to write this method.
   def sequence_id
