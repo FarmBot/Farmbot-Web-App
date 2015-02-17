bcb2bb2caf395969ac5b643967e38727eba0e761
Rick Carlino <rick.carlino@gmail.com>
2015-02-17 17:55:41 +0700

    Ability to see next schedule occurence

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,11 +1,24 @@
 # Responsible for timing and execution of sequences.
 class Schedule
   include Mongoid::Document
+  UNITS_OF_TIME = %w(minutely hourly daily weekly monthly yearly)
+
   has_one :sequence
-  field :start_date, type: Date
-  field :end_date, type: Date
-  field :every, type: Integer
-  UNITS_OF_TIME = %w(minutes hours days weeks months years)
-  field :unit_of_time
-  validates_inclusion_of :unit_of_time, in: UNITS_OF_TIME
+
+  field :start_time, type: Time
+  field :end_time, type: Time
+  field :next_time, type: Time
+  field :repeat, type: Integer
+  field :time_unit
+  validates_inclusion_of :time_unit, in: UNITS_OF_TIME
+
+  def schedule
+    @schedule ||= IceCube::Schedule.new(start_time, end_time: end_time) do |sch|
+      sch.add_recurrence_rule IceCube::Rule.send(time_unit.to_sym, repeat)
+    end
+  end
+
+  def calculate_next_occurence
+    schedule.next_occurrence
+  end
 end
--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :schedule do
-    
+    start_time { Date.yesterday.to_time + 1.minute }
+    end_time { Date.today + 1.minute + 2.days }
+    time_unit "daily"
+    repeat 1
   end
-
 end
