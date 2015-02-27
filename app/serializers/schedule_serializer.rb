8cbfd6394425167298f277db2c7b7df5d28b60b8
Rick Carlino <rick.carlino@gmail.com>
2015-02-27 08:27:22 +0700

    Better calendar (does multiple events)

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -1,7 +1,7 @@
 class ScheduleSerializer < ActiveModel::Serializer
   attributes :_id, :start_time, :end_time, :next_time, :repeat, :time_unit,
-             :sequence_id, :sequence_name
-
+             :sequence_id, :sequence_name, :calendar
+  try :url, :sequence
   # TODO: This is almost certainly wrong. I shouldn't need to write this method.
   def sequence_id
     object.sequence._id.to_s
@@ -9,5 +9,15 @@ class ScheduleSerializer < ActiveModel::Serializer
 
   def sequence_name
     object.sequence.name
+  end
+
+  def calendar
+    start  = serialization_options[:start]
+    finish = serialization_options[:finish]
+    if start && finish
+      object.between start, finish
+    else
+      []
+    end
   end
 end
