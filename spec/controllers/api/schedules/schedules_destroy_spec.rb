612ebf6ba0686e987602ad1b1f47388c2065fd68
Rick Carlino <rick.carlino@gmail.com>
2015-03-09 09:37:09 -0500

    97.26% test coverage

---
+++ spec/controllers/api/schedules/schedules_destroy_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Api::SchedulesController do
+  include Devise::TestHelpers
+
+  describe '#destroy' do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    it 'deletes a schedule' do
+      sign_in user
+      schedule = FactoryGirl.create(:schedule, user: user)
+      before = Schedule.count
+      delete :destroy, id: schedule._id.to_s
+
+      expect(response.status).to eq(200)
+      expect(before > Schedule.count).to be_truthy
+    end
+
+    it 'prevents unauthorized deletion' do
+      sign_in user
+      schedule = FactoryGirl.create(:schedule)
+      delete :destroy, id: schedule._id.to_s
+      before = Schedule.count
+
+      expect(response.status).to eq(403)
+      expect(before == Schedule.count).to be_truthy
+      expect(json[:error]).to include("Not your schedule.")
+    end
+  end
+end
