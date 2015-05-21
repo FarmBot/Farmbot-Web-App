0666389cbb4058e483586f342e1252d92606ec28
Rick Carlino <rick.carlino@gmail.com>
2015-05-21 09:30:06 -0500

    Only load SPA on SPA pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,2 @@
 //= require lodash
 //= require jquery
-//= require pickadate
-//= require sio-client
-//= require angular/angular
-//= require angular/angular-route
-//= require ng-sortable
-//= require js-data
-//= require js-data-angular
-//= require ng-rails-csrf
-//= require ng-pickadate
-//= require farmbot
-//= require_tree ./farmbot_app
---
+++ app/assets/javascripts/farmbot.js
@@ -0,0 +1,12 @@
+//= require sio-client
+//= require pickadate
+//= require angular/angular
+//= require angular/angular-route
+//= require ng-sortable
+//= require js-data
+//= require js-data-angular
+//= require ng-pickadate
+//= require ng-rails-csrf
+//= require farmbot_config
+//= require_tree ./farmbot_app
+// This is the main loader for the FarmBot SPA
