737e367984f308374d2764a89c47721b40fdd938
Rick Carlino <rick.carlino@gmail.com>
2015-11-23 13:38:20 -0500

    Add `name` field (with default) to Plant model`

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,10 +1,12 @@
-# a single organism living in the ground that FarmBot watches over.
+# a single organism living in the
+# ground (planting_area) that FarmBot watches over.
 class Plant
   include Mongoid::Document
 
   belongs_to :device
   belongs_to :planting_area
 
+  field :name, default: "Unknown Plant"
   field :x, type: Integer
   field :y, type: Integer
 end
