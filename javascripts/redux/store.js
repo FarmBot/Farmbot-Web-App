3c408a4a4793c2cf4a293f561ec65944e20b63b4
Rick Carlino <rick.carlino@gmail.com>
2015-11-24 13:01:43 -0500

    Removing certain asset management gems for frontend decoupling

--- javascripts/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { reducer } from './reducer';
-import thunk from 'redux-thunk';
-
-var wrappedCreatedStore = applyMiddleware(thunk)(createStore);
-var store = wrappedCreatedStore(reducer, window.initialState);
-
-export { store };
