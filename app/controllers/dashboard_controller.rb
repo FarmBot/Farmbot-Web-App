b897e26939a00e9ed32c23a3ed2cb6ac44b2b577
Rick Carlino <rick.carlino@gmail.com>
2015-12-29 16:41:21 -0600

    [STABLE] Fix login page issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,6 @@
 # A non-resourceful controller for the 'glue' that holds this SPA together.
 # Let's try not to overwhelm this control and write as much API as possible.
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
   def index
     render :index, layout: false
   end
