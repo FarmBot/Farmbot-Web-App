3119830e75e03b51ce2420d58d843f45ac25ebf0
Rick Carlino <rick.carlino@gmail.com>
2016-03-23 12:10:50 -0500

    Change JWT encrytion to RS256

--- lib/session_token.rb
+++ lib/session_token.rb
@@ -1,17 +1,19 @@
 class SessionToken
-  PASSWORD = Rails.application.secrets.secret_key_base
-  EXPIRY   = 4.days
-  ISSUER   = 'farmbot-web-app'
+  EXPIRY       = 4.days
+  ISSUER       = 'farmbot-web-app'
+  PRIVATE_KEY  = KeyGen.current
+  PUBLIC_KEY   = KeyGen.current.public_key
+  ALG          = 'RS256'
 
   attr_accessor :encoded, :unencoded
 
   def initialize(payload)
     @unencoded = payload
-    @encoded   = JWT.encode(payload, PASSWORD)
+    @encoded   = JWT.encode(payload, PRIVATE_KEY, ALG)
   end
 
   def self.decode!(token)
-    self.new JWT.decode(token, PASSWORD)
+    self.new JWT.decode(token, PUBLIC_KEY, true, algorithm: ALG)
   end
 
   def self.issue_to(user, iat = Time.now.to_i, exp = EXPIRY.from_now.to_i)
@@ -20,6 +22,6 @@ class SessionToken
              jti: SecureRandom.uuid, # TODO: Add ability to revoke.
              iss: ISSUER,
              exp: exp,
-             alg: "RS256")
+             alg: ALG)
   end
 end
