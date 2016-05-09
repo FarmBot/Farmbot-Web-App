2d77f9f4f65fe647f6fa47c364136464587eeacd
Rick Carlino <rick.carlino@gmail.com>
2016-05-09 08:52:43 -0500

    Stop using MQTT_URL and start using MQTT_HOST

--- app/mutations/auth/from_jwt.rb
+++ app/mutations/auth/from_jwt.rb
@@ -5,12 +5,20 @@ module Auth
     required { string :jwt }
 
     def execute
-      token  = SessionToken.decode!(jwt)
+      token  = SessionToken.decode!(just_the_token)
       claims = token.unencoded.first
       email  = claims['sub']
       User.find_by(email: email)
     rescue JWT::DecodeError
       add_error :jwt, :decode_error, "JSON Web Token is not valid."
+    end
+
+    def just_the_token
+      # Token auth requires the `authorization` header to be in the format of:
+      # "Authorization: Bearer <INSERT_TOKEN_HERE>"
+      # This helper method strips out the use "Bearer " text from a token, if it
+      # managed to sneak in.
+      jwt.split(" ").last
     end
   end
 end
