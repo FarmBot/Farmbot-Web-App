1eab598421b5fe9532b34498d0589e193e014029
Rick Carlino <rick.carlino@gmail.com>
2016-06-24 14:51:56 -0500

    [STABLE] Add img_url, icon_url, openfarm_slug to backend models

--- app/mutations/plants/create.rb
+++ app/mutations/plants/create.rb
@@ -6,6 +6,14 @@ module Plants
       float :y
     end
 
+    optional do
+      string :name, default: "Unknown Plant"
+      string :img_url, default: "http://placehold.it/200x150"
+      string :icon_url, default: "/icons/Natural Food-96.png"
+      string :openfarm_slug, default: "not-set"
+      integer :planted_at, default: ->{ Time.now.utc }
+    end
+
     def execute
       Plant.create!(inputs)
     end
