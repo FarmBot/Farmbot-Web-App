573ec9e9610044ee6f21119258e65cae85d0caa8
Rick Carlino <rick.carlino@gmail.com>
2016-08-12 17:33:25 -0500

    Fix failing tests (sorry)

--- app/models/regimen.rb
+++ app/models/regimen.rb
@@ -1,12 +1,14 @@
 
 class Regimen
   include Mongoid::Document
+
   field :color, type: String, default: -> { Sequence::COLORS.sample }
   field :name
-  has_many :regimen_items
+
+  has_many   :regimen_items
   belongs_to :device, dependent: :destroy
 
-  validates_uniqueness_of :name, scope: :device_id
-  validates_presence_of :next_time
-  validates_inclusion_of :color, in: Sequence::COLORS
+  validates_presence_of    :name
+  validates_uniqueness_of  :name, scope: :device_id
+  validates_inclusion_of   :color, in: Sequence::COLORS
 end
