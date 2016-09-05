9ef0dc5715e5283fa217c85f9d00f7917951789e
Rick Carlino <rick.carlino@gmail.com>
2016-09-05 10:52:10 -0500

    Convert x/y to float, remove schedule_id from regimentItem, new gitignore entries, seeds.rb, remove mongoid serializer hacks.

--- app/models/regimen_item.rb
+++ app/models/regimen_item.rb
@@ -1,5 +1,4 @@
 class RegimenItem < ActiveRecord::Base
-  belongs_to :schedule
   belongs_to :regimen
   belongs_to :sequence
 end
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# User for testing purposes:
+Users::Create.run!(name:                  "Administrator",
+                   email:                 "admin@admin.com",
+                   password:              "password123",
+                   password_confirmation: "password123")
