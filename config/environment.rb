ef34015e4bf8551fae0ee27800eda05d96998643
Rick Carlino <rick.carlino@gmail.com>
2016-09-14 12:42:47 -0500

    [STABLE] Move config settings to application.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,3 @@
 require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 FarmBot::Application.initialize!
-Rails.application.routes.default_url_options[:host] = ENV["API_HOST"] || "localhost" 
-Rails.application.routes.default_url_options[:port] = ENV["API_PORT"] || 3000 
