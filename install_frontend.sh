a930242fefeed6b8ff16a7977e5fd7bf12423c15
Rick Carlino <rick.carlino@gmail.com>
2016-09-23 14:44:47 -0500

    -g flag for webpack

--- install_frontend.sh
+++ install_frontend.sh
@@ -7,7 +7,7 @@ rm -rf /tmp/farmbot_frontend/.git
 mkdir public/ -p
 cp -R /tmp/farmbot_frontend/* public/ 
 cd public
-npm install webpack -g 2>&1
+npm install -g webpack 2>&1
 npm install i18n-webpack-plugin -g 2>&1
 npm install 2>&1
 # npm run build 2>&1
