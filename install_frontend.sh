37f90fc48d75399d31203f0e4ab3610abd562bb2
Rick Carlino <rick.carlino@gmail.com>
2016-09-26 15:58:11 -0500

    Weird peer dependency stuff

--- install_frontend.sh
+++ install_frontend.sh
@@ -8,6 +8,7 @@ mkdir public/ -p
 cp -R /tmp/farmbot_frontend/* public/ 
 cd public
 npm install -g webpack 2>&1
+npm install webpack 2>&1
 npm install i18n-webpack-plugin -g 2>&1
 npm install 2>&1
 # npm run build 2>&1
