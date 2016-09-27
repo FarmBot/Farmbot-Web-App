8d228148936b9bb2c248f5e98eb836c3f618c3ab
Rick Carlino <rick.carlino@gmail.com>
2016-09-27 16:11:10 -0500

    Remove danerous --dev flag

--- install_frontend.sh
+++ install_frontend.sh
@@ -8,7 +8,6 @@ mkdir public/ -p
 cp -R /tmp/farmbot_frontend/* public/ 
 cd public
 npm install -g webpack --no-bin-links 2>&1
-npm install --dev --no-bin-links 2>&1
 npm install --no-bin-links 2>&1
 chmod +x node_modules/webpack/bin/webpack.js
 npm run build 2>&1
