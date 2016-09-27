71fc5ab4c175cd3f3daf4e3db457f6ce94429d44
Rick Carlino <rick.carlino@gmail.com>
2016-09-27 16:33:06 -0500

    Done.

--- install_frontend.sh
+++ install_frontend.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Clone into a temp folder...
 rm -rf /tmp/farmbot_frontend
 mkdir /tmp/farmbot_frontend
 git clone https://github.com/farmbot/farmbot-web-frontend.git /tmp/farmbot_frontend
 rm -rf /tmp/farmbot_frontend/.git
+
+# Move it over to the rails /public directory and install deps
 mkdir public/ -p
 cp -R /tmp/farmbot_frontend/* public/ 
 cd public
-# npm install -g webpack 2>&1
 npm install 2>&1
+
+# Make webpack executable and compile everything.
 chmod +x node_modules/webpack/bin/webpack.js
 npm run build 2>&1
