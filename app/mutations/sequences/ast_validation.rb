bc82743b081417b99515b254821dd701d307849d
Rick Carlino <rick.carlino@gmail.com>
2016-10-06 17:28:52 -0500

    Syntax checker

--- app/mutations/sequences/ast_validation.rb
+++ app/mutations/sequences/ast_validation.rb
@@ -2,21 +2,20 @@ module Sequences
   # Validate the syntax of a sequence body.
   # Just validates syntax. Does not perform any sort of contextual analysis. 
   class AstValidation < Mutations::Command
-
-    PARAM_SCHEMA = {
+    PARAM_SCHEMA      = {
         x:               Fixnum,
         y:               Fixnum,
         z:               Fixnum,
         speed:           Fixnum,
         pin_number:      Fixnum,
         pin_value:       Fixnum,
-        pin_mode:        Fixnum, # Refine to 0 | 1;
+        pin_mode:        Fixnum,
         data_label:      String,
         milliseconds:    Fixnum,
         message:         String,
-        sub_sequence_id: Fixnum, # Requires contextual analysis.
-        lhs:             String, # "x" | "y" | "z" | "PUT_OTHER_STUFF_HERE";
-        op:              String, # ">" | "<" | "is" | "not";
+        sub_sequence_id: Fixnum, # TODO Requires contextual analysis.
+        lhs:             String,
+        op:              String,
         rhs:             Fixnum
     }
 
@@ -31,6 +30,21 @@ module Sequences
       if_statement:  [ :lhs, :op, :rhs, :sub_sequence_id ]
     }
 
+    ALLOWED_OPS       = ["<", ">", "is", "not"]
+    ALLOWED_PIN_MODES = [0, 1]
+    ALLOWED_LHS       = [ "x", "y", "z", "s", "busy",
+                          "param_version", "movement_timeout_x",
+                          "movement_timeout_y", "movement_timeout_z",
+                          "movement_invert_endpoints_x", "movement_invert_endpoints_y",
+                          "movement_invert_endpoints_z", "movement_invert_motor_x",
+                          "movement_invert_motor_y", "movement_invert_motor_z",
+                          "movement_steps_acc_dec_x", "movement_steps_acc_dec_y",
+                          "movement_steps_acc_dec_z", "movement_home_up_x",
+                          "movement_home_up_y", "movement_home_up_z", "movement_min_spd_x",
+                          "movement_min_spd_y", "movement_min_spd_z", "movement_max_spd_x",
+                          "movement_max_spd_y", "movement_max_spd_z", "time", "pin0", "pin1",
+                          "pin2", "pin3", "pin4", "pin5", "pin6", "pin7", "pin8", "pin9",
+                          "pin10", "pin11", "pin12", "pin13", ]
     required do
       array :body do
         hash do
@@ -45,30 +59,70 @@ module Sequences
       body.each_with_index do |node, index|
         validate_structure(node, index)
       end
+
+      # TODO: I think this will be slow.
+      # I have no evidence, though.
+      body.each_with_index do |node, index|
+        node[:args].keys.each do |key|
+          name = "validate_#{ key }"
+          send(name, node, index) if respond_to?(name)
+        end
+      end
     end
 
     def execute
-      inputs[:body].map do |hash|
-        { # Strip out irrelevant keys.
-          kind: hash[:kind],
-          args: hash[:args],
-          comments: hash[:comments]
-        }
+      inputs[:body]
+    end
+
+    def validate_pin_mode(node, index)
+      actual = node[:args][:pin_mode]
+      unless ALLOWED_PIN_MODES.include?(actual)
+        sorry("pin_mode",
+              index + 1,
+              ALLOWED_PIN_MODES.join(" or "),
+              actual)
+      end
+    end
+
+    def validate_op(node, index)
+      actual = node[:args][:op]
+      unless ALLOWED_OPS.include?(actual)
+        sorry("op (operand)",
+              index + 1,
+              ALLOWED_OPS.join(" or "),
+              actual)
+      end
+    end
+
+    def validate_lhs(node, index)
+      actual = node[:args][:lhs]
+      unless ALLOWED_LHS.include?(actual)
+        sorry("lhs (left hand side)",
+              index + 1,
+              "one of: " + ALLOWED_LHS.join(", "),
+              actual)
       end
     end
 
 private
+
     def validate_structure(node, index)
       ARGS_SCHEMA[node[:kind].to_sym].each do |arg|
         value = node[:args][arg]
-        unless value.is_a?(PARAM_SCHEMA[arg])
+        klass = PARAM_SCHEMA[arg]
+        sorry(arg,
+              index + 1,
+              klass,
+              value.class.name) unless value.is_a?(klass)
+      end
+    end
+
+    def sorry(cause, location, expected, actual)
           add_error :bad_args,
           :bad_args,
-          "Expected argument '#{}' of node"\
-          " at position #{ index + 1 } to be a"\
-          " #{ klass }, but got #{ p.class }"
-        end
-      end
+          "Expected '#{ cause }' of step"\
+          " #{ location } to be #{ expected }, "\
+          "got #{ actual }"
     end
   end
 end
--- spec/mutations/sequences/ast_validation_spec.rb
+++ spec/mutations/sequences/ast_validation_spec.rb
@@ -21,11 +21,52 @@ describe Sequences::AstValidation do
       expect(result[index][:kind]).to eq(node["kind"])
     end
   end
-  it 'strips out useless args'
-  it 'explains invalid args'
-  it 'ensures that mode is 0 or 1'
-  it 'validates lhs'
-  it 'validates op'
-  it 'validates rhs'
+
+  it 'strips out useless args' do
+    nodes[0]["foo"] = "bar"
+    results = Sequences::AstValidation.run!(body: nodes)
+    expect(results.map { |i| i["foo"] }.uniq!.first).to eq(nil)
+  end
+
+  it 'allows comments' do
+    msg = "Just like a real programming language"
+    nodes[0]["comments"] = msg
+    results = Sequences::AstValidation.run!(body: nodes)
+    expect(results[0]["comments"]).to eq(msg)
+  end
+
+  it 'explains invalid args' do
+    nodes[3]["args"]["pin_number"] = {}
+    results = Sequences::AstValidation.run(body: nodes)
+    expect(results.success?).to eq(false)
+    expect(results.errors.length).to eq(1)
+    actual = results.errors["bad_args"].message
+    expected = "Expected 'pin_number' of step 4 to be Fixnum, got ActiveSupport::HashWithIndifferentAccess"
+    expect(actual).to eq(expected)
+  end
+
+  it 'ensures that mode is 0 or 1' do
+    nodes[2]["args"]["pin_mode"] = 6 # Not valid.
+    results = Sequences::AstValidation.run(body: nodes)
+    expect(results.success?).to eq(false)
+    expect(results.errors.length).to eq(1)
+    actual = results.errors["bad_args"].message
+    expected = "Expected 'pin_mode' of step 3 to be 0 or 1, got 6"
+    expect(actual).to eq(expected)
+  end
+
+  it 'validates lhs' do
+    nodes.last["args"]["lhs"] = "foo"
+    results = Sequences::AstValidation.run(body: nodes)
+    expected = "Expected 'lhs (left hand side)' of step 8 to be one of:"
+    expect(results.errors["bad_args"].message).to include(expected)
+  end
+
+  it 'validates op' do
+    nodes.last["args"]["op"] = "/"
+    results = Sequences::AstValidation.run(body: nodes)
+    expected = "Expected 'op (operand)' of step 8 to be < or > or is or not, got /"
+    expect(results.errors["bad_args"].message).to include(expected)
+  end
 
 end
