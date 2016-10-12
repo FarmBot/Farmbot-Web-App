56b65c83e8187873c3a84c03de80710a8583b1d0
Rick Carlino <rick.carlino@gmail.com>
2016-10-12 10:07:16 -0500

    Clean up debuggers and logs

--- lib/celery_script/tree_climber.rb
+++ lib/celery_script/tree_climber.rb
@@ -1,23 +1,16 @@
 module CeleryScript
   class TreeClimber
     def self.travel(node, callable = nil, &blk)
-      puts ""
       visit_node(node, callable || blk)
-    end
-
-    def self.find_by_kind(node)
     end
 
   private
 
     def self.visit_node(node, callable)
       if node.is_a?(AstNode)
-        puts "visiting #{node.kind}"
         callable.call(node)
         visit_each_arg(node, callable)
         visit_each_body_item(node, callable)
-      else
-        puts "Skip     #{node.to_s.first(40)}"
       end
     end
 
--- spec/lib/celery_script/tree_climber_spec.rb
+++ spec/lib/celery_script/tree_climber_spec.rb
@@ -1,21 +1,19 @@
 require 'spec_helper'
 
 describe CeleryScript::TreeClimber do
-  FIXTURE_FILE = File.read("/home/rick/code/farmbot/api/spec/mutations/sequences/improved_ast_fixture.json")
+  file_path = File.read("/home/rick/code/farmbot/api/spec/mutations/sequences/improved_ast_fixture.json")
   let(:node) do
-      hash = JSON.parse(FIXTURE_FILE).deep_symbolize_keys
+      hash = JSON.parse(file_path).deep_symbolize_keys
       CeleryScript::AstNode.new(hash)
   end
 
   it "travels to each node" do
       kinds = []
       callback = ->(node) { kinds.push(node.kind) }
-      begin
-        CeleryScript::TreeClimber.travel(node, callback)
-      rescue => exception
-        binding.pry        
-      end
+      CeleryScript::TreeClimber.travel(node, callback)
       kinds.sort!
-      binding.pry
+      expect(kinds.length).to eq(5)
+      expect(kinds.first).to eq("blah")
+      expect(kinds.last).to eq("whatever")
   end
 end
