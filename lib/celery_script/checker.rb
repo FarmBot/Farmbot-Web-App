6d7c17589de18c782451d492a380fcdc6d0dcd00
Rick Carlino <rick.carlino@gmail.com>
2016-10-13 12:36:26 -0500

    Delete old junk

--- lib/celery_script/checker.rb
+++ lib/celery_script/checker.rb
@@ -1,35 +1,5 @@
 module CeleryScript
   class TypeCheckError < StandardError; end
   class Checker
-      attr_reader :tree, :corpus, :problems
-
-      UNKNOWN_NODE_KIND = "`kind` attribute of node is not recognized."
-
-      def initialize(tree, corpus)
-          @tree, @corpus, @problems = tree, corpus, []
-      end
-
-      def run
-        puts "========================================="
-        check = ->(node) { validate_node(node) if node }
-        TreeClimber.travel(tree, check)
-        self
-      end
-
-      def validate_node(node)
-          nd = corpus.fetchNodeSpecification(node.kind)
-          if nd
-            # TODO: Try &method(:problem)
-            info = ValidationInfo.new(node, corpus, ->(n, m) { problem(n, m) })
-            nd.validate(info)
-          else
-            problem(node, UNKNOWN_NODE_KIND)
-          end
-      end
-
-      def problem(node, message)
-          binding.pry
-          raise TypeCheckError, Problem.new(node, message)
-      end
   end
 end
--- lib/celery_script/corpus.rb
+++
@@ -1,4 +0,0 @@
-module CeleryScript
-  class Corpus
-  end
-end
