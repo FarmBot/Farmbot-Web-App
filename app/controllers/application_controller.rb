b5bf411f151713d9672c2cbfa642d78bbe0eb1f2
Rick Carlino <rick.carlino@gmail.com>
2016-11-09 16:54:05 -0600

    Remove EVEN MORE non-API parts of the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 end
