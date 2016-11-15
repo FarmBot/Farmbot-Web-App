0df3e66fa9474e761d66a080c05c0b410b1d5cd1
Rick Carlino <rick.carlino@gmail.com>
2016-11-15 10:27:58 -0600

    Remove double validation errors when trying to register a pre-registered user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 ### A single system User on the decision support system.
 class User < ActiveRecord::Base
   belongs_to :device, dependent: :destroy
-  validates_uniqueness_of :email
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable
 
