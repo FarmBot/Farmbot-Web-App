2b484deb11609b9601981da3cc2f549ce5ee6773
Rick Carlino <rick.carlino@gmail.com>
2016-11-18 16:40:25 -0600

    Ascii art indentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # User for testing purposes:
 unless Rails.env == "production"
-ENV['MQTT_HOST'] = "blooper.io"
-ENV['OS_UPDATE_SERVER'] = "http://blah.com"
-ENV['FW_UPDATE_SERVER'] = "http://test.com"
+    ENV['MQTT_HOST'] = "blooper.io"
+    ENV['OS_UPDATE_SERVER'] = "http://blah.com"
+    ENV['FW_UPDATE_SERVER'] = "http://test.com"
 
-    Users::Create.run!(name:                  "Administrator",
-                       email:                 "admin@admin.com",
-                       password:              "password123",
-                       password_confirmation: "password123")
+        Users::Create.run!(name:                  "Administrator",
+                           email:                 "admin@admin.com",
+                           password:              "password123",
+                           password_confirmation: "password123")
 end
