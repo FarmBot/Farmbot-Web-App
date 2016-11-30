0623691b6a7948a6cd4c4fc76e1efcae389079c2
Rick Carlino <rick.carlino@gmail.com>
2016-11-30 14:30:48 -0600

    Tests for ToolSlotController

--- spec/controllers/api/tool_slots/show_spec.rb
+++ spec/controllers/api/tool_slots/show_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
-describe Api::ToolBaysController do
+describe Api::ToolSlotsController do
   include Devise::Test::ControllerHelpers
   describe '#show' do
     let(:user) { FactoryGirl.create(:user) }
     let!(:tool_bay) { FactoryGirl.create(:tool_bay, device: user.device) }
     let!(:tool_slot) { FactoryGirl.create(:tool_slot, tool_bay: tool_bay) }
 
-    it 'updates a tool bay' do
+    it 'renders a tool slot' do
       sign_in user
-      payload = { id: tool_bay.id }
+      payload = { id: tool_slot.id }
       get :show, params: payload
-      expect(payload[:id]).to eq(json[:id])      
-      expect(json[:name]).to eq(tool_bay.name)      
+      expect(response.status).to eq(200)
+      that = tool_slot.as_json.symbolize_keys.except(:created_at, :updated_at)
+      expect(that).to eq(json)
     end
   end
 end
