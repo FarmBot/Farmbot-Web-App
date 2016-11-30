1b28f82ff929122aa3f05b47c8d12c59891224b5
Rick Carlino <rick.carlino@gmail.com>
2016-11-30 09:54:44 -0600

    Add tests for ToolBayController#show and #update

--- spec/controllers/api/tool_bays/update_spec.rb
+++ spec/controllers/api/tool_bays/update_spec.rb
@@ -8,8 +8,9 @@ describe Api::ToolBaysController do
     it 'updates a tool bay' do
       sign_in user
       payload = { name: "Fooo", id: tool_bay.id }
-      patch :update, params: payload
       expect(tool_bay.name).not_to eq(payload[:name])
+      patch :update, params: payload
+      tool_bay.reload
       expect(response.status).to eq(200)
       expect(tool_bay.name).to eq(payload[:name])
       expect(json[:name]).to eq(payload[:name])      
