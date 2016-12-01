2116295764426f064980d19191d7023043ff5277
Rick Carlino <rick.carlino@gmail.com>
2016-12-01 16:52:18 -0600

    Password reset tokens and password updates

--- app/controllers/api/password_resets_controller.rb
+++ app/controllers/api/password_resets_controller.rb
@@ -3,11 +3,14 @@ module Api
     skip_before_action :authenticate_user!, only: [:create, :update]
 
     def create
-      mutate PasswordResets::Create.run({})
+      mutate PasswordResets::Create.run(email: params[:email])
     end
 
     def update
-      mutate PasswordResets::Update.run({})
+      mutate PasswordResets::Update.run(
+        password:              params[:password],
+        password_confirmation: params[:password_confirmation],
+        token:                 params[:id])
     end
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
       @user = user
       mail(to: @user.email, subject: 'Welcome to The FarmBot Web App!')
     end
+
+    def password_reset(user, raw_token)
+      @user  = user
+      @token = raw_token
+      mail(to: @user.email, subject: 'FarmBot Password Reset Instructions')
+    end
 end
