2e8009711967295e73125e1e433b8362806dbe60
Rick Carlino <rick.carlino@gmail.com>
2016-12-05 09:17:51 -0600

    Fix erroneous migration

--- db/migrate/20161201195329_create_token_expirations.rb
+++ db/migrate/20161201195329_create_token_expirations.rb
@@ -1,9 +1,9 @@
 class CreateTokenExpirations < ActiveRecord::Migration[5.0]
   def change
     create_table :token_expirations do |t|
-      t.string :sub
-      t.number :exp
-      t.string :jti
+      t.string  :sub
+      t.integer :exp
+      t.string  :jti
 
       t.timestamps
     end
