1349fae6d93e9dbe376c727c5fcffb42cd40908d
Rick Carlino <rick.carlino@gmail.com>
2016-12-08 15:09:13 -0600

    Fix buuld (sorry)

--- spec/controllers/api/users/verification_spec.rb
+++ spec/controllers/api/users/verification_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Api::UsersController do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user, verified_at: nil) }
   include Devise::Test::ControllerHelpers
 
     it 'creates a new user' do
