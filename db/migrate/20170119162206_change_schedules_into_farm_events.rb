c72003c5d97209e93cd055357e0ba6564aabc90a
Rick Carlino <rick.carlino@gmail.com>
2017-01-19 11:15:45 -0600

    Migration hotfix

--- db/migrate/20170119162206_change_schedules_into_farm_events.rb
+++ db/migrate/20170119162206_change_schedules_into_farm_events.rb
@@ -1,6 +1,6 @@
 class ChangeSchedulesIntoFarmEvents < ActiveRecord::Migration[5.0]
   def change
-    remove_column :farm_events, :sequence_id
+    remove_column :farm_events, :sequence_id, :integer
     rename_table  :farm_events, :farm_events
     add_column    :farm_events, :repeats, :boolean
     add_reference :farm_events,
