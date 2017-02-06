32371fb3ec6f0e13949d0d89a36f3fa4a9549908
Rick Carlino <rick.carlino@gmail.com>
2017-02-06 13:54:53 -0600

    Fix farmEvents::Create more

--- app/mutations/farm_events/create.rb
+++ app/mutations/farm_events/create.rb
@@ -1,7 +1,8 @@
 module FarmEvents
   class Create < Mutations::Command
     using LegacyRefinementsModule
-
+    NO_EXECUTABLE = "You must provide a valid executable_id and "\
+                    "executable_type for a Sequence or Regimen object."
     required do
       integer :executable_id
       string :executable_type, in: ["Sequence", "Regimen"]
@@ -15,10 +16,24 @@ module FarmEvents
       time :end_time
     end
 
+    def validate
+        add_error :executable, :not_found, NO_EXECUTABLE unless executable
+    end
+
     def execute
       create(FarmEvent, inputs) do |sched|
         sched.next_time = sched.calculate_next_occurence
       end
+    end
+
+    private
+
+    def executable
+      @executable ||= klass.where(id: executable_id).first
+    end
+
+    def klass
+      ({"Sequence" => Sequence, "Regimen"  => Regimen })[executable_type]
     end
   end
 end
