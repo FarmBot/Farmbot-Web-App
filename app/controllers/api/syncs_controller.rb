2cfac1bcbe785e51d186bd0d0cab15f0eae42095
connor rigby <connor@farmbot.io>
2017-02-09 10:59:12 -0800

    :basketball: add profiler for sync object

--- app/controllers/api/syncs_controller.rb
+++ app/controllers/api/syncs_controller.rb
@@ -1,12 +1,20 @@
 
 # Generates a tree of all syncable resources. Typically used by a FarmBot device
 # for caching of resources. 
+$PROFILE = ->(marker){
+  $LAST_ONE ||= Time.now
+  diff = Time.now - $LAST_ONE
+  puts "#{"=" * 30} (#{diff.to_s}) @#{marker.to_s}"
+  $LAST_ONE = Time.now
+}
+
 module Api
   class SyncsController < Api::AbstractController
 
     # GET /api/sync
     def show
-      mutate Sync::Fetch.run(device: current_device)
+      $PROFILE.("ENTERING MUTATION")
+      mutate Sync::Fetch.run(device: current_device).tap { $PROFILE.("EXIT MUTATION") }
     end
   end
 end
--- app/mutations/sync/fetch.rb
+++ app/mutations/sync/fetch.rb
@@ -34,61 +34,61 @@ module Sync
 
     def farm_events
       @farm_events ||= ActiveModel::ArraySerializer.new(FarmEvent.where(device: device),
-                                                        each_serializer: FarmEventSerializer)
+                                                        each_serializer: FarmEventSerializer).tap { $PROFILE.("1") }
     end
 
     def images
       @images ||= ActiveModel::ArraySerializer
                     .new(Images::Fetch.run!(device: device),
-                         each_serializer: ImageSerializer)
+                         each_serializer: ImageSerializer).tap { $PROFILE.("2") }
     end
 
     def tools
       @tools = ActiveModel::ArraySerializer.new(Tool.where(device: device),
-                                                each_serializer: ToolSerializer)
+                                                each_serializer: ToolSerializer).tap { $PROFILE.("3") }
     end
 
     def tool_slots
       @tool_slots ||= ActiveModel::ArraySerializer
                         .new(ToolSlot.where(tool_bay_id: tool_bays.pluck(:id)),
-                             each_serializer: ToolSlotSerializer)
+                             each_serializer: ToolSlotSerializer).tap { $PROFILE.("4") }
     end
 
     def tool_bays
-      @tool_bays ||= device.tool_bays
+      @tool_bays ||= device.tool_bays.tap { $PROFILE.("5") }
     end
 
     def plants
-      @plants ||= device.plants
+      @plants ||= device.plants.tap { $PROFILE.("6") }
     end
 
     def regimen_items
-      @regimen_items ||= RegimenItem.where(regimen_id: regimens.pluck(:id))
+      @regimen_items ||= RegimenItem.where(regimen_id: regimens.pluck(:id)).tap { $PROFILE.("7") }
     end
 
     def peripherals
-      @peripherals ||= device.peripherals
+      @peripherals ||= device.peripherals.tap { $PROFILE.("8") }
     end
 
     def regimens
-      @regimens ||= device.regimens;
+      @regimens ||= device.regimens.tap { $PROFILE.("9") };
     end
 
     def sequences
-      @sequences ||= device.sequences
+      @sequences ||= device.sequences.tap { $PROFILE.("10") }
     end
 
     def users
-      @users ||= device.users
+      @users ||= device.users.tap { $PROFILE.("11") }
     end
 
     def logs
-      @logs ||= device.limited_log_list
+      @logs ||= device.limited_log_list.tap { $PROFILE.("12") }
     end
 
     def points
       @points = ActiveModel::ArraySerializer.new(Point.where(device: device),
-                                                each_serializer: PointSerializer)
+                                                each_serializer: PointSerializer).tap { $PROFILE.("13") }
     end
 
     # PROBLEM: The UI does not offer a means of creating tool bays. You must
@@ -97,7 +97,7 @@ module Sync
     # TODO: Remove this when UI level creation of tool bays happens.
     def maybe_initialize_a_tool_bay
       unless device.tool_bays.any?
-        ToolBay.create(device: device, name: "Tool Bay 1")
+        ToolBay.create(device: device, name: "Tool Bay 1").tap { $PROFILE.("14") }
       end
     end
   end
