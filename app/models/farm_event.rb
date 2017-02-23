cbd6b9d1a44a4f0ff167955cfca88915500e96a8
Rick Carlino <rick.carlino@gmail.com>
2017-02-23 17:21:53 -0600

    Fix stuff

--- app/models/farm_event.rb
+++ app/models/farm_event.rb
@@ -12,35 +12,22 @@ class FarmEvent < ActiveRecord::Base
   belongs_to :device
   validates :device_id, presence: true
 
-  class NullSchedule
-    def initialize(*)
-    end
-
-    def next_occurrence
-    end
-
-    def occurrences_between(*)
-      []
-    end
-  end
-
   def farm_event_rules
-    @farm_event_rules ||= make_rules!
+    if time_unit.to_sym == NEVER
+      {}
+    else
+      @farm_event_rules ||= IceCube::Schedule.new(start_time, end_time: end_time) do |sch|
+        sch.add_recurrence_rule IceCube::Rule.send(time_unit.to_sym, repeat)
+      end
+    end
   end
 
   def calculate_next_occurence
     farm_event_rules.next_occurrence
   end
 
-  def calendar(start = Time.now.midnight, finish = end_time)
+  def between(start, finish)
+    # Just for reference for later. Probably should just delegate.
     farm_event_rules.occurrences_between(start, finish)
-  end
-
-  def make_rules!
-    unit  = time_unit.to_sym
-    klass = unit == NEVER ? NullSchedule : IceCube::Schedule
-    klass.new(start_time, end_time: end_time) do |sch|
-      sch.add_recurrence_rule IceCube::Rule.send(unit, repeat)
-    end
   end
 end
