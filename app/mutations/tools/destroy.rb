fa079548a2b71852d0f783365d94a494b046dca5
Rick Carlino <rick.carlino@gmail.com>
2017-03-08 15:10:31 -0600

    Verbiage fix related to deletion of slotted tools

--- app/mutations/tools/destroy.rb
+++ app/mutations/tools/destroy.rb
@@ -3,7 +3,7 @@ module Tools
     STILL_IN_USE  = "Can't delete tool because the following sequences are "\
                     "still using it: %s"
     STILL_IN_SLOT = "Can't delete tool because it is still in a tool slot. "\
-                    "Please remove it from the tool bay first."
+                    "Please remove it from the tool slot first."
 
     required do
       model :tool, class: Tool
