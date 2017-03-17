fa9e38f510f3b06efd88d8b2a56c66b66fb6032c
Rick Carlino <rick.carlino@gmail.com>
2017-03-17 08:01:31 -0500

    More sync object trimming

--- app/mutations/sync/fetch.rb
+++ app/mutations/sync/fetch.rb
@@ -1,96 +1,17 @@
 module Sync
   class Fetch  < Mutations::Command
-    API_VERSION = ENV.fetch("HEROKU_SLUG_COMMIT", `git log --pretty=format:"%h" -1`)
-    COMPAT_NUM  = 1
+    API_VERSION = ENV.fetch("HEROKU_SLUG_COMMIT",
+                            `git log --pretty=format:"%h" -1`)
 
     required do
       model :device, class: Device
     end
 
-    def validate
-      maybe_initialize_a_tool_bay
-    end
-
     def execute
-      return { api_version:   API_VERSION[0,7],
-               compat_num:    COMPAT_NUM,
-               device:        device,
-               users:         users,
-               regimen_items: regimen_items,
-               logs:          logs
-              }
-    end
-
-  private
-
-    def farm_events
-      @farm_events ||= ActiveModel::ArraySerializer.new(FarmEvent.where(device: device),
-                                                        each_serializer: FarmEventSerializer)
-    end
-
-    def images
-      @images ||= ActiveModel::ArraySerializer
-                    .new(Images::Fetch.run!(device: device),
-                         each_serializer: ImageSerializer)
-    end
-
-    def tools
-      @tools = ActiveModel::ArraySerializer.new(Tool.where(device: device),
-                                                each_serializer: ToolSerializer)
-    end
-
-    def tool_slots
-      @tool_slots ||= ActiveModel::ArraySerializer
-                        .new(ToolSlot.where(tool_bay_id: tool_bays.pluck(:id)),
-                             each_serializer: ToolSlotSerializer)
-    end
-
-    def tool_bays
-      @tool_bays ||= device.tool_bays
-    end
-
-    def plants
-      @plants ||= device.plants
-    end
-
-    def regimen_items
-      @regimen_items ||= RegimenItem.where(regimen_id: regimens.pluck(:id))
-    end
-
-    def peripherals
-      @peripherals ||= device.peripherals
-    end
-
-    def regimens
-      @regimens ||= device.regimens
-    end
-
-    def sequences
-      @sequences ||= device.sequences
-    end
-
-    def users
-      @users ||= device.users
-    end
-
-    def logs
-      @logs ||= device.limited_log_list
-    end
-
-    def points
-      # FAST
-      @points = ActiveModel::ArraySerializer.new(Point.where(device: device),
-                                                each_serializer: PointSerializer)
-    end
-
-    # PROBLEM: The UI does not offer a means of creating tool bays. You must
-    # have a ToolBay to create a ToolSlot or Tool. Temporary fix: Create a
-    # ToolBay in the background if the user does not have one.
-    # TODO: Remove this when UI level creation of tool bays happens.
-    def maybe_initialize_a_tool_bay
-      unless device.tool_bays.any?
-        ToolBay.create(device: device, name: "Tool Bay 1")
-      end
+      return {
+        users:         device.users,
+        regimen_items: RegimenItem.where(regimen_id: device.regimens.pluck(:id))
+      }
     end
   end
 end
--- spec/controllers/api/sync/sync_spec.rb
+++ spec/controllers/api/sync/sync_spec.rb
@@ -8,15 +8,11 @@ describe Api::SyncsController do
     it 'downloads a sync object' do
       sign_in user
       device_id = user.device.id
-      FactoryGirl.create_list(:sequence, 2, device_id: device_id)
-      FactoryGirl.create_list(:image, 2, device_id: device_id)
-      farm_event_ids = user.device.sequences.map(&:id).sort
 
       get :show
 
       expect(response.status).to eq(200)
       expect(json).to be_a_kind_of(Hash)
-      expect(json[:device][:id]).to eq(device_id)
     end
   end
 end
