9bbe18aca9f0111221c1479be9feb913c9a218c1
Rick Carlino <rick.carlino@gmail.com>
2017-03-20 09:15:29 -0500

    Possible fix for tool.status missing

--- app/serializers/tool_serializer.rb
+++ app/serializers/tool_serializer.rb
@@ -2,10 +2,14 @@ class ToolSerializer < ActiveModel::Serializer
   attributes :id, :name, :status
 
   def status
-    if object.association(:tool_slot).loaded?
-      "unknown" # Prevent N+1 insanity.
-    else
-      object.tool_slot ? "active" : "inactive"
-    end
+    # Going to try this temporarily.
+    # If performance becomes an issue, I can pull it out or refactor.
+    active = ToolSlot.where(tool_id: object.id).any?
+    active ? "active" : "inactive"
+    # if object.association(:tool_slot).loaded?
+    #   "unknown" # Prevent N+1 insanity.
+    # else
+    #   object.tool_slot ? "active" : "inactive"
+    # end
   end
 end
