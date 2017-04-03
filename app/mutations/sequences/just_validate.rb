543e601f7b96fd611bd0ce24b429b3ff2537ce4c
Rick Carlino <rick.carlino@gmail.com>
2017-04-03 14:29:25 -0500

    remove call to .save in validator.

--- app/mutations/sequences/just_validate.rb
+++ app/mutations/sequences/just_validate.rb
@@ -17,16 +17,6 @@ module Sequences
     end
 
     def execute
-      seq = Sequence.new(inputs)
-      seq.args["is_outdated"] = false
-      # version is never user definable!
-      # IF YOU REMOVE THIS BAD STUFF WILL HAPPEN:
-      seq.args["version"]     = SequenceMigration::Base.latest_version
-      # See comment above ^
-      ActiveRecord::Base.transaction do
-        seq.save!
-        reload_dependencies(seq)
-      end
       seq
     end
   end
