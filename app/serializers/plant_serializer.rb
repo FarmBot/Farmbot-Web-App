e69492c99e3770cbb7d97670de39921269d0f268
Rick Carlino <rick.carlino@gmail.com>
2017-04-05 16:29:01 -0500

    Remove icon_url from API; WIP farmevent changes.

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,4 +1,4 @@
 class PlantSerializer < ActiveModel::Serializer
-  attributes :id, :x, :y, :radius, :name, :img_url, :icon_url, :openfarm_slug,
+  attributes :id, :x, :y, :radius, :name, :img_url, :openfarm_slug,
              :created_at, :device_id, :planting_area_id
 end
