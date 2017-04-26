dbcbe2dc2271e7fe14b7f9cb191dc56f4424f88b
Rick Carlino <rick.carlino@gmail.com>
2017-04-26 14:34:11 -0500

    [FAILING] Wrote tests, now to implement

--- app/controllers/api/farm_events_controller.rb
+++ app/controllers/api/farm_events_controller.rb
@@ -1,5 +1,6 @@
 module Api
   class FarmEventsController < Api::AbstractController
+
     def index
       render json: current_device.farm_events
     end
--- spec/controllers/api/farm_events/farm_events_index_spec.rb
+++ spec/controllers/api/farm_events/farm_events_index_spec.rb
@@ -19,5 +19,17 @@ describe Api::FarmEventsController do
       expect(json.length).to eq(2)
       expect(json.map { |s| s[:id] }.sort).to eq(farm_event_ids)
     end
+
+    it 'cleans up old farm events' do
+      sign_in user
+      farm_events = FactoryGirl.create_list(:farm_event,
+                                            2,
+                                            device_id: user.device.id,
+                                            end_time:  Date.yesterday - 1.day)
+      start_count = FarmEvent.count
+      process :index, method: :get
+      end_count = FarmEvent.count
+      expect(start_count).to be < end_count
+    end
   end
 end
