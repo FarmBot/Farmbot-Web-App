63ad905a540eb7f1376db10260f8e4ecf8b79320
Rick Carlino <rick.carlino@gmail.com>
2017-05-04 11:28:38 -0500

    38 passing specs

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,7 @@
 class Point < ApplicationRecord
-  SHARED_FIELDS = [:x, :y, :radius, :name, :device_id ]
+  SHARED_FIELDS = [:x, :y, :z, :radius, :name, :device_id]
   belongs_to :device
   belongs_to :pointer, polymorphic: true
   validates_presence_of :pointer
+  accepts_nested_attributes_for :pointer
 end
--- spec/controllers/api/plants/update_spec.rb
+++ spec/controllers/api/plants/update_spec.rb
@@ -8,21 +8,20 @@ describe Api::PlantsController do
 
     it 'updates a plant' do
       sign_in user
-      RIGHT_NOW = "2017-01-24T07:40:26.965-06:00"
       p = { id: plant.id,
             x: 23,
             y: 45,
             name: "My Lettuce",
-            openfarm_slug: "limestone-lettuce",
-            created_at: RIGHT_NOW }
+            openfarm_slug: "limestone-lettuce"
+           }
       patch :update, params: p
+      binding.pry
       expect(response.status).to eq(200)
       plant.reload
-      expect(plant.x).to eq(p[:x])
-      expect(plant.y).to eq(p[:y])
-      expect(plant.name).to eq(p[:name])
+      expect(plant.point.x).to eq(p[:x])
+      expect(plant.point.y).to eq(p[:y])
+      expect(plant.point.name).to eq(p[:name])
       expect(plant.openfarm_slug).to eq(p[:openfarm_slug])
-      expect(plant.created_at).to eq(p[:created_at])
       p.keys.each do |key|
         expect(json).to have_key(key)
       end
