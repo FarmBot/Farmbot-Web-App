e4d43ce42f07ac21757d9d44f0db44456200a9a5
Rick Carlino <rick.carlino@gmail.com>
2017-05-04 10:48:56 -0500

    36 passing specs

--- spec/controllers/api/plants/update_spec.rb
+++ spec/controllers/api/plants/update_spec.rb
@@ -4,7 +4,7 @@ describe Api::PlantsController do
   include Devise::Test::ControllerHelpers
   describe '#update' do
     let(:user) { FactoryGirl.create(:user) }
-    let(:plant) { FactoryGirl.create(:plant, device: user.device) }
+    let(:plant) { FactoryGirl.create(:plant_point, device: user.device).pointer }
 
     it 'updates a plant' do
       sign_in user
