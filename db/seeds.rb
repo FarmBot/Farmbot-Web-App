e2adf2be149239a6ea9441820840e5ab457113bd
Rick Carlino <rick.carlino@gmail.com>
2017-05-08 07:41:01 -0500

    Cleanup tool slot stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,10 @@ unless Rails.env == "production"
     ENV['MQTT_HOST']        = "blooper.io"
     ENV['OS_UPDATE_SERVER'] = "http://blah.com"
     ENV['FW_UPDATE_SERVER'] = "http://test.com"
+    ToolSlot.destroy_all
+    Device.destroy_all
     User.destroy_all
     Point.destroy_all
-    Device.destroy_all
     Users::Create.run!(name:                  "Administrator",
                        email:                 "notos@notos.com",
                        password:              "password123",
