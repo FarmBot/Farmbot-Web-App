333d63e4a40e27c749ff1368e6564ed6e8824b87
Rick Carlino <rick.carlino@gmail.com>
2017-05-10 09:43:29 -0500

    WIP: Need to see if we still post arrays to /tool_slots

--- spec/controllers/api/points/create_spec.rb
+++ spec/controllers/api/points/create_spec.rb
@@ -6,6 +6,26 @@ describe Api::PointsController do
     let(:user) { FactoryGirl.create(:user) }
     let(:device) { user.device }
 
+    it 'creates a plant' do
+      sign_in user
+      p = { x: 23,
+            y: 45,
+            name: "My Lettuce",
+            openfarm_slug: "limestone-lettuce" }
+      post :create, body: p.to_json,
+                    params: { format: :json }
+      expect(response.status).to eq(200)
+      plant = Plant.last
+      expect(plant.point.x).to eq(p[:x])
+      expect(plant.point.y).to eq(p[:y])
+      expect(plant.point.name).to eq(p[:name])
+      expect(plant.openfarm_slug).to eq(p[:openfarm_slug])
+      expect(plant.created_at).to be_truthy
+      p.keys.each do |key|
+        expect(json).to have_key(key)
+      end
+    end
+
     it 'creates a point' do
       sign_in user
       body = { x: 1,
--- spec/controllers/api/tool_slots/update_spec.rb
+++ spec/controllers/api/tool_slots/update_spec.rb
@@ -7,16 +7,5 @@ describe Api::ToolSlotsController do
     let!(:tool_slot) do
       FactoryGirl.create(:tool_slot_point, device: user.device).pointer
     end
-
-    it 'updates a tool slot' do
-      sign_in user
-      payload = { name: "Fooo" }
-      expect(tool_slot.point.name).not_to eq(payload[:name])
-      patch :update, body: payload.to_json, params: {id: tool_slot.id}
-      tool_slot.reload
-      expect(response.status).to eq(200)
-      expect(tool_slot.point.name).to eq(payload[:name])
-      expect(json[:name]).to eq(payload[:name])
-    end
   end
 end
