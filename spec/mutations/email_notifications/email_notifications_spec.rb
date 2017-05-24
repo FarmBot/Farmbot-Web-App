6cc55226643a0338c547e95a83fa95cade5bc6ef
Rick Carlino <rick.carlino@gmail.com>
2017-05-24 14:02:49 -0500

    Stub model test

--- spec/mutations/email_notifications/email_notifications_spec.rb
+++ spec/mutations/email_notifications/email_notifications_spec.rb
@@ -1,16 +1,16 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe EmailNotifications::Create do
-  let(:device) { FactoryGirl.create(:device)}
-# USE CASES:
-#  * User needs it NOW => Send it now
-#  * User wrote a runaway recursive sequence. => Rate limit exceeded
-#      * Throttle with Rack::Attack
-#  *
-  it 'sends notifications' do
-    pending
-    results = EmailNotifications::Create.run(device: device)
-  end
-  it 'batches multiple requests into one'
+# describe EmailNotifications::Create do
+#   let(:device) { FactoryGirl.create(:device)}
+# # USE CASES:
+# #  * User needs it NOW => Send it now
+# #  * User wrote a runaway recursive sequence. => Rate limit exceeded
+# #      * Throttle with Rack::Attack
+# #  *
+#   it 'sends notifications' do
+#     pending
+#     results = EmailNotifications::Create.run(device: device)
+#   end
+#   it 'batches multiple requests into one'
 
-end
+# end
