e020aaa11178761fd750c7c9488a6d530b56ed99
Rick Carlino <rick.carlino@gmail.com>
2017-06-29 16:45:04 -0500

    install_frontend.sh changes

--- install_frontend.sh
+++ install_frontend.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "=========   RUNNING install_frontend.sh"
 CURRENT_DIR=$(pwd)
-export BUILT_AT=$(git log --pretty=format:'%h' -1)
-echo "=========   Going into frontend/ directory"
 
-cd frontend
+echo "=========   Going into frontend/ directory"
 
 if [ "$NPM_ADDON" ]; then
     echo "=========   NPM ADD ON DETECTED... INSTALLING"
@@ -12,7 +10,7 @@ if [ "$NPM_ADDON" ]; then
 fi
 
 echo "=========   NPM INSTALLING"
-npm install 2>&1
+yarn install 2>&1
 
 # Make webpack executable and compile everything.
 echo "=========   Changing Webpack permissions"
@@ -20,9 +18,6 @@ chmod +x node_modules/webpack/bin/webpack.js
 
 echo "=========   Running `npm run build`"
 npm run build 2>&1
-
-echo "=========   Going back into Rails dir"
-cd $CURRENT_DIR
 
 # Move it over to the rails /public directory and install deps
 mkdir public/ -p
