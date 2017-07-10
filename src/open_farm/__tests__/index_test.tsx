8d16e762e7ecf359ba2431b42487afeafe694a86
Rick Carlino <rick.carlino@gmail.com>
2017-07-10 10:26:37 -0500

    Update tests to reflect changes

--- src/open_farm/__tests__/index_test.tsx
+++ src/open_farm/__tests__/index_test.tsx
@@ -24,12 +24,12 @@ describe("cachedIcon()", () => {
   it("does an HTTP request if the icon can't be found locally", (done) => {
     cachedCrop("lettuce")
       .then(function (item) {
-        expect(item).toContain(DATA_URI);
-        expect(item).toContain(encodeURIComponent("<svg>Wow</svg>"));
+        expect(item.svg_icon).toContain("<svg>Wow</svg>");
         done();
       })
-      .catch(() => {
-        fail();
+      .catch((error) => {
+        expect(error).toBeFalsy();
+        done();
       });
   });
 
