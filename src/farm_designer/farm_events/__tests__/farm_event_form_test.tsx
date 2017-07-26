ed3aba3e463e66c197ae46e2f0c9e8db2566250d
Rick Carlino <rick.carlino@gmail.com>
2017-07-26 14:02:00 -0500

    Remove old form, start tests for new form.

---
+++ src/farm_designer/farm_events/__tests__/farm_event_form_test.tsx
@@ -0,0 +1,17 @@
+import * as react from "react";
+
+describe("<FarmEventForm/>", () => {
+  it("sets defaults");
+  it("determines if it is a one time event");
+  it("has a dispatch");
+  it("has a view model");
+  it("has an executable");
+  it("sets the executable");
+  it("gets a property of an executable");
+  it("sets a subfield of state.fe");
+  it("gets a subfield of state.fe");
+  it("merges state");
+  it("toggles repeat");
+  it("commits the view model");
+  it("knows if it has a regimen");
+});
