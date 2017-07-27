ab61471516e076f424b262b88b9acb366b46f6e9
Rick Carlino <rick.carlino@gmail.com>
2017-07-27 15:04:01 -0500

    Upgrade to FBJS 4.2.1

--- src/controls/__tests__/axis_display_group_test.ts
+++ src/controls/__tests__/axis_display_group_test.ts
@@ -6,16 +6,18 @@ import { bot } from "../../__test_support__/fake_state/bot";
 describe("<AxisDisplayGroup />", () => {
   let params = { bot, label: "Heyoo" };
   let wrapper = mount(AxisDisplayGroup(params));
+
   it("has 3 inputs and a label", () => {
     expect(wrapper.find("input").length).toEqual(3);
     expect(wrapper.find("label").length).toEqual(1);
   });
+
   it("renders correct values", () => {
     let inputs = wrapper.find("input");
     let label = wrapper.find("label");
-    expect(inputs.at(0).props().value).toBe(-1);
-    expect(inputs.at(1).props().value).toBe(-1);
-    expect(inputs.at(2).props().value).toBe(-1);
+    expect(inputs.at(0).props().value).toBe("");
+    expect(inputs.at(1).props().value).toBe("");
+    expect(inputs.at(2).props().value).toBe("");
     expect(label.text()).toBe("Heyoo");
   });
 });
