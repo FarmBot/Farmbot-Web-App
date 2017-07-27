2deab5ea6788ce49f7ca7dd1a107f02edfb68ca2
Rick Carlino <rick.carlino@gmail.com>
2017-07-27 09:02:44 -0500

    Add tests for <TitleBar/>

--- src/images/weed_detector/__tests__/title_test.tsx
+++ src/images/weed_detector/__tests__/title_test.tsx
@@ -1,7 +1,16 @@
 import * as React from "react";
+import { TitleBar } from "../title";
+import { mount } from "enzyme";
+import { get } from "lodash";
 
 describe("<TitleBar/>", () => {
-  it("needs to be written", () => {
-    fail();
+  it("hides elements when call bakcs are not provided", () => {
+    let props = { title: "WOW!", help: "COOL!" };
+    let el = mount(<TitleBar {...props} />);
+    expect(el.text()).toContain(props.title);
+    expect(el.text()).toContain(props.help);
+    el.find("button").forEach(html => {
+      expect(get(html.props(), "hidden")).toBe(true);
+    });
   });
 });
