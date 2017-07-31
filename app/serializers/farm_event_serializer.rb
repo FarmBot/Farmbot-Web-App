4772ba0a253358f391d806c8c3d15edc7ad7e163
Rick Carlino <rick.carlino@gmail.com>
2017-07-31 12:32:26 -0500

    :heavy_checkmark: Fix GenerateCalendar

--- app/serializers/farm_event_serializer.rb
+++ app/serializers/farm_event_serializer.rb
@@ -15,10 +15,11 @@ class FarmEventSerializer < ActiveModel::Serializer
 
   def sequence_calendar
     FarmEvents::GenerateCalendar
-      .run!(start_time: object.start_time,
-            end_time:   object.end_time,
-            repeat:     object.repeat,
-            time_unit:  object.time_unit)
+      .run!(origin:      object.start_time,
+            lower_limit: instance_options[:upper_limit] || object.start_time,
+            upper_limit: instance_options[:lower_limit] || object.end_time,
+            repeat:      object.repeat,
+            time_unit:   object.time_unit)
       .map(&:utc)
       .map(&:as_json)
   end
