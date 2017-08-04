46229fa89cec6aa4157410e64db7d4bf3bb2c630
MrChristofferson <chrisvan0848@gmail.com>
2017-08-04 14:21:28 -0500

    rm arbitrary tests

--- src/__tests__/hotkeys_test.tsx
+++ src/__tests__/hotkeys_test.tsx
@@ -1,12 +1,7 @@
-import * as React from "react";
-import { HotKeys } from "../hotkeys";
-import { render } from "enzyme";
+// import * as React from "react";
+// import { HotKeys } from "../hotkeys";
+// import { render } from "enzyme";
 
 describe("<HotKeys />", () => {
-
-  it("renders", () => {
-    let container = render(<HotKeys dispatch={jest.fn()} />);
-    expect(container).toBeTruthy();
-  });
-
+  pending("WIP.");
 });
--- src/ui/__tests__/help_test.tsx
+++ src/ui/__tests__/help_test.tsx
@@ -1,11 +1,7 @@
-import * as React from "react";
-import { Help } from "../help";
-import { render } from "enzyme";
+// import * as React from "react";
+// import { Help } from "../help";
+// import { render } from "enzyme";
 
 describe("<Help />", () => {
-  it("renders", () => {
-    let wrapper = render(<Help text={"Heyo"} />);
-    expect(wrapper).toBeTruthy();
-  });
-
+  pending("WIP.");
 });
