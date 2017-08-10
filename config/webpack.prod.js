778e9162771b993fc7a68e294dd4aee552214739
Rick Carlino <rick.carlino@gmail.com>
2017-08-10 12:55:03 -0500

    6

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -91,30 +91,35 @@ module.exports = {
       "process.env.PRIV_URL": JSON
         .stringify(process.env.PRIV_URL || false).toString()
     }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
-    }),
-    new ExtractTextPlugin({
-      // Temporary hotfix for some issues on staging.
-      // - RC 12 MAY 17
-      // filename: "dist/styles.css",
-      filename: "dist/[name].[chunkhash].css",
-      disable: false,
-      allChunks: true
-    }),
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: require("cssnano"),
-      cssProcessorOptions: { discardComments: { removeAll: true } },
-      canPrint: true
-    }),
-    new UglifyJsPlugin({
-      cacheFolder: path.resolve(__dirname, "../public/dist/cached_uglify/"),
-      debug: true,
-      minimize: true,
-      sourceMap: true,
-      screw_ie8: true,
-      output: { comments: false },
-      compressor: { warnings: false }
-    })]
+
+
+    // new webpack.DefinePlugin({
+    //   "process.env.NODE_ENV": JSON.stringify("production")
+    // }),
+    // new ExtractTextPlugin({
+    //   // Temporary hotfix for some issues on staging.
+    //   // - RC 12 MAY 17
+    //   // filename: "dist/styles.css",
+    //   filename: "dist/[name].[chunkhash].css",
+    //   disable: false,
+    //   allChunks: true
+    // }),
+    // new OptimizeCssAssetsPlugin({
+    //   assetNameRegExp: /\.css$/g,
+    //   cssProcessor: require("cssnano"),
+    //   cssProcessorOptions: { discardComments: { removeAll: true } },
+    //   canPrint: true
+    // }),
+    // new UglifyJsPlugin({
+    //   cacheFolder: path.resolve(__dirname, "../public/dist/cached_uglify/"),
+    //   debug: true,
+    //   minimize: true,
+    //   sourceMap: true,
+    //   screw_ie8: true,
+    //   output: { comments: false },
+    //   compressor: { warnings: false }
+    // })
+
+
+  ]
 }
