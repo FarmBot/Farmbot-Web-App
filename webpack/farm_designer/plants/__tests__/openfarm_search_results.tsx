beba944b97f6f399d9378a0bbef2ecd07e594f64
Rick Carlino <rick.carlino@gmail.com>
2017-08-23 15:49:30 -0500

    Add tests for openfarm_search_results

---
+++ webpack/farm_designer/plants/__tests__/openfarm_search_results.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { mount } from "enzyme";
+import { OpenFarmResults, SearchResultProps } from "../openfarm_search_results";
+
+describe("<OpenFarmResults/>", () => {
+  it("renders OpenFarmSearchResults", () => {
+    let props: SearchResultProps = {
+      cropSearchResults: [
+        {
+          crop: {
+            slug: "potato",
+            name: "S. tuberosum"
+          },
+          image: "potato.jpg"
+        },
+        {
+          crop: {
+            slug: "tomato",
+            name: "Solanum lycopersicum"
+          },
+          image: "tomato.jpg"
+        },
+      ]
+    };
+    let el = mount(<OpenFarmResults {...props} />);
+    let text = el.text();
+    expect(text).toContain(props.cropSearchResults[0].crop.name);
+    expect(text).toContain(props.cropSearchResults[1].crop.name);
+    expect(el.find("Link").length).toEqual(props.cropSearchResults.length);
+    expect(el.find("Link").first().prop("to"))
+      .toContain(props.cropSearchResults[0].crop.slug);
+  });
+});
