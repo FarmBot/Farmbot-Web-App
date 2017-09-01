7c0147b4fed44907d1e117604fe50b09e9d43d11
Rick Carlino <rick.carlino@gmail.com>
2017-09-01 15:13:37 -0500

    Moar tests

--- spec/controllers/dashboard_spec.rb
+++ spec/controllers/dashboard_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe DashboardController do
   include Devise::Test::ControllerHelpers
-  describe 'ACME endpoint' do
 
+  render_views
+
+  describe 'ACME endpoint' do
     it "has a fallback" do
       process :lets_encrypt, method: :get, params: { id: "FOO" }
       expect(response.body).to include(DashboardController::NO_ENV)
     end
 
     it "renders the terms of service" do
-      process :tos_update, method: :get
-      binding.pry
+      get :tos_update
       expect(response.status).to eq(200)
+      expect(response.body).to include("webpack/tos_update.js")
     end
   end
 end
