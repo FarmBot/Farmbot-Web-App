bf27cd8d0eee72106e70540291344b3d18399def
Rick Carlino <rick.carlino@gmail.com>
2017-09-25 09:56:54 -0500

    Hound started working again?

--- app/mutations/auth/from_jwt.rb
+++ app/mutations/auth/from_jwt.rb
@@ -9,17 +9,17 @@ module Auth
       claims = token.unencoded
       sub    = claims['sub']
       case sub
-        when Integer then User.find(sub)
-        # HISTORICAL CONTEXT: We once used emails as a `sub` field. At the time,
-        # it seemed nice because it was human readable. The problem was that
-        # emails are mutable. Under this scheme, changing your email address
-        # would invalidate your JWT. Switching it to user_id (that does not
-        # change) gets around this issue. We still need to support emails in
-        # JWTs, atleast for another month or so because it would invalidate
-        # existing tokens otherwise.
-        # TODO: Only use user_id (not email) for validation after 25 OCT 17 - RC
-        when String then User.find_by!(email: sub)
-        else raise "SUB was neither string nor number"
+      when Integer then User.find(sub)
+      # HISTORICAL CONTEXT: We once used emails as a `sub` field. At the time,
+      # it seemed nice because it was human readable. The problem was that
+      # emails are mutable. Under this scheme, changing your email address
+      # would invalidate your JWT. Switching it to user_id (that does not
+      # change) gets around this issue. We still need to support emails in
+      # JWTs, atleast for another month or so because it would invalidate
+      # existing tokens otherwise.
+      # TODO: Only use user_id (not email) for validation after 25 OCT 17 - RC
+      when String then User.find_by!(email: sub)
+      else raise "SUB was neither string nor number"
       end
     rescue JWT::DecodeError
       add_error :jwt, :decode_error, "JSON Web Token is not valid."
