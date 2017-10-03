be04131338f7b0f1d74fda2c8a5666ea92b998dc
Rick Carlino <rick.carlino@gmail.com>
2017-10-03 11:07:27 -0500

    Intercept RESOURCE_READY to apply last_saw_mq heuristics.

--- webpack/connectivity/reducer.ts
+++ webpack/connectivity/reducer.ts
@@ -1,6 +1,9 @@
 import { generateReducer } from "../redux/generate_reducer";
 import { Actions } from "../constants";
-import { ConnectionState, EdgeStatus } from "./interfaces";
+import { ConnectionState, EdgeStatus, ConnectionStatus } from "./interfaces";
+import { DeviceAccountSettings } from "../devices/interfaces";
+import { max, isString } from "lodash";
+import * as moment from "moment";
 
 export const DEFAULT_STATE: ConnectionState = {
   "bot.mqtt": undefined,
@@ -13,4 +16,29 @@ export let connectivityReducer =
     .add<EdgeStatus>(Actions.NETWORK_EDGE_CHANGE, (s, { payload }) => {
       s[payload.name] = payload.status;
       return s;
+    })
+    .add<ResourceReady>(Actions.RESOURCE_READY, (s, a) => {
+      const isRelevant = a.payload.name === "devices";
+      if (isRelevant) {
+        s["bot.mqtt"] = computeBestTime(s["bot.mqtt"], a.payload.data[0]);
+      }
+      return s;
     });
+
+function computeBestTime(cs: ConnectionStatus | undefined,
+  dev: DeviceAccountSettings | undefined,
+  now = moment().toDate()): ConnectionStatus | undefined {
+
+  if (dev && isString(dev.last_saw_mq)) {
+    const mx = max([now, moment(dev.last_saw_mq).toDate()]);
+    return { at: (mx || now).toJSON(), state: (cs && cs.state) || "down" };
+  } else {
+    // don't bother guessing if info is unavailable
+    return cs;
+  }
+}
+
+interface ResourceReady {
+  name: string,
+  data: [DeviceAccountSettings];
+}
