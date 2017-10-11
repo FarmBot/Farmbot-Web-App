9fd294d08f492542b53b9417cfccba349820ab73
Rick Carlino <rick.carlino@gmail.com>
2017-10-11 16:25:50 -0500

    Ability to set vhost

--- mqtt/server.rb
+++ mqtt/server.rb
@@ -1,22 +1,22 @@
 require 'erb'
 puts "=== Retrieving container info"
-PLUGIN_PATH         = "mqtt/jwt_plugin/plugins/rabbit_auth_backend*"
-PLUGIN_IS_BUILT     = Dir[PLUGIN_PATH].any?
-FORCE_REBUILD       = ENV["FORCE_REBUILD"].present?
-DOCKER_IMG_NAME     = "farmbot-mqtt"
-IMG_IS_BUILT        = `cd mqtt; sudo docker images`.include?(DOCKER_IMG_NAME)
+PLUGIN_PATH     = "mqtt/jwt_plugin/plugins/rabbit_auth_backend*"
+PLUGIN_IS_BUILT = Dir[PLUGIN_PATH].any?
+FORCE_REBUILD   = ENV["FORCE_REBUILD"].present?
+DOCKER_IMG_NAME = "farmbot-mqtt"
+IMG_IS_BUILT    = `cd mqtt; sudo docker images`.include?(DOCKER_IMG_NAME)
 
 puts "=== Setting config data"
-CONFIG_PATH         = "./mqtt/conf"
-CONFIG_FILENAME     = "rabbitmq.config"
-CONFIG_OUTPUT       = "#{CONFIG_PATH}/#{CONFIG_FILENAME}"
-NO_API_HOST         = "You need to set API_HOST to a real IP address or " +
-                      "domain name (not localhost)."
-TEMPLATE_FILE       = "./mqtt/rabbitmq.config.erb"
-TEMPLATE            = File.read(TEMPLATE_FILE)
-RENDERER            = ERB.new(TEMPLATE)
-PROTO               = ENV["FORCE_SSL"] ? "https:" : "http:"
-VHOST               = ENV.fetch("MQTT_VHOST") { "/" }
+CONFIG_PATH     = "./mqtt/conf"
+CONFIG_FILENAME = "rabbitmq.config"
+CONFIG_OUTPUT   = "#{CONFIG_PATH}/#{CONFIG_FILENAME}"
+NO_API_HOST     = "You need to set API_HOST to a real IP address or " +
+                  "domain name (not localhost)."
+TEMPLATE_FILE   = "./mqtt/rabbitmq.config.erb"
+TEMPLATE        = File.read(TEMPLATE_FILE)
+RENDERER        = ERB.new(TEMPLATE)
+PROTO           = ENV["FORCE_SSL"] ? "https:" : "http:"
+VHOST           = ENV.fetch("MQTT_VHOST") { "/" }
 
 puts "=== Building JWT plugin config"
 farmbot_api_key_url = ENV.fetch("API_PUBLIC_KEY_PATH") do
