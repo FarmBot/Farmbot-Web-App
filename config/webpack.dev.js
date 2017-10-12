790c2e0e5d80c29f8ac6e059da8aa6d74a34419a
Rick Carlino <rick.carlino@gmail.com>
2017-10-12 11:36:47 -0500

    Upgrade deps, bring back hot reloading.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,16 +3,22 @@ var path = require("path");
 var genConfig = require("./webpack.base");
 var conf = genConfig();
 
+
 conf.output = {
   // must match config.webpack.output_dir
   path: path.join(__dirname, '..', 'public', 'webpack'),
   publicPath: '//localhost:' + devServerPort + '/webpack/',
-  filename: '[name].js'
+  filename: '[name].js',
+  watch: true
 };
 
 conf.devServer = {
   port: devServerPort,
   disableHostCheck: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  },
   host: "0.0.0.0",
   headers: { 'Access-Control-Allow-Origin': '*' }
 };
