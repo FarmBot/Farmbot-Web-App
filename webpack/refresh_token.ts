52d360a3f24387114b7e4be637aeabb10f0a9c72
Rick Carlino <rick.carlino@gmail.com>
2017-10-12 15:34:35 -0500

    Reduce block nesting level. NEXT: Tests

--- webpack/refresh_token.ts
+++ webpack/refresh_token.ts
@@ -1,20 +1,21 @@
 import axios from "axios";
 import { API } from "./api/index";
 import { AuthState } from "./auth/interfaces";
-import { HttpData } from "./util";
+import { HttpData, goHome } from "./util";
 import { setToken } from "./auth/actions";
+
+type Resp = HttpData<AuthState>;
+
+/** What to do when the Token refresh request completes. */
+const ok = (x: Resp) => {
+  setToken(x.data); // Start using new token in HTTP requests.
+  return x.data;
+};
 
 /** Grab a new token from the API (won't extend token's exp. date).
  * Redirect to home page on failure. */
 export let maybeRefreshToken = (old: AuthState): Promise<AuthState> => {
   API.setBaseUrl(old.token.unencoded.iss);
-  setToken(old); // The Axios interceptors might not be set yet.
-  type Resp = HttpData<AuthState>;
-
-  return axios.get(API.current.tokensPath).then((x: Resp) => {
-    setToken(old);
-    return x.data;
-  }, (x) => {
-    return Promise.reject("X");
-  });
+  setToken(old); // Precaution: The Axios interceptors might not be set yet.
+  return axios.get(API.current.tokensPath).then(ok, goHome);
 };
