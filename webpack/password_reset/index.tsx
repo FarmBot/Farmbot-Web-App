9bd931f42c0fe3ba914122ef41d79db97b608317
Rick Carlino <rick.carlino@gmail.com>
2017-10-16 08:18:57 -0500

    Add test for onInit() in password reset page

--- webpack/password_reset/index.tsx
+++ webpack/password_reset/index.tsx
@@ -1,23 +1,6 @@
-import * as React from "react";
 import "../css/_index.scss";
 import { detectLanguage } from "../i18n";
+import * as I18n from "i18next";
+import { onInit } from "./on_init";
 
-detectLanguage().then(async (config) => {
-  const i18next = await import("i18next");
-  const { render } = await import("react-dom");
-  const { PasswordReset } = await import("./password_reset");
-  i18next.init(config, (err, t) => {
-    const node = document.createElement("DIV");
-    node.id = "root";
-    document.body.appendChild(node);
-
-    const reactElem = React.createElement(PasswordReset, {});
-    const domElem = document.getElementById("root");
-
-    if (domElem) {
-      render(reactElem, domElem);
-    } else {
-      throw new Error(t("Add a div with id `root` to the page first."));
-    }
-  });
-});
+detectLanguage().then(async config => I18n.init(config, onInit));
