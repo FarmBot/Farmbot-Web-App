3489dc3b6ad3a7410a6443137e23d04d64e0538b
Rick Carlino <rick.carlino@gmail.com>
2017-10-21 10:35:03 -0500

    Late addition: redirect for already verified email addr changes.

--- app/mutations/users/reverify.rb
+++ app/mutations/users/reverify.rb
@@ -3,10 +3,9 @@ module Users
     required { model :user, class: User }
 
     def execute
-      user.update_attributes!(confirmation_token: nil,
-                              confirmed_at:       Time.now,
-                              email:              user.unconfirmed_email,
-                              unconfirmed_email:  nil)
+      user.update_attributes!(confirmed_at:      Time.now,
+                              email:             user.unconfirmed_email,
+                              unconfirmed_email: nil)
       SessionToken.as_json(user, AbstractJwtToken::HUMAN_AUD)
     end
   end
