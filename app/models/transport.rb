28685dcfd6d6d2729a342f0c676730a0b71da371
Rick Carlino <rick.carlino@gmail.com>
2017-10-30 10:21:55 -0500

    Add AMQP URL inference mechanism

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,10 +1,18 @@
 # A wrapper around AMQP to stay DRY. Will make life easier if we ever need to
 # change protocols
 module Transport
+
+  AMQP_URL = ENV['CLOUDAMQP_URL'] ||
+             ENV['RABBITMQ_URL']  ||
+             "amqp://guest:guest@localhost:5672"
+
+  def self.connection
+    @connection ||= Bunny.new(AMQP_URL, read_timeout: 10, heartbeat: 10).start
+  end
+
   def self.topic
-    @topic ||= Bunny
-                 .new(read_timeout: 10, heartbeat: 10)
-                 .start
+    @topic ||= self
+                 .connection
                  .create_channel
                  .topic("amq.topic", auto_delete: true)
   end
