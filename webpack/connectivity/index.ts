0fa27c42abfaa5ddf76ea12f3467d2f6be399b89
Rick Carlino <rick.carlino@gmail.com>
2017-10-30 11:07:25 -0500

    Debounce network status up / down notifications.

--- webpack/connectivity/index.ts
+++ webpack/connectivity/index.ts
@@ -1,6 +1,10 @@
 import { store } from "../redux/store";
 import { networkUp, networkDown } from "./actions";
 import { Edge } from "./interfaces";
+import { debounce } from "lodash";
+
+/** Debounce calls to these functions to avoid unnecessary re-paints. */
+const SLOWDOWN_TIME = 1500;
 
 /* ABOUT THIS FILE: These functions allow us to mark the network as up or
 down from anywhere within the app (even outside of React-Redux). I usually avoid
@@ -8,11 +12,11 @@ directly importing `store`, but in this particular instance it might be
 unavoidable. */
 
 export let dispatchNetworkUp =
-  (edge: Edge, at = (new Date()).toJSON()) => {
+  debounce((edge: Edge, at = (new Date()).toJSON()) => {
     return store.dispatch(networkUp(edge, at));
-  };
+  }, SLOWDOWN_TIME);
 
 export let dispatchNetworkDown =
-  (edge: Edge, at = (new Date()).toJSON()) => {
+  debounce((edge: Edge, at = (new Date()).toJSON()) => {
     return store.dispatch(networkDown(edge, at));
-  };
+  }, SLOWDOWN_TIME);
