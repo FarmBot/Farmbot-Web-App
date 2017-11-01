6b648e799f884ce3b91c42fcd229f5fea02d1e23
Rick Carlino <rick.carlino@gmail.com>
2017-11-01 12:35:13 -0500

    Our version of node doesnt support Object.values yet

--- webpack/connectivity/__tests__/reducer_reset_network_test.ts
+++ webpack/connectivity/__tests__/reducer_reset_network_test.ts
@@ -1,13 +1,11 @@
 import { connectivityReducer, DEFAULT_STATE } from "../reducer";
 import { ReduxAction } from "../../redux/interfaces";
 import { resetNetwork } from "../../devices/actions";
-
+import { values } from "lodash";
 describe("Connectivity Reducer - RESET_NETWORK", () => {
   it("clears all network status", () => {
     const action: ReduxAction<{}> = resetNetwork();
     const result = connectivityReducer(DEFAULT_STATE, action);
-    Object
-      .values(result)
-      .map(x => expect(x).toBeUndefined);
+    values(result).map((x) => expect(x).toBeUndefined());
   });
 });
