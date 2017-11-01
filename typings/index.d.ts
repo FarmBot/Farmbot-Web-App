1354e2435416964960856600b7bd2253dcbf23ea
Rick Carlino <rick.carlino@gmail.com>
2017-11-01 11:43:35 -0500

    Remove no-longer-needed import from index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="react-redux.d.ts" />
-import { Farmbot } from "farmbot";
 
 /** This contains all of the global ENV vars passed from server => client.
  * Previously was `process.env.XYZ`. */
