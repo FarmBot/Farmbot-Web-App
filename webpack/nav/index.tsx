5c2e1e0bda7c95a29c914ac53cabc2cd0fc9dd84
Rick Carlino <rick.carlino@gmail.com>
2017-11-14 13:02:09 -0600

    Opps type error

--- webpack/nav/index.tsx
+++ webpack/nav/index.tsx
@@ -3,7 +3,7 @@ import { t } from "i18next";
 import { NavBarProps, NavBarState } from "./interfaces";
 import { EStopButton } from "../devices/components/e_stop_btn";
 import { Session } from "../session";
-import { Row, Col, Saucer } from "../ui";
+import { Row, Col } from "../ui";
 import { history } from "../history";
 import { updatePageInfo } from "../util";
 import { SyncButton } from "./sync_button";
