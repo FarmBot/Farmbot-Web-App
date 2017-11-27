7615197dd47d84588fa3461997fdca8cfd40d9bd
Rick Carlino <rick.carlino@gmail.com>
2017-11-27 11:02:31 -0600

    Add test form CeleryScript::JSONClimber

---
+++ spec/lib/celery_script/json_climber_spec.rb
@@ -0,0 +1,19 @@
+fixture = {
+  kind: "parent",
+  args: {},
+  body: [
+    {
+      kind: "child",
+      args: { grandchild: { kind: "grandchild", args: {} } }
+    }
+  ]
+}
+describe "JSONClimber" do
+  it 'Climbs JSON' do
+    results = []
+    CeleryScript::JSONClimber.climb(fixture) do |hmm|
+      results.push(hmm[:kind])
+    end
+    expect(results).to eq(["parent", "child", "grandchild"])
+  end
+end
