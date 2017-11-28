9a362f1851ae881c063e85f9684288190236d321
Rick Carlino <rick.carlino@gmail.com>
2017-11-28 16:48:37 -0600

    TODO: Move generateList() into mapStateToProps

--- webpack/sequences/state_to_props.ts
+++ webpack/sequences/state_to_props.ts
@@ -2,30 +2,36 @@ import { Everything } from "../interfaces";
 import { Props } from "./interfaces";
 import {
   selectAllSequences,
-  selectAllTools,
   findSequence,
   selectAllToolSlotPointers
 } from "../resources/selectors";
 import { getStepTag } from "../resources/sequence_tagging";
+import { TaggedTool } from "../resources/tagged_resources";
+import { joinKindAndId } from "../resources/reducer";
+import { betterCompact } from "../util";
 
 export function mapStateToProps(props: Everything): Props {
   const uuid = props.resources.consumers.sequences.current;
-  const syncStatus =
-    props.bot.hardware.informational_settings.sync_status || "unknown";
-  const sequence =
-    (uuid) ? findSequence(props.resources.index, uuid) : undefined;
+  const syncStatus = props
+    .bot
+    .hardware
+    .informational_settings
+    .sync_status || "unknown";
+  const sequence = uuid ? findSequence(props.resources.index, uuid) : undefined;
+  sequence && (sequence.body.body || []).map(x => getStepTag(x));
+  const Tool: TaggedTool["kind"] = "Tool";
+  const slots = selectAllToolSlotPointers(props.resources.index);
 
-  if (sequence) {
-    (sequence.body.body || [])
-      .map(x => {
-        getStepTag(x);
-      });
-  }
+  const { byKindAndId, references } = props.resources.index;
+  const tools = betterCompact(slots
+    .map(x => references[byKindAndId[joinKindAndId(Tool, x.body.tool_id)] || ""])
+    .map(tool => (tool && tool.kind === "Tool") ? tool : undefined));
+
   return {
     dispatch: props.dispatch,
     sequences: selectAllSequences(props.resources.index),
-    tools: selectAllTools(props.resources.index),
-    slots: selectAllToolSlotPointers(props.resources.index),
+    tools,
+    slots,
     sequence: sequence,
     auth: props.auth,
     resources: props.resources.index,
