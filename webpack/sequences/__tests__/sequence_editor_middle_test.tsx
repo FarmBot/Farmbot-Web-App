a8773ed475b9300929ff349af8a25cfbdeff586f
Rick Carlino <RickCarlino@users.noreply.github.com>
2017-11-28 18:01:35 -0600

    Restrict move_abs tool selection to active tools only (#544)
    * Debugging webpack config part I
    
    * Debugging webpack config part II
    
    * interim_email claim
    
    * Update `clean` npm task
    
    * ¯\_(ツ)_/¯
    
    * TODO: Move generateList() into mapStateToProps
    
    * :clap: remove deprecated props
    
    * Restrict move_abs to active tools

--- webpack/sequences/__tests__/sequence_editor_middle_test.tsx
+++ webpack/sequences/__tests__/sequence_editor_middle_test.tsx
@@ -8,11 +8,8 @@ import { fakeSequence } from "../../__test_support__/fake_state/resources";
 describe("<SequenceEditorMiddle/>", () => {
   function fakeProps(): SequenceEditorMiddleProps {
     return {
-      slots: [],
       dispatch: jest.fn(),
       sequence: fakeSequence(),
-      sequences: [],
-      tools: [],
       resources: buildResourceIndex(FAKE_RESOURCES).index,
       syncStatus: "synced",
       consistent: true,
--- webpack/sequences/step_tiles/tile_move_absolute/generate_list.ts
+++ webpack/sequences/step_tiles/tile_move_absolute/generate_list.ts
@@ -1,25 +1,34 @@
 import { ResourceIndex } from "../../../resources/interfaces";
 import {
   selectAllPoints,
-  selectAllTools,
-  mapToolIdToName
+  mapToolIdToName,
+  selectAllToolSlotPointers
 } from "../../../resources/selectors";
-import { CowardlyDictionary } from "../../../util";
+import { CowardlyDictionary, betterCompact } from "../../../util";
 import { PointerTypeName } from "../../../interfaces";
 import { PointerType, TaggedTool } from "../../../resources/tagged_resources";
 import { DropDownItem } from "../../../ui/index";
 import { Vector3 } from "farmbot/dist";
 import { TOOL } from "./interfaces";
 import * as _ from "lodash";
+import { joinKindAndId } from "../../../resources/reducer";
+
+export function activeTools(resources: ResourceIndex) {
+  const Tool: TaggedTool["kind"] = "Tool";
+  const slots = selectAllToolSlotPointers(resources);
+
+  const { byKindAndId, references } = resources;
+  return betterCompact(slots
+    .map(x => references[byKindAndId[joinKindAndId(Tool, x.body.tool_id)] || ""])
+    .map(tool => (tool && tool.kind === "Tool") ? tool : undefined));
+}
 
 export function generateList(input: ResourceIndex): DropDownItem[] {
   const toolNameById = mapToolIdToName(input);
   const SORT_KEY: keyof DropDownItem = "headingId";
   const points = selectAllPoints(input)
     .filter(x => (x.body.pointer_type !== "ToolSlot"));
-  const toolDDI: DropDownItem[] = selectAllTools(input)
-    .filter((x: TaggedTool) => !!x.body.id)
-    .map(t => formatTools(t));
+  const toolDDI: DropDownItem[] = activeTools(input).map(t => formatTools(t));
   return _(points)
     .map(formatPoint(toolNameById))
     .sortBy(SORT_KEY)
