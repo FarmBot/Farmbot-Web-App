1f2e7e002fd1ee80c9609360b89b72610b5bb157
Rick Carlino <rick.carlino@gmail.com>
2017-11-30 10:06:02 -0600

    :heavy_check_mark: Ready

--- webpack/sequences/__tests__/test_button_test.tsx
+++ webpack/sequences/__tests__/test_button_test.tsx
@@ -12,7 +12,7 @@ describe("<TestButton/>", () => {
         "name": "Goto 0, 0, 0",
         "color": "gray",
         "body": [],
-        "args": { "version": 4, "label": "WIP" },
+        "args": { "version": 4 },
         "kind": "sequence"
       },
       "uuid": "Sequence.23.47"
