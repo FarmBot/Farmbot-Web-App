bae3b28e40fc00d0ca1161a7e8af82fe526d2872
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2017-12-01 20:32:03 -0800

    FBOS details pop-up and tests

--- webpack/devices/components/hardware_settings/__tests__/motors_test.tsx
+++ webpack/devices/components/hardware_settings/__tests__/motors_test.tsx
@@ -1,10 +1,22 @@
+const mockDevice = {
+  updateMcu: jest.fn(() => { return Promise.resolve(); }),
+};
+jest.mock("../../../../device", () => ({
+  getDevice: () => (mockDevice)
+}));
+
 import * as React from "react";
-import { MotorsProps } from "../interfaces";
-import { bot } from "../../../__test_support__/fake_state/bot";
-import { Motors, StepsPerMmSettings } from "../hardware_settings/motors";
-import { render, shallow } from "enzyme";
+import { MotorsProps } from "../../interfaces";
+import { bot } from "../../../../__test_support__/fake_state/bot";
+import { Motors, StepsPerMmSettings } from "../motors";
+import { render, shallow, mount } from "enzyme";
+import { McuParamName } from "farmbot";
 
 describe("<Motors/>", () => {
+  beforeEach(function () {
+    jest.clearAllMocks();
+  });
+
   it("renders the base case", () => {
     const props: MotorsProps = { dispatch: jest.fn(), bot };
     const el = render(<Motors {...props} />);
@@ -30,6 +42,22 @@ describe("<Motors/>", () => {
     const wrapper = render(<Motors {...props} />);
     expect(wrapper.text()).toContain("Homing Speed");
   });
+
+  function testParamToggle(
+    description: string, parameter: McuParamName, position: number) {
+    it(description, () => {
+      bot.controlPanelState.motors = true;
+      bot.hardware.mcu_params[parameter] = 1;
+      const props: MotorsProps = { dispatch: jest.fn(), bot };
+      const wrapper = mount(<Motors {...props} />);
+      wrapper.find("button").at(position).simulate("click");
+      expect(mockDevice.updateMcu)
+        .toHaveBeenCalledWith({ [parameter]: 0 });
+    });
+  }
+  testParamToggle("toggles retries e-stop parameter", "param_e_stop_on_mov_err", 0);
+  testParamToggle("toggles enable X2", "movement_secondary_motor_x", 7);
+  testParamToggle("toggles invert X2", "movement_secondary_motor_invert_x", 8);
 });
 
 describe("<StepsPerMmSettings/>", () => {
--- webpack/devices/components/hardware_settings/__tests__/space_panel_header_test.tsx
+++ webpack/devices/components/hardware_settings/__tests__/space_panel_header_test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render } from "enzyme";
-import { SpacePanelHeader } from "../hardware_settings/space_panel_header";
+import { SpacePanelHeader } from "../space_panel_header";
 
 describe("<SpacePanelHeader/>", () => {
   it("has children", () => {
