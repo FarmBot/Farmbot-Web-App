88a0d64abb4805b13cfaa257494f7e1552c05e29
Rick Carlino <rick.carlino@gmail.com>
2017-12-06 12:43:46 -0600

    Comment logs service out for now

--- lib/log_service.rb
+++ lib/log_service.rb
@@ -7,11 +7,14 @@ Transport
     # not_legacy = !headers.keys.join(" ").include?("mqtt")
     # if (not_legacy)
       # Extract current user (if version is appropriate)
-      device_id = delivery_info.routing_key.split(".")[1].gsub("device_", "").to_i
-      device    = Device.find(device_id)
-      # Parse payload
-      payload   = JSON.parse(payload)
-      puts "===== INCOMING LOG ====="
-      puts payload
+      # device_id = delivery_info.routing_key.split(".")[1].gsub("device_", "").to_i
+      # device    = Device.find(device_id)
+      # # Parse payload
+      # payload   = JSON.parse(payload)
+
+      # # {"meta"=>{"z"=>0, "y"=>0, "x"=>0, "type"=>"info"}, "message"=>"HQ FarmBot TEST 123 Pin 13 is 0", "created_at"=>1512585641, "channels"=>[]}
+
+      # puts "===== INCOMING LOG ====="
+      # puts payload
     # end
   end
