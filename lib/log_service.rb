4a45e1b2b2c4570786d443dcab11f4922b65a40f
Rick Carlino <rick.carlino@gmail.com>
2017-12-06 12:06:30 -0600

    Early stage log worker: Next- update procfile.

--- lib/log_service.rb
+++ lib/log_service.rb
@@ -1,6 +1,17 @@
-binding.pry
-
-loop do
-  puts "Ding!"
-  sleep 5
-end
+Transport
+  .log_channel
+  .subscribe(block: true) do |delivery_info, properties, payload|
+    # Legacy FBOS still uses the API for log storage.
+    headers    = properties[:headers] || {}
+    # Guess version
+    not_legacy = !headers.keys.join(" ").include?("mqtt")
+    if (not_legacy)
+      # Extract current user (if version is appropriate)
+      device_id = delivery_info.routing_key.split(".")[1].gsub("device_", "").to_i
+      device    = Device.find(device_id)
+      # Parse payload
+      payload   = JSON.parse(payload)
+      puts "===== INCOMING LOG ====="
+      puts payload
+    end
+  end
