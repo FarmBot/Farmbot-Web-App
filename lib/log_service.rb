78b33d7621b403eba01dec014f3d217bf0f61cd6
Rick Carlino <rick.carlino@gmail.com>
2017-12-06 12:31:39 -0600

    :bug:

--- lib/log_service.rb
+++ lib/log_service.rb
@@ -1,11 +1,11 @@
 Transport
   .log_channel
   .subscribe(block: true) do |delivery_info, properties, payload|
-    # Legacy FBOS still uses the API for log storage.
-    headers    = properties[:headers] || {}
-    # Guess version
-    not_legacy = !headers.keys.join(" ").include?("mqtt")
-    if (not_legacy)
+    # # Legacy FBOS still uses the API for log storage.
+    # headers    = properties[:headers] || {}
+    # # Guess version
+    # not_legacy = !headers.keys.join(" ").include?("mqtt")
+    # if (not_legacy)
       # Extract current user (if version is appropriate)
       device_id = delivery_info.routing_key.split(".")[1].gsub("device_", "").to_i
       device    = Device.find(device_id)
@@ -13,5 +13,5 @@ Transport
       payload   = JSON.parse(payload)
       puts "===== INCOMING LOG ====="
       puts payload
-    end
+    # end
   end
