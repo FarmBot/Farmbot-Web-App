98d4706f2e5dfa460badfb45f98a53ed38ed4482
Rick Carlino <rick.carlino@gmail.com>
2017-12-12 10:10:18 -0600

    [UNSTABLE] Tests pass. NEXT: Handle warnings.

--- webpack/__test_support__/setup_enzyme.js
+++ webpack/__test_support__/setup_enzyme.js
@@ -1,3 +1,3 @@
 // enzyme-adapter-react-15 is not important enough to import @types for.
-var Adapter = require('enzyme-adapter-react-15');
+var Adapter = require('enzyme-adapter-react-16');
 require('enzyme').configure({ adapter: new Adapter() });
