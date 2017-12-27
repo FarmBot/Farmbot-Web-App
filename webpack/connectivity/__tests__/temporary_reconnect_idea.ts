5fd908b9f31ad241dae85b00f8c445e868bbedf4
Rick Carlino <rick.carlino@gmail.com>
2017-12-27 14:19:32 -0600

    Tests

--- webpack/connectivity/__tests__/temporary_reconnect_idea.ts
+++ webpack/connectivity/__tests__/temporary_reconnect_idea.ts
@@ -1,3 +1,38 @@
+import { temporaryReconnectIdea } from "../reducer";
+import { ConnectionState, EdgeStatus } from "../interfaces";
+import { success } from "farmbot-toastr";
 describe("temporaryReconnectIdea", () => {
-  it("calls success()");
+  it("calls success() when going from down to up", () => {
+    jest.resetAllMocks();
+    const state: ConnectionState = {
+      ["user.mqtt"]: { state: "down", at: "---" },
+      ["user.api"]: undefined,
+      ["bot.mqtt"]: undefined
+    };
+
+    const action: EdgeStatus = {
+      name: "user.mqtt",
+      status: { state: "up", at: "---" }
+    };
+
+    temporaryReconnectIdea(state, action);
+    expect(success).toHaveBeenCalled();
+  });
+
+  it("skips success() when going from up to down", () => {
+    jest.resetAllMocks();
+    const state: ConnectionState = {
+      ["user.mqtt"]: { state: "up", at: "---" },
+      ["user.api"]: undefined,
+      ["bot.mqtt"]: undefined
+    };
+
+    const action: EdgeStatus = {
+      name: "user.mqtt",
+      status: { state: "down", at: "---" }
+    };
+
+    temporaryReconnectIdea(state, action);
+    expect(success).not.toHaveBeenCalled();
+  });
 });
