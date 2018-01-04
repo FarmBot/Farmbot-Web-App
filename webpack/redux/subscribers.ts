4069bac61da3bd7b3c94688fb9bf2cf9809a76a3
Rick Carlino <rick.carlino@gmail.com>
2018-01-04 10:06:56 -0600

    :heavy_check_mark:

--- webpack/redux/subscribers.ts
+++ webpack/redux/subscribers.ts
@@ -3,6 +3,7 @@ import { Store } from "./interfaces";
 import { EnvName } from "./interfaces";
 import { all } from "../resources/selectors";
 import { SpecialStatus } from "../resources/tagged_resources";
+import { getDevice } from "../device";
 
 function stopThem() { return "You have unsaved work."; }
 function dontStopThem() { }
@@ -22,9 +23,23 @@ interface Subscription {
   env: EnvName;
 }
 
+/** Need to compare last state to current state. Stash old state here: */
+const mqttHistory = { last: { state: "down", at: (new Date()).toISOString() } };
+
 /** To make it easier to manage all things watching the state tree,
  * we keep subscriber functions in this array. */
 export let subscriptions: Subscription[] = [
+  {
+    env: "*",
+    fn: (s) => {
+      const x = s.bot.connectivity["bot.mqtt"];
+      const wasDown = mqttHistory.last.state === "down";
+      if (x && wasDown && x.state === "up") {
+        mqttHistory.last = x;
+        getDevice().readStatus().then(() => console.log("REFETCH"), () => { });
+      }
+    }
+  },
   {
     env: "*",
     fn: dontExitIfBrowserIsOnHold
