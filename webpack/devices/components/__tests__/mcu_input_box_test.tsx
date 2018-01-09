10a93487364e5df97682f93f47cbc1eedcddb25a
Rick Carlino <rick.carlino@gmail.com>
2018-01-09 13:01:27 -0600

    clamping tests

---
+++ webpack/devices/components/__tests__/mcu_input_box_test.tsx
@@ -0,0 +1,25 @@
+jest.mock("farmbot-toastr", () => ({ warning: jest.fn() }));
+
+import { McuInputBox } from "../mcu_input_box";
+import { BotState } from "../../interfaces";
+import { warning } from "farmbot-toastr";
+
+describe("McuInputBox", () => {
+  it("clamps numbers", () => {
+    jest.clearAllMocks();
+    const mib = new McuInputBox({
+      bot: {} as BotState,
+      setting: "encoder_enabled_x",
+      dispatch: jest.fn()
+    });
+    const result = mib.clampInputAndWarn("-1", "short");
+    expect(result).toEqual(0);
+    expect(warning)
+      .toHaveBeenCalledWith("Must be a positive number. Rounding up to 0.");
+    jest.clearAllMocks();
+    expect(() => mib.clampInputAndWarn("QQQ", "short"))
+      .toThrowError("Bad input in mcu_input_box. Impossible?");
+    expect(warning)
+      .toHaveBeenCalledWith("Please enter a number between 0 and 32,000");
+  });
+});
