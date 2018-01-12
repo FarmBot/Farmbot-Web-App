da5be5982c7b610f7ef68326bd7435e9345210b7
Rick Carlino <rick.carlino@gmail.com>
2018-01-12 15:42:12 -0600

    DRY up documentation links and tooltips

---
+++ webpack/ui/__tests__/doc_link_test.ts
@@ -0,0 +1,8 @@
+import { docLink, BASE_URL } from "../doc_link";
+
+describe("docLink", () => {
+  it("creates doc links", () => {
+    expect(docLink()).toEqual(BASE_URL);
+    expect(docLink("farmware")).toEqual(BASE_URL + "farmware");
+  })
+});
