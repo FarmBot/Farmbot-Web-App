5060d1a564f2ef2d83b0a58b0ad31ea057509f5b
Rick Carlino <rick.carlino@gmail.com>
2018-01-14 20:46:12 -0600

    Comment cleanup

--- webpack/connectivity/experiment.ts
+++ webpack/connectivity/experiment.ts
@@ -2,20 +2,20 @@ import { NetworkState } from "./interfaces";
 import { maybeGetDevice } from "../device";
 import { store } from "../redux/store";
 
-// const FRESHEN_STATE_TREE: RpcRequest = {
-//   kind: "rpc_request",
-//   args: { label: "FRESHEN_STATE_TREE" },
-//   body: [{ kind: "read_status", args: {} }]
-// };
-
-const iDontLikeThis: { last: NetworkState } = { last: "down" };
+/** PROBLEM: FarmBotJS does not (currently) have a reliable "reconnect"
+ *           mechanism. If you get disconnected for a long time, the bot state
+ *           will become very stale. This requires some acrobatics on our end.
+ *           The code below will detect changes in connectivity and force a
+ *           re-fetch of the state tree when going from "up => down" or vice
+ *           versa. */
+const lastState: { value: NetworkState } = { value: "down" };
 
 store.subscribe(() => {
-  const { state } =
-    (store.getState().bot.connectivity["user.mqtt"] || { state: undefined });
   const device = maybeGetDevice();
-  if (device && state && state !== iDontLikeThis.last) {
+  const currentState = store.getState().bot.connectivity["user.mqtt"];
+  const state = currentState ? currentState.state : undefined;
+  if (device && state && state !== lastState.value) {
     device.readStatus();
-    iDontLikeThis.last = state;
+    lastState.value = state;
   }
 });
