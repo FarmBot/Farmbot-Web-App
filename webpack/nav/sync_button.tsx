029e70ee14a0894f5aad71a58f90f56628922e34
Rick Carlino <rick.carlino@gmail.com>
2018-01-16 13:28:50 -0700

    bot.mqtt status preempts sync_status

--- webpack/nav/sync_button.tsx
+++ webpack/nav/sync_button.tsx
@@ -29,13 +29,21 @@ export function SyncButton({ user, bot, dispatch, consistent }: NavButtonProps) 
   }
   let { sync_status } = bot.hardware.informational_settings;
   sync_status = sync_status || "unknown";
-  const color = consistent ? (COLOR_MAPPING[sync_status] || "red") : "gray";
-  const text = TEXT_MAPPING[sync_status] || "DISCONNECTED";
+  /** WHY DO WE TRACK ONLINE STATUS IN THE SYNC BUTTON?
+   * When the device is offline, there might be old state floating around.
+   * By checking bot.connectivity["bot.mqtt"] first, we can know if it is safe
+   * to ignore the current sync_status.
+   */
+  const x = bot.connectivity["bot.mqtt"];
+  const online = x && x.state === "up";
+  const color =
+    (consistent && online) ? (COLOR_MAPPING[sync_status] || "red") : "gray";
+  const text = online ? TEXT_MAPPING[sync_status] : undefined;
   return (
     <button
       className={`nav-sync ${color} fb-button`}
       onClick={() => dispatch(sync())}>
-      {text}
+      {text || TEXT_MAPPING.unknown}
     </button>
   );
 }
