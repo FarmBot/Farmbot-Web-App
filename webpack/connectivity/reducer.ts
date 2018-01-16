6183fff3a652ad1361402e3784ddc65ba534a90e
Rick Carlino <rick.carlino@gmail.com>
2018-01-16 12:32:15 -0700

    Minor tweak to RESET_NETWORK

--- webpack/connectivity/reducer.ts
+++ webpack/connectivity/reducer.ts
@@ -1,6 +1,6 @@
 import { generateReducer } from "../redux/generate_reducer";
 import { Actions } from "../constants";
-import { ConnectionState, EdgeStatus, ResourceReady } from "./interfaces";
+import { ConnectionState, EdgeStatus, ResourceReady, ConnectionStatus } from "./interfaces";
 import { computeBestTime } from "./reducer_support";
 
 export const DEFAULT_STATE: ConnectionState = {
@@ -9,17 +9,19 @@ export const DEFAULT_STATE: ConnectionState = {
   "user.api": undefined
 };
 
+const BACKOFF_TIME = 6000;
+
 export let connectivityReducer =
   generateReducer<ConnectionState>(DEFAULT_STATE)
     .add<EdgeStatus>(Actions.NETWORK_EDGE_CHANGE, (s, { payload }) => {
-      // const now = s[payload.name];
-      // if (shouldReplace(payload.status, now)) {
-      s[payload.name] = payload.status;
-      return s;
-      // } else {
-      //   console.log("No, that's outdated.");
-      //   return s;
-      // }
+      const now = s[payload.name];
+      if (shouldReplace(payload.status, now)) {
+        s[payload.name] = payload.status;
+        return s;
+      } else {
+        console.log("Ignoring outdated connectivity report");
+        return s;
+      }
     })
     .add<ResourceReady>(Actions.RESOURCE_READY, (s, a) => {
       const isRelevant = a.payload.name === "devices";
@@ -32,17 +34,23 @@ export let connectivityReducer =
     .add<Actions.RESET_NETWORK>(Actions.RESET_NETWORK, (s, a) => {
       type Keys = (keyof ConnectionState)[];
       const keys: Keys = ["bot.mqtt", "user.mqtt", "user.api"];
-      keys.map(x => (s[x] = undefined));
+      const later = new Date().getTime();
+      keys.map(x => {
+        s[x] = {
+          at: new Date(x === "bot.mqtt" ? later : later + BACKOFF_TIME).toISOString(),
+          state: "down"
+        };
+      });
 
       return s;
     });
 
-// function shouldReplace(incoming: ConnectionStatus, current?: ConnectionStatus) {
-//   if (current) {
-//     const alreadyHave = (new Date(current.at).getTime());
-//     const possibleReplacement = (new Date(incoming.at).getTime());
-//     return possibleReplacement > alreadyHave;
-//   } else {
-//     return true;
-//   }
-// }
+function shouldReplace(incoming: ConnectionStatus, current?: ConnectionStatus) {
+  if (current) {
+    const alreadyHave = (new Date(current.at).getTime());
+    const possibleReplacement = (new Date(incoming.at).getTime());
+    return possibleReplacement > alreadyHave;
+  } else {
+    return true;
+  }
+}
