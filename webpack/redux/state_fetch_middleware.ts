b102176993e8209cd362f91ed5caccecebcddb58
Rick Carlino <rick.carlino@gmail.com>
2018-01-19 08:43:48 -0700

    Type errors

--- webpack/redux/state_fetch_middleware.ts
+++ webpack/redux/state_fetch_middleware.ts
@@ -1,39 +1,9 @@
-import {
-  generateRefreshTrigger
-} from "../connectivity/generate_refresh_trigger";
-import { maybeGetDevice } from "../device";
-import { get } from "lodash";
-import { Edge } from "../connectivity/interfaces";
 import { MiddlewareConfig } from "./middlewares";
 import { Middleware } from "redux";
-import { Actions } from "../constants";
-import { Everything } from "../interfaces";
 
-const maybePingBot = generateRefreshTrigger();
-const expectation: Edge = "bot.mqtt";
+// Leaving this in as a no-op for debugging.
 const stateFetchMiddleware: Middleware =
-  (store) => (next) => (action: any) => {
-    const device = maybeGetDevice();
-    const action_type = action.type;
-    const x = get(action, "payload.name", "?");
-    const s: Everything = store.getState() as any;
-    const botMqtt = s.bot.connectivity["bot.mqtt"];
-    const { sync_status } = s.bot.hardware.informational_settings;
-    const statusDidChange = (action_type === Actions.NETWORK_EDGE_CHANGE
-      && x === expectation);
-    const stillNeedState = (botMqtt && botMqtt.state === "up") && !sync_status;
+  (store) => (next) => (action: any) => next(action);
 
-    if (stillNeedState) {
-      console.log("Were online and dont have a sync status. Let's ping.");
-    }
-
-    if (device && (statusDidChange || stillNeedState)) {
-      maybePingBot(device, s);
-    }
-    return next(action);
-  };
-
-export const stateFetchMiddlewareConfig: MiddlewareConfig = {
-  env: "*",
-  fn: stateFetchMiddleware
-};
+export const stateFetchMiddlewareConfig: MiddlewareConfig =
+  ({ env: "*", fn: stateFetchMiddleware });
