996bef556b67af97999114d60aedc56b650f48a7
Rick Carlino <rick.carlino@gmail.com>
2018-01-20 09:25:22 -0700

    Bring back tests for Peripherals::Edit

--- spec/controllers/api/peripherals/peripherals_update_spec.rb
+++ spec/controllers/api/peripherals/peripherals_update_spec.rb
@@ -1,27 +1,30 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe Api::PeripheralsController do
-#   include Devise::Test::ControllerHelpers
+describe Api::PeripheralsController do
+  include Devise::Test::ControllerHelpers
 
-#   describe '#update' do
-#     let(:user) { FactoryBot.create(:user) }
+  describe '#update' do
+    let(:user) { FactoryBot.create(:user) }
 
-#     it 'allows authorized modification' do
-#       sign_in user
-#       id = FactoryBot.create(:peripheral, device: user.device).id
-#       input = { id: id, pin: 9 }
-#       patch :update, input
-#       expect(response.status).to eq(200)
-#       expect(json[:pin]).to eq(9)
-#     end
+    it 'allows authorized modification' do
+      sign_in user
+      id = FactoryBot.create(:peripheral, device: user.device).id
+      body = { pin: 9 }
+      patch :update, body: body.to_json, params: { id: id, format: :json }
+      expect(response.status).to eq(200)
+      expect(json[:pin]).to eq(9)
+      expect(Peripheral.find(id).reload.pin).to eq(9)
+    end
 
-#     it 'prevents unauthorized modification' do
-#       sign_in user
-#       id = FactoryBot.create(:peripheral).id
-#       input = { id: id, pin: 9 }
-#       patch :update, input
-#       expect(response.status).to eq(403)
-#       expect(json[:error]).to include('Not your Peripheral')
-#     end
-#   end
-# end
+    it 'prevents unauthorized modification' do
+      sign_in user
+      id = FactoryBot.create(:peripheral).id
+      body = { pin: 9 }
+      patch :update,
+        body: body.to_json,
+        params: { id: id, format: :json }
+      expect(response.status).to eq(404)
+      expect(json[:error]).to include('not found')
+    end
+  end
+end
