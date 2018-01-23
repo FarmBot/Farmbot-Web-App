96cafdb85b7c01c0f9fdf90ef2f66ef9f13c0c24
Rick Carlino <rick.carlino@gmail.com>
2018-01-23 15:27:24 -0600

    Needs better tests. WIP

--- webpack/__tests__/revert_to_english_test.ts
+++ webpack/__tests__/revert_to_english_test.ts
@@ -1,5 +1,20 @@
+// jest.mock("i18next", () => {
+//   return { init: jest.fn() };
+// });
+
+jest.mock("../i18n", () => {
+  return { detectLanguage: jest.fn(() => Promise.resolve({ lng: "de" })) };
+});
+
+import { init } from "i18next";
+import { detectLanguage } from "../i18n";
+import { revertToEnglish } from "../revert_to_english";
 describe("revertToEnglish", () => {
   it("calls the appropriate handler with the appropriate config", () => {
+    pending();
     revertToEnglish();
+    expect(detectLanguage).toHaveBeenCalledWith("en");
+    // expect(init).toHaveBeenCalled();
+    // // With(expect.objectContaining({ lng: "en" }), expect.anything());
   });
 });
--- webpack/revert_to_english.ts
+++ webpack/revert_to_english.ts
@@ -1,9 +1,8 @@
 import { detectLanguage } from "./i18n";
-import * as I18n from "i18next";
+import { init } from "i18next";
 
-export function revertToEnglish() {
-  detectLanguage("en").then(x => {
-    x.lng = "en";
-    I18n.init(x, () => { });
-  });
+export async function revertToEnglish() {
+  const x = init;
+  debugger;
+  init(await detectLanguage("en"), () => { });
 }
