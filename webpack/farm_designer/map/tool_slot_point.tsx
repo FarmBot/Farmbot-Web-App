0dfd4aa56d983d38241624f934f9e202a0d68660
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-01-23 20:45:24 -0800

    toolslot direction

--- webpack/farm_designer/map/tool_slot_point.tsx
+++ webpack/farm_designer/map/tool_slot_point.tsx
@@ -4,6 +4,8 @@ import { getXYFromQuadrant } from "./util";
 import { MapTransformProps } from "./interfaces";
 import * as _ from "lodash";
 import { Color } from "../../ui/index";
+import { trim } from "../../util";
+import { ToolPulloutDirection } from "../../interfaces";
 
 export interface TSPProps {
   slot: SlotWithTool;
@@ -21,16 +23,21 @@ export class ToolSlotPoint extends
     hovered: false
   };
 
+  angles = [0, 0, 180, 270, 90];
+
   get slot() { return this.props.slot; }
 
   render() {
-    const { id, x, y } = this.slot.toolSlot.body;
+    const { id, x, y, pullout_direction } = this.slot.toolSlot.body;
     const { quadrant, gridSize } = this.props.mapTransformProps;
     const { qx, qy } = getXYFromQuadrant(x, y, quadrant, gridSize);
     const toolName = this.slot.tool ? this.slot.tool.body.name : "no tool";
     const seedBin = _.includes((toolName || "").toLowerCase(), "seed bin");
     const seedTray = _.includes((toolName || "").toLowerCase(), "seed tray");
     const seedTrayRect = getXYFromQuadrant(x, y, quadrant, gridSize);
+    const labelAnchor = pullout_direction === ToolPulloutDirection.NEGATIVE_X
+      ? "end"
+      : "start";
     return <g id={"toolslot-" + id}>
       <defs>
         <radialGradient id="SeedBinGradient">
@@ -41,7 +48,25 @@ export class ToolSlotPoint extends
           x={0} y={0} width={0.25} height={0.25}>
           <circle cx={6} cy={6} r={5} fill="url(#SeedBinGradient)" />
         </pattern>
+        <g id={"toolbay-slot-" + id}
+          fillOpacity={0.25}
+          fill={Color.mediumGray}>
+          <path d={trim(`M${qx + 50} ${qy + 50}
+          h -150 v -100 h 150 v 15.5
+          a 5 5 0 0 1 -2.5 2.5
+          h -61.5
+          a 35 35 0 0 0 0 64
+          h 61.5
+          a 5 5 0 0 1 2.5 2.5
+          z`)} />
+        </g>
       </defs>
+
+      {pullout_direction &&
+        <use style={{ pointerEvents: "none" }}
+          xlinkHref={"#toolbay-slot-" + id}
+          transform={
+            `rotate(${this.angles[pullout_direction]}, ${qx}, ${qy})`} />}
 
       {seedBin &&
         <g id="seed-bin" key={this.slot.toolSlot.uuid}
@@ -87,7 +112,7 @@ export class ToolSlotPoint extends
           fill={this.state.hovered ? Color.darkGray : Color.mediumGray} />
       }
 
-      <text
+      <text textAnchor={labelAnchor}
         visibility={this.state.hovered ? "visible" : "hidden"}
         x={qx}
         y={qy}
