f3caa88f7368468434e3b10b55bb39921586b2c2
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-01-24 16:33:00 -0800

    update time tests

--- webpack/farm_designer/farm_events/calendar/__tests__/index_test.ts
+++ webpack/farm_designer/farm_events/calendar/__tests__/index_test.ts
@@ -16,10 +16,10 @@ describe("calendar", () => {
     const calendar = new Calendar();
     calendar.insert(occurrence(TIME.MONDAY, fakeFarmEventWithExecutable(), 0));
     calendar.insert(occurrence(TIME.TUESDAY, fakeFarmEventWithExecutable(), 0));
-    expect(calendar.getAll().length).toEqual(2);
-    expect(calendar.value).toBeInstanceOf(Object);
-    expect(calendar.value["061917"]).toBeInstanceOf(Array);
-    expect(calendar.value["062017"]).toBeInstanceOf(Array);
+    expect(calendar.value).toEqual(expect.objectContaining({
+      "061917": expect.any(Array),
+      "062017": expect.any(Array)
+    }));
   });
 
   it("finds by date", () => {
