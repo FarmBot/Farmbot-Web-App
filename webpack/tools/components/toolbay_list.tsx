0e93de6d6596889fa4382f344bff26d5d5b9d764
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-01-25 16:50:17 -0800

    toolbay slot refactoring

--- webpack/tools/components/toolbay_list.tsx
+++ webpack/tools/components/toolbay_list.tsx
@@ -25,7 +25,7 @@ export class ToolBayList extends React.Component<ToolBayListProps, {}> {
           const tool = getToolByToolSlotUUID(slot.uuid);
           const name = (tool && tool.body.name) || "None";
           return <Row key={slot.uuid}>
-            <Col xs={2}>
+            <Col xs={1}>
               <label>{index + 1}</label>
             </Col>
             <Col xs={2}>{slot.body.x}</Col>
