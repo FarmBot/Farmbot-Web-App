3fedfb50cdcd537b8c0c49f3944468e55071f4e7
Rick Carlino <rick.carlino@gmail.com>
2018-01-29 09:47:41 -0600

    Cleanup paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,15 +1,1 @@
-puts "IS THIS THING ON?"
-[
-  # Paperclip::AttachmentAdapter,
-  # Paperclip::DataUriAdapter,
-  # Paperclip::EmptyStringAdapter,
-  # Paperclip::IdentityAdapter,
-  # Paperclip::NilAdapter,
-  # Paperclip::HttpUrlProxyAdapter,
-  # Paperclip::UploadedFileAdapter,
-  # Paperclip::UriAdapter,
-  # Paperclip::FileAdapter,
-  Paperclip::StringioAdapter,
-].map do |klass|
-  Paperclip.io_adapters.register(klass)
-end
+Paperclip.io_adapters.register(Paperclip::StringioAdapter)
