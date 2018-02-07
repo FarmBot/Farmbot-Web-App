2fc420ada4669202f55e74e8880b09b701a7ac53
Rick Carlino <rick.carlino@gmail.com>
2018-02-07 09:42:54 -0600

    Cleanup before removing old secondPass idea

--- app/lib/celery_script/first_pass.rb
+++ app/lib/celery_script/first_pass.rb
@@ -21,7 +21,7 @@ module CeleryScript
     end
 
     def execute
-      flat_ir
+      return flat_ir
         .each do |node|
           # Step 1- instantiate records.
           node[I] = PrimaryNode.create!(kind: node[K], sequence: sequence)
@@ -52,30 +52,6 @@ module CeleryScript
             end
             node
         end
-
-      raise "DO NOT PROCEEEEDEEE"
-      #   .each do |item|
-      #   # Edge nodes are primitive values.
-      #   # We can instantiate all EdgeNodes on the first pass easily.
-      #   edge_nodes = item[:edge_nodes] # No longer used.
-      #     .to_a
-      #     .select do |(key, value)|
-      #       key.to_s.start_with?(L)
-      #     end
-      #     .map do |(kind, value)|
-      #       EdgeNode.new(kind: kind, value: value, sequence: sequence)
-      #     end
-
-      #   # Augment the flat IR array with a special "instance" field that will be
-      #   # needed when we run the second pass (resolve parent/child nodes).
-      #   # `I` represents a `PrimaryNode` that is missing information
-      #   # which can only be resolved after all nodes have been processed.
-      #   instance = PrimaryNode.new(kind:       item[:kind],
-      #                              sequence:   sequence,
-      #                              edge_nodes: edge_nodes)
-      #   item[I] = instance
-      # end
-      # flat_ir
     end
 
 private
