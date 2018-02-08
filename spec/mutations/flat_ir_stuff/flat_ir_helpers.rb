5132a52d2e25b8dba2fc2aa8428b6721a68d1f8e
Rick Carlino <rick.carlino@gmail.com>
2018-02-08 07:52:23 -0600

    Add test to identify `body` linkage bug.

--- spec/mutations/flat_ir_stuff/flat_ir_helpers.rb
+++ spec/mutations/flat_ir_stuff/flat_ir_helpers.rb
@@ -31,42 +31,43 @@ class FlatIrHelpers
       }
     end
 
+    def self.typical_sequence2
+      {
+        kind: 'sequence',
+        args: { locals: { kind: 'scope_declaration', args: {}, body: [] } },
+        body: [
+          { kind: 'take_photo', args: {} },
+          {
+            kind: 'send_message',
+            args: { message: 'test case 1', message_type: 'success' },
+            body: [
+              { kind: 'channel', args: { channel_name: 'toast' } },
+            ],
+          },
+          {
+            kind: '_if',
+            args: {
+              lhs: 'x',
+              op: 'is',
+              rhs: 0,
+              _then: { kind: 'execute', args: { sequence_id: 10 } },
+              _else: { kind: 'nothing', args: {} }
+            }
+          },
+        ]
+      }
+    end
+
     def self.flattened_heap
       slicer = CeleryScript::Slicer.new
-      slicer.run!(typical_sequence)
+      slicer.run!(typical_sequence2)
       slicer.heap_values
     end
 
     def self.fake_first_pass
-      fake_tree = {
-      kind: 'sequence',
-      args: { locals: { kind: 'scope_declaration', args: {}, body: [] } },
-      body: [
-        {
-          kind: 'send_message',
-          args: { message: 'test case 1', message_type: 'success' },
-          body: [
-            { kind: 'channel', args: { channel_name: 'toast' } },
-            { kind: 'channel', args: { channel_name: 'email' } },
-            { kind: 'channel', args: { channel_name: 'espeak' } } # Test this.
-          ],
-        },
-        { kind: 'take_photo', args: {} },
-        {
-          kind: '_if',
-          args: {
-            lhs: 'x',
-            op: 'is',
-            rhs: 0,
-            _then: { kind: 'nothing', args: {} },
-            _else: { kind: 'nothing', args: {} }
-          }
-        },
-      ]
-      }
       sequence      = FactoryBot.create(:sequence)
-      sequence.args = fake_tree[:args]
-      sequence.body = fake_tree[:body]
+      sequence.args = typical_sequence[:args]
+      sequence.body = typical_sequence[:body]
       FirstPass.run!(sequence: sequence)
     end
   end
--- spec/mutations/flat_ir_stuff/slicer_spec.rb
+++ spec/mutations/flat_ir_stuff/slicer_spec.rb
@@ -7,25 +7,44 @@ describe CeleryScript::Slicer do
   next_  = CeleryScript::CSHeap::NEXT
   body   = CeleryScript::CSHeap::BODY
 
+  n = "nothing"
+
   CORRECT_LINKAGE = { # Came from the JS implementation which is known good.
-    0 => { kind => "nothing",           parent => 0             },
-    1 => { kind => "sequence",          parent => 0, body  => 2 },
-    2 => { kind => "move_absolute",     parent => 1, next_ => 3 },
-    3 => { kind => "coordinate",        parent => 2             },
-    4 => { kind => "coordinate",        parent => 2             },
-    5 => { kind => "move_relative",     parent => 2, next_ => 6 },
-    6 => { kind => "write_pin",         parent => 5, next_ => 0 },
-    7 => { kind => "scope_declaration", parent => 1             }
+    "sequence"          => { p: n,              b: "take_photo", n: n },
+    "scope_declaration" => { p: "sequence",     b: n,            n: n, },
+    "take_photo"        => { p: "sequence",     b: n,            n: "send_message", },
+    "send_message"      => { p: "take_photo",   b: "channel",    n: "_if",},
+    "channel"           => { p: "send_message", b: n,            n: n, },
+    "_if"               => { p: "send_message", b: n,            n: n, },
+    "execute"           => { p: "_if",          b: n,            n: n, },
   }
 
   it "has edge cases" do
-    output = CeleryScript::FlatIrHelpers.flattened_heap
-    CORRECT_LINKAGE.to_a.map do |(index, expected)|
-      actual = output[index]
-      expect(actual[kind]).to        eq(expected[kind])
-      expect(actual[parent].to_i).to eq(expected[parent]) if expected[parent]
-      expect(actual[next_].to_i ).to eq(expected[next_])  if expected[next_]
-      expect(actual[body].to_i  ).to eq(expected[body])   if expected[body]
+    heap = CeleryScript::FlatIrHelpers.flattened_heap
+    nothing_node = heap[0]
+    expect(nothing_node[kind]).to         eq(n)
+    expect(nothing_node[body].value).to   eq(0)
+    expect(nothing_node[parent].value).to eq(0)
+    expect(nothing_node[next_].value).to  eq(0)
+    output = heap.index_by { |x| x[kind] }
+    CORRECT_LINKAGE.keys.map do |target_kind|
+        actual      = output[target_kind]
+        expectation = CORRECT_LINKAGE[actual[kind]]
+        expect(actual[kind]).to eq(target_kind)
+
+        parent_addr = output[target_kind][parent].value
+        parent_kind = heap[parent_addr][kind]
+        expect(parent_kind).to eq(expectation[:p])
+
+        next_addr = output[target_kind][next_].value
+        next_kind = heap[next_addr][kind]
+        expect(next_kind).to eq(expectation[:n])
+
+        body_addr = output[target_kind][body].value
+        body_kind = heap[body_addr][kind]
+        failure = "`#{target_kind}` needs a body of `#{expectation[:b]}`. "\
+                  "Got `#{body_kind}`."
+        expect(body_kind).to(eq(expectation[:b]), failure)
     end
   end
 end
