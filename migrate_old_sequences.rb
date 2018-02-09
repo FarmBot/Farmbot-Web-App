954e48af6d5da32b9164e3f689fecb07360f6825
Rick Carlino <rick.carlino@gmail.com>
2018-02-09 10:16:08 -0600

    :heavy_check_mark: Works

--- migrate_old_sequences.rb
+++ migrate_old_sequences.rb
@@ -6,10 +6,10 @@ next_seq = find_next_seq
 
 until next_seq == nil
   t = Time.now
-  puts "Migrating sequence #{next_seq.id}: #{next_seq.name}"
-  Sequence.transaction { CeleryScript::StoreCelery.run!(sequence: sequence) }
-  puts "migrated in #{Time.now - t} seconds"
+  puts "=== Migrating sequence #{next_seq.id}: #{next_seq.name}"
+  Sequence.transaction { CeleryScript::StoreCelery.run!(sequence: next_seq) }
+  puts "=== migrated in #{Time.now - t} seconds"
   next_seq = find_next_seq
 end
 
-puts "ALL DONE!"
+puts "=== ALL DONE!"
