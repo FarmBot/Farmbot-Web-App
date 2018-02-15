2d41a18b60e03821a2844e52ae5509b88b0149d0
Rick Carlino <rick.carlino@gmail.com>
2018-02-15 12:14:27 -0600

    Add legacy `mode` field back to the API to prevent crashing old FBOS

--- app/serializers/peripheral_serializer.rb
+++ app/serializers/peripheral_serializer.rb
@@ -1,3 +1,7 @@
 class PeripheralSerializer < ActiveModel::Serializer
-  attributes :id, :pin, :label
+  attributes :id, :pin, :label, :mode
+
+  def mode
+    -1 # DEPRECATED. Still here for legacy reasons. Don't use it. - RC
+  end
 end
