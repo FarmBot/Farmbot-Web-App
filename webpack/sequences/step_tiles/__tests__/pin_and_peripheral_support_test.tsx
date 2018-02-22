ef695819af89a3780c0c9783ac9640ab32850850
Rick Carlino <rick.carlino@gmail.com>
2018-02-22 09:58:42 -0600

    Tests for `pinDropdowns`

--- webpack/sequences/step_tiles/__tests__/pin_and_peripheral_support_test.tsx
+++ webpack/sequences/step_tiles/__tests__/pin_and_peripheral_support_test.tsx
@@ -1,9 +1,22 @@
-import { buildResourceIndex } from "../../../__test_support__/resource_index_builder";
+import {
+  buildResourceIndex
+} from "../../../__test_support__/resource_index_builder";
+import {
+  pinDropdowns,
+  PIN_RANGE,
+  PIN_HEADING,
+  PinGroupName
+} from "../pin_and_peripheral_support";
+import * as _ from "lodash";
 
 describe("Pin and Peripheral support files", () => {
-
   it("has a list of unnamed pins", () => {
-    pinDropdowns
+    expect(pinDropdowns.length)
+      .toBe(PIN_RANGE.length + 1); // 54 pins plus the header.
+    expect(pinDropdowns[0]).toBe(PIN_HEADING);
+    // Grab all uniq heading IDs- we expect only 1.
+    const values = _(pinDropdowns).tail().map(x => x.headingId).uniq().value();
+    expect(values).toEqual([PinGroupName.pin]);
   });
 
   it("Makes a list of Peripheral drop down selectors", () => {
