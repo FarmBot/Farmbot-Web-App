a4970e5c581c92f4ecfdc104ac7da5951530d36c
Rick Carlino <rick.carlino@gmail.com>
2018-03-02 13:36:28 -0600

    [UNSTABLE] Reduce duplication in selectors.tsx

--- webpack/resources/selector_support.ts
+++ webpack/resources/selector_support.ts
@@ -1,15 +1,42 @@
-// export function indexSequenceById(index: ResourceIndex) {
-//   const output: CowardlyDictionary<TaggedSequence> = {};
-//   const uuids = index.byKind.Sequence;
-//   uuids.map(uuid => {
-//     assertUuid("Sequence", uuid);
-//     const sequence = index.references[uuid];
-//     if (sequence && isTaggedSequence(sequence) && sequence.body.id) {
-//       output[sequence.body.id] = sequence;
-//     }
-//   });
-//   return output;
-// }
+import { TaggedResource } from "./tagged_resources";
+import { CowardlyDictionary } from "../util";
+import { ResourceIndex } from "./interfaces";
+import { assertUuid } from "./selectors";
 
-type IndexLookupDictionary = Dictionary<Resource<"Sequence", Sequence> | undefined>;
-type Indexer = (index: ResourceIndex): IndexLookupDictionary;
+interface IndexLookupDictionary<T extends TaggedResource>
+  extends CowardlyDictionary<T> { }
+
+interface Indexer<T extends TaggedResource> {
+  (index: ResourceIndex): IndexLookupDictionary<T>;
+}
+
+interface MapperFn<T extends TaggedResource> {
+  (item: T): T | undefined;
+}
+
+/** Build a function,
+ *    that returns a function,
+ *      that returns a dictionary,
+ *        that contains TaggedResource of kind T
+ *          that uses the resource's id as the dictionary key.
+ * */
+export const buildIndexer =
+  <T extends TaggedResource>(kind: T["kind"], mapper?: MapperFn<T>): Indexer<T> => {
+    return function (index: ResourceIndex, ) {
+      const noop: MapperFn<T> = (i) => i;
+      const output: CowardlyDictionary<T> = {};
+      const uuids = index.byKind.Sequence;
+      const m = mapper || noop;
+      uuids.map(uuid => {
+        assertUuid(kind, uuid);
+        const resource = index.references[uuid];
+        if (resource
+          && (resource.kind === kind)
+          && resource.body.id
+          && m(resource as T)) {
+          output[resource.body.id] = resource as T;
+        }
+      });
+      return output;
+    };
+  };
