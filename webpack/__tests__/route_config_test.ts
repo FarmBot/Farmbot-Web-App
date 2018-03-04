f6a8fffb828d621f6622998f09445d5a438132e5
Rick Carlino <rick.carlino@gmail.com>
2018-03-04 16:40:26 -0600

    :heavy_check_mark:

--- webpack/__tests__/route_config_test.ts
+++ webpack/__tests__/route_config_test.ts
@@ -2,15 +2,13 @@ jest.mock("fastclick", () => ({ attach: jest.fn() }));
 
 import { topLevelRoutes } from "../route_config";
 
-const cb = jest.fn(() => {
-  console.log("TADA!");
-});
-
 describe("route configs", () => {
   it("generates all of them", async () => {
+    const cb = jest.fn();
     const routes = topLevelRoutes.childRoutes;
     const results = await Promise.all(routes.map(route => route.getComponent(undefined, cb)));
-    console.log("IMPROVE THESE TESTS THEY ARE NOT GOOD RIGHT NOW");
     expect(cb).toHaveBeenCalled();
+    expect(cb).toHaveBeenCalledTimes(routes.length);
+    cb.mock.calls.map(x => expect(!!x[1]).toBeTruthy());
   });
 });
