81636758c7a4eba6cde0ab0790248e2113067d34
Rick Carlino <rick.carlino@gmail.com>
2018-03-04 16:56:10 -0600

    Tests for maybeReplaceDesignerModules

--- webpack/__tests__/route_config_test.ts
+++ webpack/__tests__/route_config_test.ts
@@ -1,14 +1,45 @@
 jest.mock("fastclick", () => ({ attach: jest.fn() }));
 
-import { topLevelRoutes } from "../route_config";
+import {
+  topLevelRoutes,
+  designerRoutes,
+  maybeReplaceDesignerModules
+} from "../route_config";
+import { RouterState, RedirectFunction } from "react-router";
 
-describe("route configs", () => {
-  it("generates all of them", async () => {
-    const cb = jest.fn();
-    const routes = topLevelRoutes.childRoutes;
-    const results = await Promise.all(routes.map(route => route.getComponent(undefined, cb)));
-    expect(cb).toHaveBeenCalled();
-    expect(cb).toHaveBeenCalledTimes(routes.length);
-    cb.mock.calls.map(x => expect(!!x[1]).toBeTruthy());
+async function makeSureTheyAreRoutes(input: typeof topLevelRoutes.childRoutes) {
+  const cb = jest.fn();
+  const results = await Promise
+    .all(input.map(route => route.getComponent(undefined, cb)));
+  expect(cb).toHaveBeenCalled();
+  expect(cb).toHaveBeenCalledTimes(input.length);
+  cb.mock.calls.map(x => expect(!!x[1]).toBeTruthy());
+}
+
+describe("top level routes", () => {
+  it("generates all of them",
+    () => makeSureTheyAreRoutes(topLevelRoutes.childRoutes));
+});
+
+describe("designer routes", () => {
+  it("generates all of them",
+    () => makeSureTheyAreRoutes(designerRoutes.childRoutes));
+});
+
+describe("maybeReplaceDesignerModules", () => {
+  it("does replace the route", () => {
+    const pathname = "/app/designer";
+    const next = { location: { pathname } } as RouterState;
+    const replace = jest.fn() as RedirectFunction;
+    maybeReplaceDesignerModules(next, replace);
+    expect(replace).toHaveBeenCalledWith(`${pathname}/plants`);
+  });
+
+  it("does not replace the route", () => {
+    const pathname = "/app/nope";
+    const next = { location: { pathname } } as RouterState;
+    const replace = jest.fn() as RedirectFunction;
+    maybeReplaceDesignerModules(next, replace);
+    expect(replace).not.toHaveBeenCalled();
   });
 });
