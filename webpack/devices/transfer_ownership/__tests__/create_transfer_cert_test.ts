bd10b04f802efc304095ab38736b2c14f5391d31
Rick Carlino <rick.carlino@gmail.com>
2018-03-12 11:43:01 -0500

    Hotfix 2

--- webpack/devices/transfer_ownership/__tests__/create_transfer_cert_test.ts
+++ webpack/devices/transfer_ownership/__tests__/create_transfer_cert_test.ts
@@ -1,12 +1,14 @@
 import { createTransferCert } from "../create_transfer_cert";
+import { getDevice } from "../../../device";
 
 describe("createTransferCert", () => {
-  it("creats a transfer cert", async () => {
-    // const result = await createTransferCert({
-    //   email: "admin@admin.com",
-    //   password: "password123",
-    //   server: "http://127.0.0.1:3000",
-    //   device: getDevice()
-    // });
+  it("creates a transfer cert", async () => {
+    const p = {
+      email: "admin@admin.com",
+      password: "password123",
+      server: "http://127.0.0.1:3000",
+      device: getDevice()
+    };
+    await createTransferCert(p);
   });
 });
