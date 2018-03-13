219ec078e8b5f724e420168052594ad8c4199824
Rick Carlino <rick.carlino@gmail.com>
2018-03-13 15:50:02 -0500

    Skip errors while migrating

--- migrate_old_sequences.rb
+++ migrate_old_sequences.rb
@@ -17,7 +17,7 @@ def try_stuff(id)
 end
 
 def find_next_seq
-  Sequence.where(migrated_nodes: false).order("created_at").last
+  Sequence.where(migrated_nodes: false).order("updated_at").last
 end
 
 next_seq   = find_next_seq
@@ -25,12 +25,16 @@ count      = 0
 total_time = Time.now
 
 until next_seq == nil
+  begin
   t = Time.now
   count += 1
   puts "=== Migrating sequence #{next_seq.id}: #{next_seq.name}..."
   Sequence.transaction { CeleryScript::StoreCelery.run!(sequence: next_seq) }
   puts "=== migrated in #{Time.now - t} seconds"
   next_seq = find_next_seq
+  rescue => e
+    puts "ERROR MIGRATING SEQUENCE #{next_seq.id}, #{next_seq.name.inspect}. #{e.try(:message)}"
+  end
 end
 
 t2 = Time.now - total_time
