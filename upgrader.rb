bc083a8edb2fb1171ba2fe13cc36a0abf3b59fba
Rick Carlino <rick.carlino@gmail.com>
2018-03-20 07:55:05 -0500

    Downgrade all webpack deps :-\

---
+++ upgrader.rb
@@ -0,0 +1,8 @@
+# Create list of deps
+# Iterate over deps
+# Install latest dep
+# Begin automated checks:
+  # run type checker
+  # run tests
+  # Run build
+    #  Failures? "#{dep} failed to upgrade because it broke #{step}"
