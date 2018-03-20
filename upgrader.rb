a7895dfec6e1cb9e634f81486b3aedb75869e1f1
Rick Carlino <rick.carlino@gmail.com>
2018-03-20 09:14:22 -0500

    :heavy_check_mark: Downgraded

--- upgrader.rb
+++
@@ -1,8 +0,0 @@
-# Create list of deps
-# Iterate over deps
-# Install latest dep
-# Begin automated checks:
-  # run type checker
-  # run tests
-  # Run build
-    #  Failures? "#{dep} failed to upgrade because it broke #{step}"
