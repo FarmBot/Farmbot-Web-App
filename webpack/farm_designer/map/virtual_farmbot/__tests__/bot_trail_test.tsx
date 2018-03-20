fedd40f22d2e10112b0ff95f5795aec98686b065
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-03-19 20:26:09 -0700

    add virtual bot trail setting

--- webpack/farm_designer/map/virtual_farmbot/__tests__/bot_trail_test.tsx
+++ webpack/farm_designer/map/virtual_farmbot/__tests__/bot_trail_test.tsx
@@ -5,13 +5,17 @@ import { BotTrail, BotTrailProps, VirtualTrail } from "../bot_trail";
 describe("<BotTrail/>", () => {
   function fakeProps(): BotTrailProps {
     sessionStorage[VirtualTrail.records] = JSON.stringify([
-      { x: 0, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 },
-      { x: 4, y: 4 }]);
+      { coord: { x: 0, y: 0 }, water: 0 },
+      { coord: { x: 1, y: 1 }, water: 10 },
+      { coord: { x: 2, y: 2 }, water: 0 },
+      { coord: { x: 3, y: 3 }, water: 0 },
+      { coord: { x: 4, y: 4 }, water: 20 }]);
     return {
       position: { x: 0, y: 0, z: 0 },
       mapTransformProps: {
         quadrant: 2, gridSize: { x: 3000, y: 1500 }
-      }
+      },
+      peripherals: []
     };
   }
 
@@ -19,28 +23,28 @@ describe("<BotTrail/>", () => {
     sessionStorage[VirtualTrail.length] = JSON.stringify(5);
     const p = fakeProps();
     p.mapTransformProps.quadrant = 2;
-    const wrapper = shallow(<BotTrail {...p } />);
+    const wrapper = shallow(<BotTrail {...p} />);
     const lines = wrapper.find("#trail").find("line");
     expect(lines.length).toEqual(4);
     expect(lines.first().props()).toEqual({
       id: "trail-line-1",
       stroke: "red",
       strokeOpacity: 0.25,
-      strokeWidth: 0.5,
+      strokeWidth: 1.5,
       x1: 2, x2: 1, y1: 2, y2: 1
     });
     expect(lines.last().props()).toEqual({
       id: "trail-line-4",
       stroke: "red",
       strokeOpacity: 1,
-      strokeWidth: 2,
+      strokeWidth: 3,
       x1: 0, x2: 4, y1: 0, y2: 4
     });
   });
 
   it("shows default length trail", () => {
     sessionStorage[VirtualTrail.length] = undefined;
-    const wrapper = shallow(<BotTrail {...fakeProps() } />);
+    const wrapper = shallow(<BotTrail {...fakeProps()} />);
     const lines = wrapper.find("#trail").find("line");
     expect(lines.length).toEqual(5);
   });
@@ -49,9 +53,24 @@ describe("<BotTrail/>", () => {
     sessionStorage[VirtualTrail.length] = undefined;
     const p = fakeProps();
     p.position = { x: 4, y: 4, z: 0 };
-    const wrapper = shallow(<BotTrail {...p } />);
+    const wrapper = shallow(<BotTrail {...p} />);
     const lines = wrapper.find("#trail").find("line");
     expect(lines.length).toEqual(4);
+  });
+
+  it("shows water", () => {
+    const wrapper = shallow(<BotTrail {...fakeProps()} />);
+    const circles = wrapper.find("#trail").find("circle");
+    expect(circles.length).toEqual(2);
+  });
+
+  it("updates water circle size", () => {
+    const p = fakeProps();
+    p.position = { x: 4, y: 4, z: 0 };
+    p.peripherals = [{ label: "water", value: true }];
+    const wrapper = shallow(<BotTrail {...p} />);
+    const water = wrapper.find("#trail").find("circle").last();
+    expect(water.props().r).toEqual(21);
   });
 
 });
