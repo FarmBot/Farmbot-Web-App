a1b19d579d885b50b62e225aa82ddc81ce9cd952
Rick Carlino <rick.carlino@gmail.com>
2018-03-23 09:49:44 -0500

    Tests for the in_use flag

--- spec/factories/farm_event.rb
+++ spec/factories/farm_event.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :farm_event do
     start_time { Date.yesterday - [*(1..5)].sample.days }
-    end_time { Date.today + 1.minute + ([*(1..5)].sample).days }
-    time_unit { FarmEvent::UNITS_OF_TIME.sample }
+    end_time   { Date.today + 1.minute + ([*(1..5)].sample).days }
+    time_unit  { FarmEvent::UNITS_OF_TIME.sample }
     repeat { [*(1..5)].sample }
     # device
     after(:build) do |s|
