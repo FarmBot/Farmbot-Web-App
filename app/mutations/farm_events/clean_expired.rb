68d934b6ede1937fc8f7598dee47bd88b76ce963
Rick Carlino <rick.carlino@gmail.com>
2018-03-28 07:04:47 -0500

    updates to "Clean Expired"

--- app/mutations/farm_events/clean_expired.rb
+++ app/mutations/farm_events/clean_expired.rb
@@ -7,31 +7,37 @@ module FarmEvents
     end
 
     def execute
-      device
-        .farm_events
-        .where.not(executable_type: "Regimen")
-        .where('end_time < ?', DateTime.now)
-        .map(&:destroy!)
-      temporary_hack
+      if device.timezone
+        device
+          .farm_events
+          .where
+          .not(executable_type: "Regimen")
+          .where('end_time < ?', DateTime.now)
+          .map(&:destroy!)
+        temporary_hack
+      end
     end
 
   private
 
-    # Temporary hack to join farm event,
-    # regimen and the decision to destroy it.
+    def timezone
+      @timezone ||= device.timezone or raise "Set Timezone Please"
+    end
+
+    def now
+      @now ||= DateTime.now.in_time_zone(timezone)
+    end
+
     Info = Struct.new(:fe, :reg, :should_destroy?)
 
     def temporary_hack
-      # Delete all farmevents
-      # where executable is a regimen
-      # and farmevent.executable.regimen_items.max is > the current time.
       device
         .farm_events
         .where(executable_type: "Regimen")
         .map do |x|
           reg_items      = x.executable.regimen_items || RegimenItem.none
           max_offset     = (reg_items.pluck(:time_offset).max || 0)
-          should_destroy = ((x.end_time + (max_offset / 1000)) < DateTime.now)
+          should_destroy = ((x.end_time.in_time_zone(timezone).midnight + (max_offset / 1000)) < now)
           Info.new(x, x.executable, should_destroy)
         end
         .select { |x| x.should_destroy? }
