0a6a74b067713cc1c66cf85bf5b44ea06036dd3a
Rick Carlino <rick.carlino@gmail.com>
2018-03-28 14:29:16 -0500

    Broadcast factory reset on password change :heavy_check_mark:

--- app/jobs/send_factory_reset_job.rb
+++ app/jobs/send_factory_reset_job.rb
@@ -1,12 +1,14 @@
 class SendFactoryResetJob < ApplicationJob
   queue_as :default
 
-  def perform(device)
-    payload = {
-      kind: "rpc_request",
-      args: { label: "FROM_API.#{SecureRandom.hex}" },
-      body: [ { kind: "factory_reset", args: { package: "farmbot_os" } } ]
-    }
-    Transport.amqp_send(payload.to_json, device.id, "from_clients")
+  def self.rpc_payload(device)
+    { kind: "rpc_request",
+      args: { label: "FROM_API" },
+      body: [ { kind: "factory_reset", args: { package: "farmbot_os" } } ] }
+  end
+
+  def perform(device, transport = Transport)
+    payl = SendFactoryResetJob.rpc_payload(device)
+    transport.amqp_send(payl.to_json, device.id, "from_clients")
   end
 end
--- spec/jobs/send_factory_reset_job_spec.rb
+++ spec/jobs/send_factory_reset_job_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe SendFactoryResetJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:device) { FactoryBot.create(:device) }
+
+  it 'sends a factory_reset RPC' do
+    dbl = double("fake transport layer")
+    payl = SendFactoryResetJob.rpc_payload(device)
+    expect(dbl)
+      .to receive(:amqp_send).with(payl.to_json, device.id, "from_clients")
+    SendFactoryResetJob.new.perform(device, dbl)
+  end
 end
