57d7b10000ff839f283005187b7421a841e88e79
Rick Carlino <rick.carlino@gmail.com>
2018-04-04 14:02:14 -0500

    🚑

--- db/migrate/20180330143232_migrate_away_from_meta_column.rb
+++ db/migrate/20180330143232_migrate_away_from_meta_column.rb
@@ -1,8 +1,8 @@
 class MigrateAwayFromMetaColumn < ActiveRecord::Migration[5.1]
   def up
-    Device.all.map do |d|
-      d.trim_log_list!(2)
-    end
+    # Device.all.map do |d|
+    #   d.trim_log_list!(2)
+    # end
   end
 
   def down
