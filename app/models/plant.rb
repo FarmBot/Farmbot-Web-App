75eb94c099f07adbbc07965341c64c798fff803d
Rick Carlino <rick.carlino@gmail.com>
2018-04-10 10:17:25 -0500

    Debug staging env

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,12 +3,11 @@ class Plant < Point
   DEFAULT_ICON = "/app-resources/img/icons/generic-plant.svg"
   def do_migrate
     puts "MIGRATING PLANT #{self.id}"
-    Plant.transaction do
-      legacy = LegacyPlant.find(self[:pointer_id])
-      self.update_attributes!(migrated_at:   Time.now,
-                              openfarm_slug: legacy.openfarm_slug,
-                              plant_stage:   legacy.plant_stage,
-                              pointer_type:  "Plant")
-    end
+    legacy = LegacyPlant.find(self[:pointer_id])
+    self.assign_attributes(migrated_at:   Time.now,
+                           openfarm_slug: legacy.openfarm_slug,
+                           plant_stage:   legacy.plant_stage,
+                           pointer_type:  "Plant")
+    self
   end
 end
