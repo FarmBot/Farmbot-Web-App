a5c959c2707b75df9276cb7443580cd38937994e
Rick Carlino <rick.carlino@gmail.com>
2018-04-30 11:27:20 -0500

    Change defaults to 0

--- db/migrate/20180430161447_april_parameter_additions.rb
+++ db/migrate/20180430161447_april_parameter_additions.rb
@@ -1,9 +1,8 @@
 class AprilParameterAdditions < ActiveRecord::Migration[5.1]
-  NEW_STUFF = {
-    movement_invert_2_endpoints_x: 75,
-    movement_invert_2_endpoints_y: 76,
-    movement_invert_2_endpoints_z: 77,
-  }
+  NEW_STUFF = { movement_invert_2_endpoints_x: 0,
+                movement_invert_2_endpoints_y: 0,
+                movement_invert_2_endpoints_z: 0 }
+
   def change
     NEW_STUFF.map do |(name, default)|
       add_column :firmware_configs, name, :integer, default: default
