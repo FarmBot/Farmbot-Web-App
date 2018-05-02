5d314b61ccb5e3b6a2d432845215740d2173232a
Rick Carlino <rick.carlino@gmail.com>
2018-05-02 14:33:08 -0500

    Fix query in ToolsController

--- app/mutations/devices/dump.rb
+++ app/mutations/devices/dump.rb
@@ -3,18 +3,26 @@ module Devices
     RESOURCES = [ :device_configs, :farm_events, :images, :logs, :peripherals,
                   :pin_bindings, :plant_templates, :points, :regimens,
                   :saved_gardens, :sensor_readings, :sensors, :sequences,
-                  :token_issuances, :tools, :users, :webcam_feeds ]
+                  :token_issuances, :users, :webcam_feeds ]
 
     required { model :device, class: Device }
 
     def execute
-      output = { device: device.body_as_json }
       RESOURCES.each do |name|
         model        = device.send(name)
         output[name] = \
           model.try(:map) { |x| x.body_as_json } || x.body_as_json
       end
       output
+    end
+
+private
+
+    def output
+      @output ||= {
+        device: device.body_as_json,
+        tools:  Tool.outter_join_slots(device.id).map(&:body_as_json)
+      }
     end
   end
 end
