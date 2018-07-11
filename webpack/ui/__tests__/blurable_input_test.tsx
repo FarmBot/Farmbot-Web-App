b5a4d79cd3e7cd84780facb5a4780b3f2fa91388
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-07-10 20:05:06 -0700

    check BlurableInput min and max

---
+++ webpack/ui/__tests__/blurable_input_test.tsx
@@ -0,0 +1,54 @@
+jest.mock("farmbot-toastr", () => ({ error: jest.fn() }));
+
+import * as React from "react";
+import { shallow } from "enzyme";
+import { BlurableInput, BIProps } from "../blurable_input";
+import { error } from "farmbot-toastr";
+
+describe("<BlurableInput />", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const fakeProps = (): BIProps => {
+    return {
+      onCommit: jest.fn(),
+      value: "",
+    };
+  };
+
+  it("focuses", () => {
+    const p = fakeProps();
+    p.value = "1";
+    const wrapper = shallow<BlurableInput>(<BlurableInput {...p} />);
+    wrapper.find("input").simulate("focus");
+    expect(wrapper.instance().state.buffer).toEqual("1");
+    expect(wrapper.instance().state.isEditing).toEqual(true);
+  });
+
+  it("out of bounds: under", () => {
+    const p = fakeProps();
+    p.type = "number";
+    p.min = 0;
+    const wrapper = shallow<BlurableInput>(<BlurableInput {...p} />);
+    wrapper.setState({ buffer: "-100" });
+    wrapper.find("input").simulate("submit");
+    expect(wrapper.instance().state.buffer).toEqual("");
+    expect(p.onCommit).not.toHaveBeenCalled();
+    expect(error).toHaveBeenCalledWith(
+      "Value must be greater than or equal to 0.");
+  });
+
+  it("out of bounds: over", () => {
+    const p = fakeProps();
+    p.type = "number";
+    p.max = 100;
+    const wrapper = shallow<BlurableInput>(<BlurableInput {...p} />);
+    wrapper.setState({ buffer: "101" });
+    wrapper.find("input").simulate("submit");
+    expect(wrapper.instance().state.buffer).toEqual("");
+    expect(p.onCommit).not.toHaveBeenCalled();
+    expect(error).toHaveBeenCalledWith(
+      "Value must be less than or equal to 100.");
+  });
+});
