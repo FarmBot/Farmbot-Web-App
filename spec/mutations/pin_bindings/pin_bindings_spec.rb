41a5e1ac95ba5f045d4fcd7c2c2e37d487643ea9
Rick Carlino <rick.carlino@gmail.com>
2018-07-13 16:17:50 -0500

    [UNSTABLE] Tests for mutual exclusivity

---
+++ spec/mutations/pin_bindings/pin_bindings_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+module PinBindingSpecHelper
+  def self.test(mutation, has_seq, has_actn, expected_result)
+    # integer :pin_num
+    # integer :sequence_id
+    # model   :device, class: Device
+    # model   :pin_binding, class: PinBinding
+    actn                    = PinBinding.special_actions.values.sample
+    sequence                = Sequence.last
+    device                  = sequence.device
+    params                  = { pin_num:     12,
+                                device:      device,
+                                pin_binding: PinBinding.last }
+    params[:special_action] = actn        if has_actn
+    params[:sequence_id]    = sequence.id if has_seq
+    mut = (mutation == :create) ? PinBindings::Create : PinBindings::Update
+    result = mut.run(params).success?
+    raise "NO NO NO" if expected_result != result
+  end
+end
+
+describe "Pin Binding updates" do
+  it "enforces mutual exclusivity" do
+    Sequence.destroy_all
+    Device.destroy_all
+    device = FactoryBot.create(:device)
+    PinBinding.create!(device: device)
+    Sequence.create!(device: device, name: "test")
+    PinBindingSpecHelper.test(:create, false, false, false)
+    PinBindingSpecHelper.test(:create, false, true,  true )
+    PinBindingSpecHelper.test(:create, true,  false, true )
+    PinBindingSpecHelper.test(:create, true,  true,  false)
+    PinBindingSpecHelper.test(:update, false, false, true )
+    PinBindingSpecHelper.test(:update, false, true,  true )
+    PinBindingSpecHelper.test(:update, true,  false, true )
+    PinBindingSpecHelper.test(:update, true,  true,  false)
+  end
+end
