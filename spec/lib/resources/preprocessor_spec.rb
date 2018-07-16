f5603b49eef2a0dbb276df5764621a9b427cd4bf
Rick Carlino <rick.carlino@gmail.com>
2018-07-16 12:08:32 -0500

    JSON format error specs

--- spec/lib/resources/preprocessor_spec.rb
+++ spec/lib/resources/preprocessor_spec.rb
@@ -11,14 +11,16 @@ describe Resources::PreProcessor do
       "%{id}",
       "%{uuid}",
     ].join(".")
-
-  it "converts string types to real types" do
-    pb     = FactoryBot.create(:pin_binding)
-    props  = { device_id: pb.device.id,
+  let(:pb) { FactoryBot.create(:pin_binding) }
+  let(:props) do
+    { device_id: pb.device.id,
                action:    "destroy",
                klass:     pb.class,
                id:        pb.id,
                uuid:      SecureRandom.uuid }
+  end
+
+  it "converts string types to real types" do
     body   = {}.to_json
     chan   = CHANNEL_TPL % props
     result = Resources::PreProcessor.from_amqp(DeliveryInfoShim.new(chan), body)
@@ -28,5 +30,13 @@ describe Resources::PreProcessor do
     expect(result[:resource]).to    eq(PinBinding)
     expect(result[:resource_id]).to eq(pb.id)
     expect(result[:uuid]).to        eq(props[:uuid])
+  end
+
+  it "handles bad JSON" do
+    body   = "}{"
+    chan   = CHANNEL_TPL % props
+    expect do
+      Resources::PreProcessor.from_amqp(DeliveryInfoShim.new(chan), body)
+    end.to raise_error(Mutations::ValidationException, "body must be a JSON object")
   end
 end
