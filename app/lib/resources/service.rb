89caf89a361477ee72ee3e2cc19a75892eb86440
Rick Carlino <rick.carlino@gmail.com>
2018-07-18 11:45:27 -0500

    Sprinkles of log-y goodness

--- app/lib/resources/service.rb
+++ app/lib/resources/service.rb
@@ -2,12 +2,14 @@ module Resources
   class Service
     def self.process(delivery_info, body)
       params = PreProcessor.from_amqp(delivery_info, body)
+      puts params
       result = Job.run!(params)
       payl   = result ? result.to_json : ""
       Transport
         .current
         .amqp_send(payl, params[:device].id, "from_api")
     rescue Mutations::ValidationException => q
+      Rollbar.error(q)
       params  ||= {}
       raw_chan  = delivery_info&.routing_key&.split(".") || []
       device_id =
