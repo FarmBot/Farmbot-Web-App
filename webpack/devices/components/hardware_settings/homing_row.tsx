a1d61e70e9e05abcd167ee8dd953bedc9faa5afa
Rick Carlino <rick.carlino@gmail.com>
2018-08-02 13:56:21 -0500

    HOME {{axis}} => FIND HOME {{axis}}

--- webpack/devices/components/hardware_settings/homing_row.tsx
+++ webpack/devices/components/hardware_settings/homing_row.tsx
@@ -33,7 +33,7 @@ export function HomingRow(props: HomingRowProps) {
           <LockableButton
             disabled={disabled || botDisconnected}
             onClick={() => findHome(axis)}>
-            {t("HOME {{axis}}", { axis })}
+            {t("FINE HOME {{axis}}", { axis })}
           </LockableButton>
         </Col>;
       })}
