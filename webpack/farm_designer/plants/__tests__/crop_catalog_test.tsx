639d82d7c0e3880c509c0c8307c63540e2352d2d
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-08-01 19:03:16 -0700

    misc tests

--- webpack/farm_designer/plants/__tests__/crop_catalog_test.tsx
+++ webpack/farm_designer/plants/__tests__/crop_catalog_test.tsx
@@ -2,20 +2,47 @@ jest.mock("react-redux", () => ({
   connect: jest.fn()
 }));
 
+jest.mock("../../../history", () => ({ history: { push: jest.fn() } }));
+
 import * as React from "react";
 import { CropCatalog } from "../crop_catalog";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
+import { CropCatalogProps } from "../../interfaces";
+import { Actions } from "../../../constants";
+import { history } from "../../../history";
 
 describe("<CropCatalog />", () => {
+  const fakeProps = (): CropCatalogProps => {
+    return {
+      dispatch: jest.fn(),
+      OFSearch: jest.fn(),
+      cropSearchResults: [],
+      cropSearchQuery: "",
+    };
+  };
+
   it("renders", () => {
-    const wrapper = mount(
-      <CropCatalog
-        OFSearch={jest.fn()}
-        dispatch={jest.fn()}
-        cropSearchResults={[]}
-        cropSearchQuery={""} />);
+    const wrapper = mount(<CropCatalog {...fakeProps()} />);
     expect(wrapper.text()).toContain("Choose a crop");
     expect(wrapper.find("input").props().placeholder)
       .toEqual("Search OpenFarm...");
+  });
+
+  it("handles search term change", () => {
+    const p = fakeProps();
+    const wrapper = shallow(<CropCatalog {...p} />);
+    wrapper.find("input").first().simulate("change", {
+      currentTarget: { value: "apple" }
+    });
+    expect(p.dispatch).toHaveBeenCalledWith({
+      payload: "apple",
+      type: Actions.SEARCH_QUERY_CHANGE
+    });
+  });
+
+  it("goes back", () => {
+    const wrapper = mount(<CropCatalog {...fakeProps()} />);
+    wrapper.find("i").first().simulate("click");
+    expect(history.push).toHaveBeenCalledWith("/app/designer/plants");
   });
 });
