9078687b2479534dfa32506fb90c74995a6d6ee5
Rick Carlino <rick.carlino@gmail.com>
2018-08-07 09:52:28 -0500

    Adding a `.delay` fixes cascade issues

--- app/models/edge_node.rb
+++ app/models/edge_node.rb
@@ -13,13 +13,25 @@ class EdgeNode < ApplicationRecord
     false
   end
 
-  after_save :maybe_cascade_changes, on: [:create, :update, :destroy]
+  def is_sequence_id?
+    kind == "sequence_id"
+  end
+
+  after_save :maybe_cascade_changes
+
   def maybe_cascade_changes
-    is_sid = kind == "sequence_id"
     (the_changes["value"] || [])
-        .compact
-        .map { |x| Sequence.find_by(id: x) }
-        .compact
-        .map { |x| x.broadcast!(Transport.current.cascade_id) } if is_sid
+      .compact
+      .uniq
+      .reject { |x| x == sequence_id } # 🤯 Skip recursive nodes
+      .map { |x| Sequence.find_by(id: x) }
+      .compact
+      .map { |x| x.delay.broadcast!(Transport.current.cascade_id) } if is_sequence_id?
+  end
+
+  after_destroy :hmm
+
+  def hmm
+    Sequence.find(self.value).delay.broadcast! if is_sequence_id?
   end
 end
