836a9fb145533fcb3ee4b45e81e310fd43710cdb
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-08-07 21:06:49 -0700

    remove unused auth code

--- webpack/auth/actions.ts
+++ webpack/auth/actions.ts
@@ -1,16 +1,11 @@
-import axios, { AxiosResponse } from "axios";
-import { t } from "i18next";
-import { error, success } from "farmbot-toastr";
+import axios from "axios";
 import {
   fetchReleases, fetchMinOsFeatureData, FEATURE_MIN_VERSIONS_URL
 } from "../devices/actions";
-import { push } from "../history";
 import { AuthState } from "./interfaces";
-import { ReduxAction, Thunk } from "../redux/interfaces";
+import { ReduxAction } from "../redux/interfaces";
 import * as Sync from "../sync/actions";
 import { API } from "../api";
-import { Session } from "../session";
-import { UnsafeError } from "../interfaces";
 import {
   responseFulfilled,
   responseRejected,
@@ -18,7 +13,6 @@ import {
 } from "../interceptors";
 import { Actions } from "../constants";
 import { connectDevice } from "../connectivity/connect_device";
-import { toastErrors } from "../toast_errors";
 import { getFirstPartyFarmwareList } from "../farmware/actions";
 
 export function didLogin(authState: AuthState, dispatch: Function) {
@@ -34,30 +28,6 @@ export function didLogin(authState: AuthState, dispatch: Function) {
   dispatch(connectDevice(authState));
 }
 
-// We need to handle OK logins for numerous use cases (Ex: login & registration)
-export function onLogin(dispatch: Function) {
-  return (response: AxiosResponse<AuthState>) => {
-    const { data } = response;
-    Session.replaceToken(data);
-    didLogin(data, dispatch);
-    push("/app/controls");
-  };
-}
-
-export function login(username: string, password: string, url: string): Thunk {
-  return dispatch => {
-    return requestToken(username, password, url).then(
-      onLogin(dispatch),
-      () => dispatch(loginErr())
-    );
-  };
-}
-
-export function loginErr() {
-  error(t("Login failed."));
-  return { type: Actions.LOGIN_ERROR };
-}
-
 /** Very important. Once called, all outbound HTTP requests will
  * have a JSON Web Token attached to their "Authorization" header,
  * thereby granting access to the API. */
@@ -68,61 +38,5 @@ export function setToken(auth: AuthState): ReduxAction<AuthState> {
   return {
     type: Actions.REPLACE_TOKEN,
     payload: auth
-  };
-}
-
-/** Sign up for the FarmBot service over AJAX. */
-export function register(name: string,
-  email: string,
-  password: string,
-  confirmation: string): Thunk {
-  return dispatch => {
-    return requestRegistration(name, email, password, confirmation)
-      .then(onLogin(dispatch), onRegistrationErr(dispatch));
-  };
-}
-
-/** Handle user registration errors. */
-export function onRegistrationErr(_: Function) {
-  return (err: UnsafeError) => toastErrors(err);
-}
-
-/** Build a JSON object in preparation for an HTTP POST
- *  to registration endpoint */
-export function requestRegistration(name: string,
-  email: string,
-  password: string,
-  password_confirmation: string) {
-
-  const form = { user: { email, password, password_confirmation, name } };
-  return axios.post(API.current.usersPath, form);
-}
-
-/** Fetch API token if already registered. */
-export function requestToken(email: string,
-  password: string,
-  url: string) {
-  const payload = { user: { email: email, password: password } };
-  // Set the base URL once here.
-  // It will get set once more when we get the "iss" claim from the JWT.
-  API.setBaseUrl(url);
-  return axios.post(API.current.tokensPath, payload);
-}
-
-export function logout() {
-  // When logging out, we pop up a toast message to confirm logout.
-  // Sometimes, LOGOUT is dispatched when the user is already logged out.
-  // In those cases, seeing a logout message may confuse the user.
-  // To circumvent this, we must check if the user had a token.
-  // If there was infact a token, we can safely show the message.
-  if (Session.fetchStoredToken()) {
-    success(t("You have been logged out."));
-  }
-
-  Session.clear();
-  // Technically this is unreachable code:
-  return {
-    type: Actions.LOGOUT,
-    payload: {}
   };
 }
