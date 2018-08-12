6409a246499726ab52a35719e363b03261ce8e49
Rick Carlino <rick.carlino@gmail.com>
2018-08-12 10:58:25 -0500

    [FAILING] Validate `point_id` in `EdgeNode`

--- spec/controllers/api/sequences/sequences_update_spec.rb
+++ spec/controllers/api/sequences/sequences_update_spec.rb
@@ -7,6 +7,36 @@ describe Api::SequencesController do
   describe '#update' do
 
     let(:user) { FactoryBot.create(:user) }
+    it 'does not let you use other peoples point resources' do
+      sign_in user
+      sequence  = FakeSequence.create( device: user.device)
+      not_yours = FactoryBot.create(:plant)
+      expect(not_yours.device_id).to_not eq(user.device_id)
+      input = {
+        id: sequence.id,
+        sequence: {
+          name: "Wrong `locals` declaration",
+          args: { locals: { kind: "scope_declaration", args: {} } },
+          body: [
+            {
+              kind: "move_absolute",
+              args: {
+                location: {
+                    kind: "point",
+                    args: { pointer_type: "Plant", pointer_id: not_yours.id }
+                  },
+                  speed: 100,
+                  offset: { kind: "coordinate", args: { x: 0, y: 0, z: 0 } }
+                }
+              }
+          ],
+        }
+      }
+      patch :update, params: {id: sequence.id }, body: input.to_json, as: :json
+      binding.pry
+      expect(response.status).to eq(422)
+      expect(json[:error]).to include("Not your point")
+    end
 
     it 'doesnt allow nonsense in `sequence.args.locals`' do
       sign_in user
