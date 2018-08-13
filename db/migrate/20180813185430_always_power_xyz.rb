1176eb14613d366d78726bf48160078ccb3f1ab1
Rick Carlino <rick.carlino@gmail.com>
2018-08-13 14:19:25 -0500

    Enable encoders by default

--- db/migrate/20180813185430_always_power_xyz.rb
+++ db/migrate/20180813185430_always_power_xyz.rb
@@ -2,6 +2,9 @@ class AlwaysPowerXyz < ActiveRecord::Migration[5.2]
   def change
     fc = :firmware_configs
 
+    change_column fc, :encoder_enabled_x,      :integer, default: 1
+    change_column fc, :encoder_enabled_y,      :integer, default: 1
+    change_column fc, :encoder_enabled_z,      :integer, default: 1
     change_column fc, :movement_keep_active_x, :integer, default: 1
     change_column fc, :movement_keep_active_y, :integer, default: 1
     change_column fc, :movement_keep_active_z, :integer, default: 1
