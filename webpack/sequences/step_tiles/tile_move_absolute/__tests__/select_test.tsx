85d13547e36710f3a690bacafc6cbf93dd304979
Rick Carlino <rick.carlino@gmail.com>
2018-08-14 16:15:18 -0500

    Type errors

--- webpack/sequences/step_tiles/tile_move_absolute/__tests__/select_test.tsx
+++ webpack/sequences/step_tiles/tile_move_absolute/__tests__/select_test.tsx
@@ -6,25 +6,24 @@ jest.mock("../format_selected_dropdown", () => {
 
 import * as React from "react";
 import { shallow } from "enzyme";
-import { fakeSequence } from "../../../../__test_support__/fake_state/resources";
 import { buildResourceIndex } from "../../../../__test_support__/resource_index_builder";
-import { SequenceBodyItem } from "farmbot";
 import { TileMoveAbsSelect } from "../select";
 import { TileMoveAbsProps } from "../interfaces";
-import { formatSelectedDropdown } from "../format_selected_dropdown";
 
-function fakeSequenceStepProps(currentStep: SequenceBodyItem) {
-  const currentSequence = fakeSequence();
-  currentSequence.body.body = [currentStep];
+// import { fakeSequence } from "../../../../__test_support__/fake_state/resources";
+// import { SequenceBodyItem } from "farmbot";
+// function fakeSequenceStepProps(currentStep: SequenceBodyItem) {
+//   const currentSequence = fakeSequence();
+//   currentSequence.body.body = [currentStep];
 
-  return {
-    resources: buildResourceIndex([currentSequence]).index,
-    currentSequence,
-    currentStep,
-    dispatch: jest.fn(),
-    index: 0
-  };
-}
+//   return {
+//     resources: buildResourceIndex([currentSequence]).index,
+//     currentSequence,
+//     currentStep,
+//     dispatch: jest.fn(),
+//     index: 0
+//   };
+// }
 
 describe("<TileMoveAbsSelect/>", () => {
   function fakeProps(): TileMoveAbsProps {
