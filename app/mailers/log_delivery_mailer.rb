5e2992251dc6c477efde412ce81ef907ab054083
Rick Carlino <rick.carlino@gmail.com>
2018-08-16 10:57:55 -0500

    WIP

--- app/mailers/log_delivery_mailer.rb
+++ app/mailers/log_delivery_mailer.rb
@@ -1,29 +1,29 @@
 class LogDeliveryMailer < ApplicationMailer
   WHOAH = "Device %s is sending too many emails!!! (> 20 / hr)"
 
+  def maybe_crash_if_too_many_logs(device)
+    query_params   = { sent_at: 1.hours.ago..Time.now, device_id: device.id }
+    sent_this_hour = Log.where(query_params).count
+    too_many       = sent_this_hour > Log.max_per_hour
+    raise Log::RateLimitError, WHOAH % [device.id] if too_many
+  end
+
+  def send_a_digest(device, unsent)
+    @emails      = device.users.pluck(:email)
+    @messages    = unsent
+                    .pluck(:created_at, :message)
+                    .map{|(t,m)| [t.in_time_zone(device.timezone || "UTC"), m] }
+                    .map{|(x,y)| "[#{x}]: #{y}"}
+    @device_name = device.name
+    mail(to: @emails, subject: "🌱 New message from #{@device_name}!")
+    unsent.update_all(sent_at: Time.now)
+  end
+
   def log_digest(device)
     Log.transaction do
-      query_params   = { sent_at: 1.hours.ago..Time.now, device_id: device.id }
-      sent_this_hour = Log.where(query_params).count
-      too_many       = sent_this_hour > Log.max_per_hour
-      raise Log::RateLimitError, WHOAH % [device.id] if too_many
-      unsent         = Log.where(sent_at: nil, device: device)
-      if(unsent.any?)
-        logs         = Log
-                        .where(id: unsent.pluck(:id))
-                        .where
-                        .not(Log::IS_FATAL_EMAIL)
-                        .order(created_at: :desc)
-        @emails      = device.users.pluck(:email)
-        @messages    = logs
-                        .pluck(:created_at, :message)
-                        .map{|(t,m)| [t.in_time_zone(device.timezone || "UTC"), m] }
-                        .map{|(x,y)| "[#{x}]: #{y}"}
-                        .join("\n\n")
-        @device_name = device.name || "Farmbot"
-        mail(to: @emails, subject: "🌱 New message from #{@device_name}!")
-        unsent.update_all(sent_at: Time.now)
-      end
+      maybe_crash_if_too_many_logs(device)
+      unsent         = device.unsent_routine_emails
+      send_a_digest(device, unsent) if unsent.any?
     end
   end
 end
