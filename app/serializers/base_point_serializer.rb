6ee22b0b1b18eb1bc69f553fae07c0e67d543169
Rick Carlino <rick.carlino@gmail.com>
2018-08-16 11:05:58 -0500

    Opps- squash this commit plz

--- app/serializers/base_point_serializer.rb
+++ app/serializers/base_point_serializer.rb
@@ -1,10 +1,6 @@
 class BasePointSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :updated_at, :device_id, :name, :pointer_type,
-             :meta, :x, :y, :z, :odelay
-
-  def odelay
-    "odelay!"
-  end
+             :meta, :x, :y, :z
 
   def meta
     object.meta || {}
