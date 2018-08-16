dbb0acc4852f5263d5630c89ace2f522364ac372
Rick Carlino <rick.carlino@gmail.com>
2018-08-16 11:03:29 -0500

    Merge conflcitsc

--- app/serializers/base_point_serializer.rb
+++ app/serializers/base_point_serializer.rb
@@ -1,17 +1,9 @@
 class BasePointSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :updated_at, :device_id, :name, :pointer_type,
-             :meta, :x, :y, :z
+             :meta, :x, :y, :z, :odelay
 
-  def x
-    object.x.round
-  end
-
-  def y
-    object.y.round
-  end
-
-  def z
-    object.z.round
+  def odelay
+    "odelay!"
   end
 
   def meta
