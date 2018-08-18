5066b039b9f2d5bcb50b53bae0f52eb78258bcc8
Rick Carlino <rick.carlino@gmail.com>
2018-08-18 11:17:57 -0500

    Remove tests for old digest delivery logic

--- app/models/log_delivery_stuff.rb
+++ app/models/log_delivery_stuff.rb
@@ -3,28 +3,12 @@
 # Also helps group "fast" messages into a digest.
 module LogDeliveryStuff
   class RateLimitError < StandardError; end
-  WAIT_PERIOD = 30
-  WAIT_UNIT   = :seconds
-
   module ClassMethods
     attr_accessor :max_per_hour
 
     # If this method grows, create a mutation.
     def deliver(device, log)
-      send_routine_emails(log, device)
       send_fatal_emails(log, device)
-    end
-
-    def digest_wait_time
-      { wait: WAIT_PERIOD.send(WAIT_UNIT) }
-    end
-
-    # TODO: Why must I explicitly pass `mailer_klass`? Somethings not right with
-    #       mocks.
-    def send_routine_emails(log, device, mailer_klass = LogDeliveryMailer)
-      puts "THIS IS TEMPORARILY STUBBED OUT!"
-      # return unless (log.channels || []).include?("email")
-      # mailer_klass.log_digest(device).deliver_later(digest_wait_time)
     end
 
     def send_fatal_emails(log, device)
