49d36f6ecb9bb61e0a3a005d6c7f670bef94ddcc
Rick Carlino <rick.carlino@gmail.com>
2018-08-24 12:07:43 -0500

    WIP

---
+++ webpack/sequences/step_tiles/mark_as/options.ts
@@ -0,0 +1,8 @@
+import { ResourceName } from "farmbot";
+import { DropDownItem } from "../../../ui";
+
+const OPTIONS: Partial<Record<ResourceName, DropDownItem[]>> = {
+  "Tool": [],
+  "Plant": [],
+  "Point": []
+};
