264aa1974532009cc353106cd0e3cae4541ade74
Rick Carlino <rick.carlino@gmail.com>
2018-08-24 15:21:16 -0500

    WIP

--- webpack/sequences/step_tiles/mark_as/options.ts
+++ webpack/sequences/step_tiles/mark_as/options.ts
@@ -1,8 +1,16 @@
-import { ResourceName } from "farmbot";
+import { ResourceName, TaggedPlantPointer } from "farmbot";
 import { DropDownItem } from "../../../ui";
 
 const OPTIONS: Partial<Record<ResourceName, DropDownItem[]>> = {
   "Tool": [],
   "Plant": [],
   "Point": []
+};
+
+const isWeed = (plant: TaggedPlantPointer) => {
+  const cb = plant.body.meta["created_by"];
+  if (cb == "weed-detection") {
+    return true;
+  }
+  return false;
 };
