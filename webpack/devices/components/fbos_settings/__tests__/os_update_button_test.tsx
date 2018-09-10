5c4b1a956636ad612c6e13118520e20672d42d31
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-09-10 13:20:00 -0700

    os update btn fixes and refactoring

--- webpack/devices/components/fbos_settings/__tests__/os_update_button_test.tsx
+++ webpack/devices/components/fbos_settings/__tests__/os_update_button_test.tsx
@@ -13,138 +13,238 @@ import { OsUpdateButton } from "../os_update_button";
 import { OsUpdateButtonProps } from "../interfaces";
 
 describe("<OsUpdateButton/>", () => {
-  beforeEach(function () {
+  beforeEach(() => {
     bot.currentOSVersion = "3.1.6";
     bot.hardware.configuration.beta_opt_in = false;
   });
 
-  const fakeProps = (): OsUpdateButtonProps => {
-    return {
-      bot,
-      sourceFbosConfig: (x) => {
-        return { value: bot.hardware.configuration[x], consistent: true };
-      },
-      botOnline: true,
-    };
+  const fakeProps = (): OsUpdateButtonProps => ({
+    bot,
+    sourceFbosConfig: x =>
+      ({ value: bot.hardware.configuration[x], consistent: true }),
+    botOnline: true,
+  });
+
+  interface TestProps {
+    installedVersion: string | undefined;
+    installedCommit: string;
+    availableVersion: string | undefined;
+    availableBetaVersion: string | undefined;
+    availableBetaCommit: string | undefined;
+    betaOptIn: boolean | undefined;
+    onBeta: boolean | undefined;
+  }
+
+  const defaultTestProps = (): TestProps => ({
+    installedVersion: "3.1.6",
+    installedCommit: "",
+    availableVersion: "3.1.6",
+    availableBetaVersion: undefined,
+    availableBetaCommit: undefined,
+    betaOptIn: false,
+    onBeta: false,
+  });
+
+  interface Results {
+    text: string;
+    title: string | undefined;
+    color: string;
+    disabled: boolean;
+  }
+
+  const updateNeeded = (title: string | undefined): Results =>
+    ({
+      text: "UPDATE",
+      title,
+      color: "green",
+      disabled: false,
+    });
+
+  const upToDate = (title: string | undefined): Results =>
+    ({
+      text: "UP TO DATE",
+      title,
+      color: "gray",
+      disabled: false,
+    });
+
+  const cantConnect = (entity: string): Results =>
+    ({
+      text: `Can't connect to ${entity}`,
+      title: undefined,
+      color: "yellow",
+      disabled: false,
+    });
+
+  const updating = (progress: string): Results =>
+    ({
+      text: progress,
+      title: undefined,
+      color: "gray",
+      disabled: true,
+    });
+
+  const testButtonState = (
+    testProps: TestProps,
+    expected: Results) => {
+    const {
+      installedVersion, installedCommit, onBeta,
+      availableVersion, availableBetaVersion, availableBetaCommit, betaOptIn
+    } = testProps;
+    bot.hardware.informational_settings.controller_version = installedVersion;
+    bot.hardware.informational_settings.commit = installedCommit;
+    bot.hardware.informational_settings.currently_on_beta = onBeta;
+    bot.currentOSVersion = availableVersion;
+    bot.currentBetaOSVersion = availableBetaVersion;
+    bot.currentBetaOSCommit = availableBetaCommit;
+    bot.hardware.configuration.beta_opt_in = betaOptIn;
+
+    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
+    const osUpdateButton = buttons.find("button").first();
+    expect(osUpdateButton.text()).toBe(expected.text);
+    expect(osUpdateButton.props().title).toBe(expected.title);
+    expect(osUpdateButton.hasClass(expected.color)).toBe(true);
+    expect((osUpdateButton.props().disabled)).toBe(expected.disabled);
   };
 
   it("renders buttons: not connected", () => {
-    bot.hardware.informational_settings.controller_version = undefined;
-    bot.currentOSVersion = undefined;
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    expect(buttons.find("button").length).toBe(1);
-    const autoUpdate = buttons.find("button").first();
-    expect(autoUpdate.hasClass("yellow")).toBeTruthy();
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("Can't connect to bot");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = undefined;
+    testProps.availableVersion = undefined;
+    const expectedResults = cantConnect("bot");
+    testButtonState(testProps, expectedResults);
   });
+
   it("renders buttons: connected to releases but not bot", () => {
-    bot.hardware.informational_settings.controller_version = undefined;
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    expect(buttons.find("button").length).toBe(1);
-    const autoUpdate = buttons.find("button").first();
-    expect(autoUpdate.hasClass("yellow")).toBeTruthy();
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("Can't connect to bot");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = undefined;
+    const expectedResults = cantConnect("bot");
+    expectedResults.title = "3.1.6";
+    testButtonState(testProps, expectedResults);
   });
+
   it("renders buttons: no releases available", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.6";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.currentOSVersion = undefined;
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("Can't connect to release server");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.6";
+    testProps.availableVersion = undefined;
+    testProps.betaOptIn = true;
+    const expectedResults = cantConnect("release server");
+    testButtonState(testProps, expectedResults);
   });
+
   it("renders buttons: only beta release available", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.6";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.currentOSVersion = undefined;
-    bot.currentBetaOSVersion = "3.1.7-beta";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UPDATE");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.6";
+    testProps.availableVersion = undefined;
+    testProps.availableBetaVersion = "3.1.7-beta";
+    testProps.betaOptIn = true;
+    const expectedResults = updateNeeded("3.1.7-beta");
+    testButtonState(testProps, expectedResults);
   });
+
   it("renders buttons: no beta release available", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.6";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.currentBetaOSVersion = undefined;
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UP TO DATE");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.6";
+    testProps.availableBetaVersion = undefined;
+    testProps.betaOptIn = true;
+    const expectedResults = upToDate("3.1.6");
+    testButtonState(testProps, expectedResults);
   });
+
   it("up to date", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.6";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UP TO DATE");
-    expect(osUpdateButton.props().title).toBe("3.1.6");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.6";
+    const expectedResults = upToDate("3.1.6");
+    testButtonState(testProps, expectedResults);
   });
+
   it("up to date: newer", () => {
-    bot.hardware.informational_settings.controller_version = "5.0.0";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UP TO DATE");
-    expect(osUpdateButton.props().title).toBe("3.1.6");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "5.0.0";
+    const expectedResults = upToDate("3.1.6");
+    testButtonState(testProps, expectedResults);
   });
+
   it("update available", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.5";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UPDATE");
-    expect(osUpdateButton.props().title).toBe("3.1.6");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.5";
+    const expectedResults = updateNeeded("3.1.6");
+    testButtonState(testProps, expectedResults);
   });
+
   it("beta update available", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.5";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.currentBetaOSVersion = "5.0.0-beta";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UPDATE");
-    expect(osUpdateButton.props().title).toBe("5.0.0-beta");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.5";
+    testProps.availableBetaVersion = "5.0.0-beta";
+    testProps.betaOptIn = true;
+    const expectedResults = updateNeeded("5.0.0-beta");
+    testButtonState(testProps, expectedResults);
   });
+
   it("latest newer than beta update: latest installed", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.6";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.currentBetaOSVersion = "3.1.6-beta";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UP TO DATE");
-    expect(osUpdateButton.props().title).toBe("3.1.6");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.6";
+    testProps.availableBetaVersion = "3.1.6-beta";
+    testProps.betaOptIn = true;
+    const expectedResults = upToDate("3.1.6");
+    testButtonState(testProps, expectedResults);
   });
+
   it("latest newer than beta update: beta installed", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.6";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.hardware.informational_settings.currently_on_beta = true;
-    bot.currentBetaOSVersion = "3.1.6-beta";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UPDATE");
-    expect(osUpdateButton.props().title).toBe("3.1.6");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.6";
+    testProps.availableBetaVersion = "3.1.6-beta";
+    testProps.betaOptIn = true;
+    testProps.onBeta = true;
+    const expectedResults = updateNeeded("3.1.6");
+    testButtonState(testProps, expectedResults);
   });
+
+  it("latest newer than beta update: beta installed (beta disabled)", () => {
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.6";
+    testProps.availableBetaVersion = "3.1.6-beta";
+    testProps.betaOptIn = false;
+    testProps.onBeta = true;
+    const expectedResults = updateNeeded("3.1.6");
+    testButtonState(testProps, expectedResults);
+  });
+
   it("on latest beta update", () => {
-    bot.hardware.informational_settings.controller_version = "3.1.7";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.hardware.informational_settings.currently_on_beta = true;
-    bot.currentBetaOSVersion = "3.1.7-beta";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UP TO DATE");
-    expect(osUpdateButton.props().title).toBe("3.1.7-beta");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.7";
+    testProps.availableBetaVersion = "3.1.7-beta";
+    testProps.betaOptIn = true;
+    testProps.onBeta = true;
+    const expectedResults = upToDate("3.1.7-beta");
+    testButtonState(testProps, expectedResults);
   });
+
   it("beta update has same numeric version: newer commit", () => {
-    bot.hardware.informational_settings.controller_version = "5.0.0";
-    bot.hardware.informational_settings.commit = "old commit";
-    bot.hardware.configuration.beta_opt_in = true;
-    bot.currentBetaOSVersion = "5.0.0-beta";
-    bot.currentBetaOSCommit = "new commit";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UPDATE");
-    expect(osUpdateButton.props().title).toBe("5.0.0-beta");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "5.0.0";
+    testProps.installedCommit = "old commit";
+    testProps.availableBetaVersion = "5.0.0-beta";
+    testProps.availableBetaCommit = "new commit";
+    testProps.betaOptIn = true;
+    testProps.onBeta = true;
+    const expectedResults = updateNeeded("5.0.0-beta");
+    testButtonState(testProps, expectedResults);
   });
+
+  it("handles installed version newer than available (beta enabled)", () => {
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.7";
+    testProps.betaOptIn = true;
+    testProps.onBeta = false;
+    testProps.availableBetaVersion = "3.1.7-beta";
+    const expectedResults = upToDate("3.1.7-beta");
+    testButtonState(testProps, expectedResults);
+  });
+
   it("calls checkUpdates", () => {
     const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
+    const osUpdateButton = buttons.find("button").first();
     osUpdateButton.simulate("click");
     expect(mockDevice.checkUpdates).toHaveBeenCalledTimes(1);
   });
@@ -155,7 +255,7 @@ describe("<OsUpdateButton/>", () => {
         "FBOS_OTA": { status: "working", bytes: progress, unit: "bytes" }
       };
       const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-      const osUpdateButton = buttons.find("button").last();
+      const osUpdateButton = buttons.find("button").first();
       expect(osUpdateButton.text()).toBe(text);
     });
   }
@@ -167,34 +267,33 @@ describe("<OsUpdateButton/>", () => {
     bot.hardware.jobs = {
       "FBOS_OTA": { status: "working", percent: 10, unit: "percent" }
     };
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("10%");
+    const expectedResults = updating("10%");
+    expectedResults.title = "3.1.6";
+    testButtonState(defaultTestProps(), expectedResults);
   });
+
   it("update success", () => {
     bot.hardware.jobs = {
       "FBOS_OTA": { status: "complete", percent: 100, unit: "percent" }
     };
-    bot.hardware.informational_settings.controller_version = "3.1.6";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UP TO DATE");
+    testButtonState(defaultTestProps(), upToDate("3.1.6"));
   });
+
   it("update failed", () => {
     bot.hardware.jobs = {
       "FBOS_OTA": { status: "error", percent: 10, unit: "percent" }
     };
-    bot.hardware.informational_settings.controller_version = "3.1.5";
-    const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
-    expect(osUpdateButton.text()).toBe("UPDATE");
+    const testProps = defaultTestProps();
+    testProps.installedVersion = "3.1.5";
+    testButtonState(testProps, updateNeeded("3.1.6"));
   });
+
   it("is disabled", () => {
     bot.hardware.jobs = {
       "FBOS_OTA": { status: "working", percent: 10, unit: "percent" }
     };
     const buttons = mount(<OsUpdateButton {...fakeProps()} />);
-    const osUpdateButton = buttons.find("button").last();
+    const osUpdateButton = buttons.find("button").first();
     osUpdateButton.simulate("click");
     expect(mockDevice.checkUpdates).not.toHaveBeenCalled();
   });
