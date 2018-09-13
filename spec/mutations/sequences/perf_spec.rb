78bb58dbbde41eb3adb9ae3e2a0ab145f9881f7d
Rick Carlino <rick.carlino@gmail.com>
2018-09-13 09:27:56 -0500

    Remove unneeded test

--- spec/mutations/sequences/perf_spec.rb
+++
@@ -1,7 +0,0 @@
-describe Sequences::Create do
-  it "has performance sprinkles" do
-    body = []
-    # Start recording the time it takes to create a sequence and
-    # graph the results.
-  end
-end
