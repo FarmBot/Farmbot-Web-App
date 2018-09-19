4de47b37a11ba28d60de8279bc25b82cb2104ec5
Rick Carlino <rick.carlino@gmail.com>
2018-09-19 18:00:07 -0500

    Use non-arrow fn, just to be safe.

--- webpack/history.tsx
+++ webpack/history.tsx
@@ -3,7 +3,9 @@ import { maybeStripLegacyUrl } from "./link";
 
 export const push = (url: string) => navigate(maybeStripLegacyUrl(url));
 
-export const getPathArray = () => location.pathname.split("/");
+export function getPathArray() {
+  return location.pathname.split("/");
+}
 
 /** This is a stub from the `react-router` days. Don't use it anymore. */
 export const history = { push, getCurrentLocation: () => window.location };
