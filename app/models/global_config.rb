349e1a51e1b29bc1b02408c6c9ce6250f1865e3c
Rick Carlino <rick.carlino@gmail.com>
2018-09-25 15:30:48 -0500

    Deal with clobbering

--- app/models/global_config.rb
+++ app/models/global_config.rb
@@ -7,20 +7,25 @@ class GlobalConfig < ApplicationRecord
   validates_uniqueness_of :key
   validates_presence_of   :key
 
+  # Bootstrap these values, but NEVER clobber pre-existing ones:
+  {
+    "FBOS_END_OF_LIFE_VERSION" => "6.3.0",
+    "MINIMUM_FBOS_VERSION"     => "6.0.0"
+  }.map do |(key, value)|
+    x = self.find_by(key: key)
+    self.create!(key: key, value: value) unless x
+  end
+
   LONG_REVISION = ENV["BUILT_AT"] || ENV["HEROKU_SLUG_COMMIT"] || "NONE"
-  # Bootstrap initial defaults:
+  # Bootstrap these values, and ALWAYS clobber pre-existing ones:
   {
     "NODE_ENV"                 => Rails.env || "development",
     "TOS_URL"                  => ENV.fetch("TOS_URL", ""),
     "PRIV_URL"                 => ENV.fetch("PRIV_URL", ""),
     "LONG_REVISION"            => LONG_REVISION,
     "SHORT_REVISION"           => LONG_REVISION.first(8),
-    "FBOS_END_OF_LIFE_VERSION" => "0.0.0",
-    "MINIMUM_FBOS_VERSION"     => "6.0.0"
   }.map do |(key, value)|
-    self
-      .find_or_create_by(key: key)
-      .update_attributes(key: key, value: value)
+    self.find_or_create_by(key: key).update_attributes(key: key, value: value)
   end
 
   # Memoized version of every GlobalConfig, with key/values layed out in a hash.
