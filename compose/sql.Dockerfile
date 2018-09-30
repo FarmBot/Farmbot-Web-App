ca3d677928e32c0551982d62c39903ed7596e7fa
Rick Carlino <rick.carlino@gmail.com>
2018-09-30 17:24:01 -0500

    WIP - `CMD` of web-api executes in wrong directory.

---
+++ compose/sql.Dockerfile
@@ -0,0 +1,4 @@
+FROM postgres:9.4
+
+# Use "postgres" database
+ENV POSTGRES_PASSWORD password123
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,3 +12,7 @@ Devise.setup do |config|
   config.reset_password_within = 6.hours
   config.sign_out_via = [:delete, :get]
 end
+puts "="*40
+puts "="*40
+puts "="*40
+puts "="*40
