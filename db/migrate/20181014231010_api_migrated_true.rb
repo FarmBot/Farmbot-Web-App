802d7494698397d7860eafd7f5c5b280bb42d970
Rick Carlino <rick.carlino@gmail.com>
2018-10-15 16:07:26 -0500

    Remove stray migration

--- db/migrate/20181014231010_api_migrated_true.rb
+++
@@ -1,8 +0,0 @@
-class ApiMigratedTrue < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default(:fbos_configs,     :api_migrated, true)
-    change_column_default(:firmware_configs, :api_migrated, true)
-    puts "This part needs a cutoff date:"
-    [FbosConfig, FirmwareConfig].map { |x| x.update_all(api_migrated: true) }
-  end
-end
