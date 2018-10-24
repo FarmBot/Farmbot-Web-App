c2e38f4c20196585c729b47c1a8ee6805bc0fc1d
Rick Carlino <rick.carlino@gmail.com>
2018-10-24 10:20:14 -0500

    Sprinkle performance on /sync endpoint

--- app/mutations/devices/sync.rb
+++ app/mutations/devices/sync.rb
@@ -1,36 +1,32 @@
 module Devices
   class Sync < Mutations::Command
-    FIELDS    = [ :id, :updated_at ]
-    PLURAL    = [ :farm_events,
-                  :farmware_envs,
-                  :farmware_installations,
-                  :peripherals,
-                  :pin_bindings,
-                  :points,
-                  :regimens,
-                  :sensor_readings,
-                  :sensors,
-                  :sequences,
-                  :tools ]
+    SEL     = "SELECT id, updated_at FROM"
+    WHERE   = "WHERE device_id = "
+    WHERE2  = "devices WHERE id = "
+    QUERIES = {
+      devices:                [SEL,                          WHERE2].join(" "),
+      farm_events:            [SEL, "farm_events",            WHERE].join(" "),
+      farmware_envs:          [SEL, "farmware_envs",          WHERE].join(" "),
+      farmware_installations: [SEL, "farmware_installations", WHERE].join(" "),
+      peripherals:            [SEL, "peripherals",            WHERE].join(" "),
+      pin_bindings:           [SEL, "pin_bindings",           WHERE].join(" "),
+      points:                 [SEL, "points",                 WHERE].join(" "),
+      regimens:               [SEL, "regimens",               WHERE].join(" "),
+      sensor_readings:        [SEL, "sensor_readings",        WHERE].join(" "),
+      sensors:                [SEL, "sensors",                WHERE].join(" "),
+      sequences:              [SEL, "sequences",              WHERE].join(" "),
+      tools:                  [SEL, "tools",                  WHERE].join(" "),
+      fbos_configs:           [SEL, "fbos_configs",           WHERE].join(" "),
+      firmware_configs:       [SEL, "firmware_configs",       WHERE].join(" "),
+    }
 
-    required do
-      model   :device, class: Device
-    end
+    required { model :device, class: Device }
 
     def execute
-      base = { now:              Time.now.utc, # Clock skew detector?
-               devices:          pluck(device),
-               fbos_configs:     pluck(device.fbos_config),
-               firmware_configs: pluck(device.firmware_config), }
-      PLURAL.reduce(base) do |json, resource|
-        json.update(resource => device.send(resource).pluck(*FIELDS))
+      conn    = ActiveRecord::Base.connection
+      QUERIES.reduce(now: Time.now) do |acc, (key, value)|
+        acc.update(key => conn.execute(value + device.id.to_s).values)
       end
-    end
-
-private
-
-    def pluck(obj)
-      [[obj.id, obj.updated_at]]
     end
   end
 end
--- spec/mutations/devices/sync_spec.rb
+++ spec/mutations/devices/sync_spec.rb
@@ -4,28 +4,24 @@ describe Devices::Sync do
     let(:user) { FactoryBot.create(:user) }
     let(:device) { user.device }
 
+  TABLES = [ :devices,
+             :farm_events,
+             :farmware_envs,
+             :farmware_installations,
+             :fbos_configs,
+             :firmware_configs,
+             :now,
+             :peripherals,
+             :pin_bindings,
+             :points,
+             :regimens,
+             :sensor_readings,
+             :sensors,
+             :sequences,
+             :tools ]
 
   it 'is different this time!' do
     results = Devices::Sync.run!(device: device)
-    expect(results.keys.sort).to eq([:devices,
-                                     :farm_events,
-                                     :farmware_envs,
-                                     :farmware_installations,
-                                     :fbos_configs,
-                                     :firmware_configs,
-                                     :now,
-                                     :peripherals,
-                                     :pin_bindings,
-                                     :points,
-                                     :regimens,
-                                     :sensor_readings,
-                                     :sensors,
-                                     :sequences,
-                                     :tools])
+    expect(results.keys.sort).to eq(TABLES)
   end
-
-  # it 'optimizes with SQL' do
-  #   query         = "SELECT id, updated_at FROM ? WHERE device_id = ?"
-  #   records_array = ActiveRecord::Base.connection.execute(sql)
-  # end
 end
