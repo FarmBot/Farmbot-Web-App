3bdd430a4d23364f5b5ee8442b6231f55cbd51e4
Rick Carlino <rick.carlino@gmail.com>
2018-11-11 19:38:00 -0600

    Fix typo

---
+++ db/migrate/20181112010427_add_firmware_confs_to_fbos_configs.rb
@@ -0,0 +1,6 @@
+class AddFirmwareConfsToFbosConfigs < ActiveRecord::Migration[5.2]
+  def change
+    add_column :fbos_configs, :firmware_path,      :string
+    add_column :fbos_configs, :firmware_debug_log, :boolean
+  end
+end
