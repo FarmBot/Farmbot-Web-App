b82268c032b4d2be006f8bf10918fab8918943ea
Rick Carlino <rick.carlino@gmail.com>
2018-11-14 15:44:01 -0600

    Unit tests for resourceUsageList

--- webpack/resources/__tests__/in_use_test.ts
+++ webpack/resources/__tests__/in_use_test.ts
@@ -1,10 +1,33 @@
 import { TaggedSequence } from "farmbot";
 import { ResourceIndex } from "../interfaces";
 import { buildResourceIndex } from "../../__test_support__/resource_index_builder";
-import { EVERY_USAGE_KIND, UsageIndex } from "../in_use";
+import { EVERY_USAGE_KIND, UsageIndex, resourceUsageList } from "../in_use";
 import { DeepPartial } from "redux";
 import { fakeSequence } from "../../__test_support__/fake_state/resources";
 
+describe("resourceUsageList", () => {
+  it("Converts `UsageIndex` type Into Record<UUID, boolean>", () => {
+    const x = {
+      "FarmEvent.Regimen": {
+        "FarmEvent.0.0": { "Regimen.2.2": true, "Regimen.1.1": true }
+      },
+      "FarmEvent.Sequence": {
+        "FarmEvent.3.3": { "Sequence.4.4": true, "Sequence.5.5": true }
+      },
+      "Regimen.Sequence": {
+        "Regimen.6.6": { "Sequence.7.7": true, "Sequence.8.8": true }
+      },
+      "Sequence.Sequence": {
+        "Regimen.9.9": { "Sequence.10.10": true, "Sequence.11.11": true }
+      },
+    };
+    const actual = Object.keys(resourceUsageList(x)).sort();
+    const expected =
+      ["FarmEvent.0.0", "FarmEvent.3.3", "Regimen.6.6", "Regimen.9.9"].sort();
+    expect(actual.length).toEqual(expected.length);
+    expected.map(y => expect(actual).toContain(y));
+  });
+});
 describe("in_use tracking at reducer level", () => {
   function testCase(sequences: TaggedSequence[]): ResourceIndex {
     return buildResourceIndex(sequences).index;
