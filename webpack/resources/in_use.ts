7923722864186406f2b4adb4fc626449f438056f
Rick Carlino <rick.carlino@gmail.com>
2018-11-14 15:49:42 -0600

    Unit tests for resourceUsageList

--- webpack/resources/in_use.ts
+++ webpack/resources/in_use.ts
@@ -18,6 +18,14 @@ export type UsageIndex = Record<UsageKind, UsageMap>;
 
 const start: Record<UUID, boolean> = {};
 
+/**
+ * SCENARIO: You need a lookup table of which resources are *not* safe to
+ *           delete.
+ * PROBLEM:  The nested nature of `UsageIndex` makes lookups across all
+ *           resource types a bit of a hassle.
+ * SOLUTION: Smoosh all resources into one heap. If you need to do iteration,
+ *           use `Object.keys();`
+ */
 export const resourceUsageList =
   (usageIndex: UsageIndex): Record<UUID, boolean> => {
     return EVERY_USAGE_KIND
