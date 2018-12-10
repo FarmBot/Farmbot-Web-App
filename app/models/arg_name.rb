5370990917cc68f4616e6b50c1e36c30e9c276ad
Rick Carlino <rick.carlino@gmail.com>
2018-12-10 17:58:24 -0600

    Add cached_by_value, fix HeapAddress#==, begin Fragments::Create

--- app/models/arg_name.rb
+++ app/models/arg_name.rb
@@ -1,3 +1,11 @@
 # Support class for Fragment. Please see fragment.rb for documentation.
 class ArgName < ApplicationRecord
+  EXPIRY = 2.hours
+  KEY    = "kind_%s"
+
+  def self.cached_by_value(v)
+    Rails
+      .cache
+      .fetch(KEY % v, expires_in: EXPIRY) { find_or_create_by(value: v) }
+  end
 end
--- spec/lib/celery_script/farm_event_celery_stuff_spec.rb
+++ spec/lib/celery_script/farm_event_celery_stuff_spec.rb
@@ -1,98 +1,156 @@
 require 'spec_helper'
 
+module Fragments
+  class Create < Mutations::Command
+    H     = CeleryScript::HeapAddress
+    K     = "__KIND__"
+
+    STUFF = { device:          0,  # === Maybe cache
+              fragment:        1,
+              primitives:      {},
+              args_names:      {}, # === Maybe cache
+              kinds:           {}, # === Maybe cache
+              nodes:           {},
+              arg_sets:        {},
+              primitive_pairs: {},
+              standard_pairs:  {}, }
+
+    STEPS = [ "Validate CeleryScript", #
+              "Flatten CeleryScript",  #
+              "move to background",    # ?
+              "create node",           #
+              "attach kind",
+              "create arg group",
+              "create primitive pairs",
+              "create standard pairs",
+              "stitch the nodes together", ]
+
+    KINDS = CeleryScriptSettingsBag::Corpus
+              .as_json[:nodes]
+              .pluck("name")
+              .sort
+              .map(&:to_s)
+
+    required do
+      model :device, class: Device
+      array :proto_nodes do
+        hash { duck :*, methods: [] }
+      end
+    end
+
+    def validate
+    end
+
+    def execute
+      primitives = {}
+      fragment   = Fragment.new(device: device)
+      nodes      = proto_nodes.map do |flat_node|
+        real_node = Node.new(kind: Kind.cached_by_value(flat_node.fetch(K)),
+                             fragment: fragment)
+        ag        = ArgSet.new(node: real_node)
+        flat_node.without(K).map do |(k,v)|
+           if k.starts_with?("__")
+            binding.pry
+           else
+            binding.pry
+           end
+          end
+      end
+      # binding.pry
+    end
+
+    def null
+      @null ||=  Node.new
+    end
+  end
+end
+
 describe CeleryScript::Checker do
   let(:device) { FactoryBot.create(:device) }
   let(:tool)   { FactoryBot.create(:tool, device: device) }
   let(:point)  { FactoryBot.create(:generic_pointer, device: device) }
   let(:corpus) { Sequence::Corpus }
-
-  it "thinks about how we're going to store this stuff" do
-    # [
+  H = CeleryScript::HeapAddress
+  it "loads CeleryScript from the database"
+  it "dumps CeleryScript into the database" do
+    # PROBLEMS THAT I MUST FIX:
+    #   * FarmEvents currently don't have CeleryScript
+    #   * SOON, Regimens will need celeryscript, too
+    #   * Celeryscript only supports sequences today (bad).
+    #   * Relocatable fragments would be cool
+    device      = FactoryBot.create(:device)
+    proto_nodes = [ { :__KIND__     => "nothing",
+                      :__parent     => H[0],
+                      :__body       => H[0],
+                      :__next       => H[0] },
+                    { :__KIND__     => "farm_event",
+                      :__parent     => H[0],
+                      :__body       => H[2],
+                      :__next       => H[0] },
+                    { :__KIND__     => "variable_declaration",
+                      :__parent     => H[1],
+                      :label        => "foo",
+                      :__data_value => H[3],
+                      :__body       => H[0],
+                      :__next       => H[0] },
+                    { :__KIND__     => "identifier",
+                      :__parent     => H[2],
+                      :label        => "makes no sense",
+                      :data_type    => "coordinate",
+                      :__body       => H[0],
+                      :__next       => H[0] } ]
+    results     = Fragments::Create.run!(device:      device,
+                                         proto_nodes: proto_nodes)
+    # [ { :__KIND__ => "nothing",
+    #     :__parent => H[0],
+    #     :__body   => H[0],
+    #     :__next   => H[0] },
     #   {
-    #     :__KIND__=>"nothing",
-    #     :__parent=>HeapAddress(0),
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)
-    #   }, {
-    #     :__KIND__=>"farm_event",
-    #     :__parent=>HeapAddress(0),
-    #     :__body=>HeapAddress(2),
-    #     :__next=>HeapAddress(0)
-    #   }, {
-    #     :__KIND__=>"variable_declaration",
-    #     :__parent=>HeapAddress(1),
-    #     :label=>"foo",
-    #     :__data_value=>HeapAddress(3),
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)
-    #   }, {
-    #     :__KIND__=>"identifier",
-    #     :__parent=>HeapAddress(2),
-    #     :label=>"makes no sense",
-    #     :data_type=>"coordinate",
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)
-    #   }
-    # ]
-
-    # [
-    #   {
-    #     :__KIND__=>"nothing",
-    #     :__parent=>HeapAddress(0),
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)
-    #   },
-    #   {
-    #     :__KIND__=>"farm_event",
-    #     :__parent=>HeapAddress(0),
-    #     :__body=>HeapAddress(2),
-    #     :__next=>HeapAddress(0)
-    #   },
+    #     :__KIND__ => "farm_event",
+    #     :__parent => H[0],
+    #     :__body   => H[2],
+    #     :__next   => H[0] },
     #   {
     #     :__KIND__=>"variable_declaration",
-    #     :__parent=>HeapAddress(1),
+    #     :__parent=>H[1],
     #     :label=>"tool",
-    #     :__data_value=>HeapAddress(3),
-    #     :__next=>HeapAddress(4),
-    #     :__body=>HeapAddress(0)
-    #   },
+    #     :__data_value=>H[3],
+    #     :__next=>H[4],
+    #     :__body=>H[0] },
     #   {
     #     :__KIND__=>"tool",
-    #     :__parent=>HeapAddress(2),
+    #     :__parent=>H[2],
     #     :tool_id=>1,
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)
-    #   },
+    #     :__body=>H[0],
+    #     :__next=>H[0] },
     #   {
     #     :__KIND__=>"variable_declaration",
-    #     :__parent=>HeapAddress(2),
+    #     :__parent=>H[2],
     #     :label=>"coordinate",
-    #     :__data_value=>HeapAddress(5),
-    #     :__next=>HeapAddress(6),
-    #     :__body=>HeapAddress(0)
-    #   },
+    #     :__data_value=>H[5],
+    #     :__next=>H[6],
+    #     :__body=>H[0] },
     #   {
     #     :__KIND__=>"coordinate",
-    #     :__parent=>HeapAddress(4),
+    #     :__parent=>H[4],
     #     :x=>0,
     #     :y=>0,
     #     :z=>0,
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)
-    #   },
+    #     :__body=>H[0],
+    #     :__next=>H[0] },
     #   {
     #     :__KIND__=>"variable_declaration",
-    #     :__parent=>HeapAddress(4),
+    #     :__parent=>H[4],
     #     :label=>"point",
-    #     :__data_value=>HeapAddress(7),
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)
+    #     :__data_value=>H[7],
+    #     :__body=>H[0],
+    #     :__next=>H[0]
     #   },
     #   {
     #     :__KIND__=>"point",
-    #     :__parent=>HeapAddress(6),
-    #     :__body=>HeapAddress(0),
-    #     :__next=>HeapAddress(0)}
+    #     :__parent=>H[6],
+    #     :__body=>H[0],
+    #     :__next=>H[0]}
     #     :pointer_type=>"GenericPointer",
     #     :pointer_id=>1,
     # ]
