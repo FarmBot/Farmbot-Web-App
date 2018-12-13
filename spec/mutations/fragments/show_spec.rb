4500c102942585a6a7bcfa57bfb60e4198df8f42
Rick Carlino <rick.carlino@gmail.com>
2018-12-13 14:11:27 -0600

    WIP

--- spec/mutations/fragments/show_spec.rb
+++ spec/mutations/fragments/show_spec.rb
@@ -35,6 +35,25 @@ describe Fragments::Create do
     expect(diff.length).to eq(0)
   end
 
+  class StubLog
+    attr_accessor :logs
+    ALIAS_MAP = { debug?:  :yes,
+                  errror?: :no,
+                  warn?:   :no,
+                  error:   :doit,
+                  warn:    :doit,
+                  debug:   :doit, }
+
+    def logs;    @logs ||= []; end
+    def no;      false;        end
+    def yes;     true;         end
+    def reset;   @logs = [];   end
+    def count;   logs.length;  end
+    def doit(m); logs.push(m); end
+
+    ALIAS_MAP.map { |(from, to)| alias_method from, to }
+  end
+
   it "prevents N+1" do
     a2z    = (('a'..'z').to_a + ('0'..'9').to_a)
     body   = a2z.map do |label|
@@ -46,12 +65,18 @@ describe Fragments::Create do
         }
       }
     end
-    origin  = { device: device, kind: "internal_farm_event", args:{}, body: body }
+    old_logger    = config.logger
+    nplusone      = StubLog.new #
+    config.logger = nplusone
+    origin        = { device: device,
+                      kind:   "internal_farm_event",
+                      args:   {},
+                      body:   body }
     fragment = \
       Fragments::Create.run!(device: device, flat_ast: Fragments::Preprocessor.run!(origin))
-    old_logger    = config.logger
-    config.logger = Logger.new(STDOUT)
+    # Do a dry-run to warm the cache up:
     result = Fragments::Show.run!(fragment_id: fragment.id, device: device)
     config.logger = old_logger
+    raise "Make sure no N+1s are introduced here."
    end
 end
