5702c4d2ac81f1a0bb9d856be1d4ebf09f111667
Rick Carlino <rick.carlino@gmail.com>
2019-01-31 11:24:10 -0600

    Yay, stuff works again! :tada:

--- frontend/password_reset/on_init.ts
+++ frontend/password_reset/on_init.ts
@@ -1,12 +1,12 @@
 import * as I18n from "i18next";
+import React from "react";
+import { render } from "react-dom";
+import { PasswordReset } from "./password_reset";
+import { bail } from "../util";
 
 export const MISSING_DIV = "Add a div with id `root` to the page first.";
 
 export const onInit: I18n.Callback = async () => {
-  const React = await import("react");
-  const { render } = await import("react-dom");
-  const { PasswordReset } = await import("./password_reset");
-  const { bail } = await import("../util");
   const node = document.createElement("DIV");
   node.id = "root";
   document.body.appendChild(node);
