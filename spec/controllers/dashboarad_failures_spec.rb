fffed2dd60d0e5cd12ba48866ffb743366ff94fa
Rick Carlino <rick.carlino@gmail.com>
2019-02-06 17:50:51 -0600

    Fix test breakage due to action name change.

--- spec/controllers/dashboarad_failures_spec.rb
+++ spec/controllers/dashboarad_failures_spec.rb
@@ -6,7 +6,7 @@ describe DashboardController do
   it 'can not re-verify' do
     user.update_attributes(confirmed_at: Time.now)
     sign_in user
-    get :verify, params: { token: user.confirmation_token }
+    get :confirmation_page, params: { token: user.confirmation_token }
     expect(response.status).to eq(409)
     expect(response.body).to include("already verified")
   end
