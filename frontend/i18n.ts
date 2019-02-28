6a9770a98919b9f1c96785eacf45e2baf32e530a
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2019-02-28 15:31:42 -0800

    i18n fix

--- frontend/i18n.ts
+++ frontend/i18n.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 import { InitOptions } from "i18next";
+import { merge } from "lodash";
+
+const translationFilePath = (lang: string): string =>
+  `/app-resources/languages/${lang}.json`;
 
 /** @public */
 export function generateUrl(langCode: string, host: string, port: string) {
   const lang = langCode.slice(0, 2);
   const baseUrl = `//${host.split(":")[0]}:${port}`;
-  const url = `${baseUrl}/app-resources/languages/${lang}.js`;
+  const url = `${baseUrl}${translationFilePath(lang)}`;
   return url;
 }
 
@@ -16,15 +20,22 @@ export function getUserLang(
     .catch(() => "en");
 }
 
+type TranslationFile = { [cat: string]: { [key: string]: string } };
+type Translations = { [key: string]: string };
+
+const parseTranslationData = (data: TranslationFile): Translations =>
+  merge(data["translated"], data["untranslated"], data["other_translations"]);
+
 export function generateI18nConfig(lang: string): Promise<InitOptions> {
   return axios
-    .get<string>(`/app-resources/languages/${lang}.js`)
-    .then(_x => {
+    .get<TranslationFile>(translationFilePath(lang))
+    .then(response => {
+      const translation = parseTranslationData(response.data);
       return {
         nsSeparator: "",
         keySeparator: "",
         lng: lang,
-        resources: { [lang]: { translation: {} } }
+        resources: { [lang]: { translation } }
       };
     });
 }
--- public/app-resources/languages/en.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    // Nothing to do here...
-}
