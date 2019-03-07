0e17ebfd0e7546b8dae933f64ddf966ad09bf8a5
Rick Carlino <rick.carlino@gmail.com>
2019-03-07 15:44:04 -0600

    internal_use column

--- spec/controllers/api/configs/web_app_configs_controller_spec.rb
+++ spec/controllers/api/configs/web_app_configs_controller_spec.rb
@@ -1,46 +1,45 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Api::WebAppConfigsController do
-
   include Devise::Test::ControllerHelpers
 
   let(:user) { FactoryBot.create(:user) }
   let(:device) { user.device }
 
-  describe '#show' do
-    it 'handles requests' do
+  describe "#show" do
+    it "handles requests" do
       sign_in user
       device.web_app_config.destroy! # Let's test defaults.
       get :show, format: :json
       expect(response.status).to eq(200)
       {
-        :confirm_step_deletion => false,
-        :disable_animations    => false,
-        :disable_i18n          => false,
-        :display_trail         => false,
-        :dynamic_map           => false,
-        :encoder_figure        => false,
-        :hide_webcam_widget    => false,
-        :legend_menu_open      => false,
-        :map_xl                => false,
-        :raw_encoders          => false,
-        :scaled_encoders       => false,
-        :show_spread           => false,
-        :show_farmbot          => true,
-        :show_plants           => true,
-        :show_points           => true,
-        :x_axis_inverted       => false,
-        :y_axis_inverted       => false,
-        :z_axis_inverted       => false,
-        :bot_origin_quadrant   => 2,
-        :zoom_level            => 1,
-        :success_log           => 1,
-        :busy_log              => 1,
-        :warn_log              => 1,
-        :error_log             => 1,
-        :info_log              => 1,
-        :fun_log               => 1,
-        :debug_log             => 1,
+        confirm_step_deletion: false,
+        disable_animations: false,
+        disable_i18n: false,
+        display_trail: false,
+        dynamic_map: false,
+        encoder_figure: false,
+        hide_webcam_widget: false,
+        legend_menu_open: false,
+        map_xl: false,
+        raw_encoders: false,
+        scaled_encoders: false,
+        show_spread: false,
+        show_farmbot: true,
+        show_plants: true,
+        show_points: true,
+        x_axis_inverted: false,
+        y_axis_inverted: false,
+        z_axis_inverted: false,
+        bot_origin_quadrant: 2,
+        zoom_level: 1,
+        success_log: 1,
+        busy_log: 1,
+        warn_log: 1,
+        error_log: 1,
+        info_log: 1,
+        fun_log: 1,
+        debug_log: 1,
       }.to_a.map { |key, value| expect(json[key]).to eq(value) }
 
       { created_at: String, updated_at: String }
@@ -48,10 +47,12 @@ describe Api::WebAppConfigsController do
     end
   end
 
-  describe '#update' do
-    it 'handles update requests' do
+  describe "#update" do
+    it "handles update requests" do
       sign_in user
-      body = { info_log: 23, bot_origin_quadrant: -1 }
+      body = { info_log: 23, bot_origin_quadrant: -1, internal_use: "null" }
+      calls = { object_id: user.device.web_app_config.id }
+      expect(Rollbar).to receive(:error).with("Internal data update use", calls)
       body.to_a.map { |key, val| expect(device.web_app_config.send(key)).not_to eq(val) }
       put :update, body: body.to_json, params: { format: :json }
       expect(response.status).to eq(200)
@@ -62,7 +63,7 @@ describe Api::WebAppConfigsController do
       end
     end
 
-    it 'disallows mass assignment attacks against device_id' do
+    it "disallows mass assignment attacks against device_id" do
       sign_in user
       body = { device_id: 99 }
       conf = device.web_app_config
@@ -73,8 +74,8 @@ describe Api::WebAppConfigsController do
     end
   end
 
-  describe '#delete' do
-    it 'resets everything to the defaults' do
+  describe "#delete" do
+    it "resets everything to the defaults" do
       sign_in user
       old_conf = device.web_app_config
       old_conf.update_attributes(zoom_level: 23)
