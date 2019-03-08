cbeabeea2f558b38ab99d89e7393438edf47dcb9
Rick Carlino <rick.carlino@gmail.com>
2019-03-08 08:21:47 -0600

    NervesHub internal NPE hotfix

--- app/jobs/send_nerves_hub_info_job.rb
+++ app/jobs/send_nerves_hub_info_job.rb
@@ -2,15 +2,16 @@ class SendNervesHubInfoJob < ApplicationJob
   queue_as :default
 
   def perform(device_id:, serial_number:, tags:)
-    device    = Device.find(device_id)
-    resp_data = NervesHub.create_or_update(serial_number, tags)
-    certs     = NervesHub.sign_device(resp_data.fetch(:identifier))
+    device = Device.find(device_id)
+    resp_data = NervesHub.maybe_create_or_update(serial_number, tags)
+    return unless resp_data # Probably has bad tags if nil
+    certs = NervesHub.sign_device(resp_data.fetch(:identifier))
     Transport.current.amqp_send(certs.to_json, device_id, "nerves_hub")
   rescue => error
-    NervesHub.report_problem({ error:         error,
-                               device_id:     device_id,
+    NervesHub.report_problem({ error: error,
+                               device_id: device_id,
                                serial_number: serial_number,
-                               tags:          tags, })
+                               tags: tags })
     raise error
   end
 end
--- spec/jobs/send_nerves_hub_info_job_spec.rb
+++ spec/jobs/send_nerves_hub_info_job_spec.rb
@@ -4,14 +4,15 @@ describe SendNervesHubInfoJob do
   let(:device) { FactoryBot.create(:device) }
 
   it "handles failure" do
-    params = { device_id:     device.id,
-               serial_number: "xyz",
-               tags:          [],
-               error:         StandardError.new("Hello!"), }
-    not_work = \
-      receive(:create_or_update).with(any_args).and_raise(params.fetch(:error))
+    params = { device_id: device.id,
+              serial_number: "xyz",
+              tags: [],
+              error: StandardError.new("Hello!") }
+    not_work = receive(:maybe_create_or_update)
+      .with(any_args)
+      .and_raise(params.fetch(:error))
     expect(NervesHub).to not_work
-    old_logger             = ActiveJob::Base.logger
+    old_logger = ActiveJob::Base.logger
     ActiveJob::Base.logger = Logger.new(nil)
     expect do
       SendNervesHubInfoJob.perform_now(**params.except(:error))
