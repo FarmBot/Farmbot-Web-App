e7bf038bee97cf8f11834e679972298f624f8e2b
Rick Carlino <rick.carlino@gmail.com>
2019-04-17 13:48:54 -0700

    Update rack middleware config

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,18 +1,19 @@
-class Rack::Attack
-  ### Throttle Spammy Clients ###
-  throttle('req/ip', limit: 1000, period: 1.minutes) do |req|
-    req.ip
-  end
+# RC 17 APR 19
+# class Rack::Attack
+#   ### Throttle Spammy Clients ###
+#   throttle('req/ip', limit: 1000, period: 1.minutes) do |req|
+#     req.ip
+#   end
 
-  ### Stop people from overusing the sync object. ###
-  throttle('sync_req/ip', limit: 5, period: 1.minutes) do |req|
-    req.ip if req.url.include?("/sync")
-  end
-end
+#   ### Stop people from overusing the sync object. ###
+#   throttle('sync_req/ip', limit: 5, period: 1.minutes) do |req|
+#     req.ip if req.url.include?("/sync")
+#   end
+# end
 
-# Always allow requests from localhost
-# (blacklist & throttles are skipped)
-Rack::Attack.safelist('allow from localhost') do |req|
-  # Requests are allowed if the return value is truthy
-  '127.0.0.1' == req.ip || '::1' == req.ip
-end
+# # Always allow requests from localhost
+# # (blacklist & throttles are skipped)
+# Rack::Attack.safelist('allow from localhost') do |req|
+#   # Requests are allowed if the return value is truthy
+#   '127.0.0.1' == req.ip || '::1' == req.ip
+# end
