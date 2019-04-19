e3abb4bf6a3d6028b64adb2bc9cfb42904d8108b
Rick Carlino <rick.carlino@gmail.com>
2019-04-19 13:35:26 -0700

    WIP

--- app/mutations/alerts/create.rb
+++ app/mutations/alerts/create.rb
@@ -5,10 +5,12 @@ module Alerts
       string :problem_tag, in: Alert::PROBLEM_TAGS
     end
 
+    optional { string :slug }
+
     def execute
       Alert.create!(device: device,
-                     problem_tag: problem_tag,
-                     slug: SecureRandom.uuid)
+                    problem_tag: problem_tag,
+                    slug: slug || SecureRandom.uuid)
     end
   end
 end
