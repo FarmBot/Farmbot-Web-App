8d9cf27e8aae32a56f802442158894d9daee9bb8
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2019-05-02 18:37:10 -0700

    reset account UI

--- frontend/account/__tests__/actions_test.ts
+++ frontend/account/__tests__/actions_test.ts
@@ -1,6 +1,6 @@
 import { API } from "../../api/api";
 import * as moxios from "moxios";
-import { deleteUser } from "../actions";
+import { deleteUser, resetAccount } from "../actions";
 
 describe("deleteUser()", () => {
   beforeEach(function () {
@@ -32,6 +32,30 @@ describe("deleteUser()", () => {
         expect(window.alert).toHaveBeenCalled();
         expect(resp.config.url).toContain("api/users");
         expect(resp.config.method).toBe("delete");
+        done();
+      });
+    });
+  });
+
+  it("resets the account", (done) => {
+    expect.assertions(3);
+    API.setBaseUrl("http://example.com:80");
+    const thunk = resetAccount({ password: "Foo!" });
+    const dispatch = jest.fn();
+    const getState = jest.fn();
+    getState.mockImplementation(() => ({ auth: {} }));
+    window.alert = jest.fn();
+    thunk(dispatch, getState);
+
+    moxios.wait(function () {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: {}
+      }).then(function (resp) {
+        expect(window.alert).toHaveBeenCalled();
+        expect(resp.config.url).toContain("api/device/reset");
+        expect(resp.config.method).toBe("post");
         done();
       });
     });
