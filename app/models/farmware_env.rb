75059ccf0f33c44a6480cbc85b382d6bdc366cbe
Rick Carlino <rick.carlino@gmail.com>
2019-05-22 15:36:09 -0500

    Keep using YAML serialization, actually

--- app/models/farmware_env.rb
+++ app/models/farmware_env.rb
@@ -1,5 +1,5 @@
 # User definable key/value pairs, usually used for Farmware authorship.
 class FarmwareEnv < ApplicationRecord
   belongs_to :device
-  serialize :value, JSON
+  serialize :value
 end
