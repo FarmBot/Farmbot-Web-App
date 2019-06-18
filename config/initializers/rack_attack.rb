4b0c65b88b0be44ed86cf627492f3a764a5542a4
Rick Carlino <rick.carlino@gmail.com>
2019-06-18 12:28:01 -0500

    Update Rack::Attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,18 +1,30 @@
 class Rack::Attack
   ### Throttle Spammy Clients ###
-  throttle('req/ip', limit: 2000, period: 1.minutes) do |req|
+  throttle("req/ip", limit: 2000, period: 1.minutes) do |req|
     req.ip
   end
 
   ### Stop people from overusing the sync object. ###
-  throttle('sync_req/ip', limit: 10, period: 1.minutes) do |req|
+  throttle("sync_req/ip", limit: 10, period: 1.minutes) do |req|
     req.ip if req.url.include?("/sync")
+  end
+
+  ### Don't allow too many guest accounts ###
+  throttle("demo_accounts/ip", limit: 10, period: 10.minutes) do |req|
+    req.ip if req.path.downcase == "/demo"
   end
 end
 
 # Always allow requests from localhost
 # (blacklist & throttles are skipped)
-Rack::Attack.safelist('allow from localhost') do |req|
+Rack::Attack.safelist("allow from localhost") do |req|
   # Requests are allowed if the return value is truthy
-  '127.0.0.1' == req.ip || '::1' == req.ip
+  "127.0.0.1" == req.ip || "::1" == req.ip
+end
+
+ActiveSupport::Notifications.subscribe("rack.attack") do |_n, _s, _f, _r, req|
+  req = req[:request]
+  if %i[throttle blocklist].include?(req.env["rack.attack.match_type"])
+    puts("BLOCKED BY RACK ATTACK: #{req.ip} => #{req.url}")
+  end
 end
