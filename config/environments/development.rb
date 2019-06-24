8c9d97d14acc042416f99c0711a2ce36461e50a6
Rick Carlino <rick.carlino@gmail.com>
2019-06-24 11:36:08 -0500

    Change default page for newly activated accounts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,18 +2,18 @@ FarmBot::Application.configure do
   config.action_controller.perform_caching = false
   config.action_mailer.default_url_options = {
     host: Rails.application.routes.default_url_options[:host],
-    port: Rails.application.routes.default_url_options[:port]
+    port: Rails.application.routes.default_url_options[:port],
   }
-  config.action_mailer.delivery_method     = :smtp
-  config.action_mailer.smtp_settings       = {
-    address: Rails.application.routes.default_url_options.fetch(:host),
-    port: 1025
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch("API_HOST"),
+    port: 1025,
   }
-  config.active_support.deprecation        = :log
-  config.assets.debug                      = true
-  config.assets.digest                     = true
-  config.assets.raise_runtime_errors       = true
-  config.cache_classes                     = false
-  config.consider_all_requests_local       = true
-  config.eager_load                        = false
+  config.active_support.deprecation = :log
+  config.assets.debug = true
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = true
+  config.cache_classes = false
+  config.consider_all_requests_local = true
+  config.eager_load = false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -107,6 +107,7 @@ FarmBot::Application.routes.draw do
 
   get "/app" => "dashboard#main_app", as: :dashboard
   get "/app/controls" => "dashboard#main_app", as: :app_landing_page
+  get "/app/messages" => "dashboard#main_app", as: :app_message_center
   match "/app/*path", to: "dashboard#main_app", via: :all, constraints: { format: "html" }
 
   get "/demo" => "dashboard#demo", as: :demo_main
