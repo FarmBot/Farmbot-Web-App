6220f31c5f827dd0204ec8ec5b1044a0aba628c1
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2019-06-26 12:34:43 -0700

    add PinMode test

--- frontend/sequences/step_tiles/__tests__/tile_read_pin_test.tsx
+++ frontend/sequences/step_tiles/__tests__/tile_read_pin_test.tsx
@@ -1,12 +1,17 @@
+jest.mock("../../../api/crud", () => ({ editStep: jest.fn() }));
+
 import * as React from "react";
-import { TileReadPin } from "../tile_read_pin";
-import { mount } from "enzyme";
+import { TileReadPin, PinMode } from "../tile_read_pin";
+import { mount, shallow } from "enzyme";
 import { fakeSequence } from "../../../__test_support__/fake_state/resources";
 import { ReadPin } from "farmbot/dist";
 import { emptyState } from "../../../resources/reducer";
+import { FBSelect } from "../../../ui";
+import { StepParams } from "../../interfaces";
+import { editStep } from "../../../api/crud";
 
 describe("<TileReadPin/>", () => {
-  function bootstrapTest() {
+  const fakeProps = (): StepParams => {
     const currentStep: ReadPin = {
       kind: "read_pin",
       args: {
@@ -16,18 +21,17 @@ describe("<TileReadPin/>", () => {
       }
     };
     return {
-      component: mount(<TileReadPin
-        currentSequence={fakeSequence()}
-        currentStep={currentStep}
-        dispatch={jest.fn()}
-        index={0}
-        resources={emptyState().index}
-        confirmStepDeletion={false} />)
+      currentSequence: fakeSequence(),
+      currentStep: currentStep,
+      dispatch: jest.fn(),
+      index: 0,
+      resources: emptyState().index,
+      confirmStepDeletion: false,
     };
-  }
+  };
 
   it("renders inputs", () => {
-    const block = bootstrapTest().component;
+    const block = mount(<TileReadPin {...fakeProps()} />);
     const inputs = block.find("input");
     const labels = block.find("label");
     const buttons = block.find("button");
@@ -40,5 +44,14 @@ describe("<TileReadPin/>", () => {
     expect(labels.at(2).text()).toEqual("Data Label");
     expect(inputs.at(1).props().value).toEqual("pinlabel");
     expect(buttons.at(0).text()).toEqual("Pin 3");
+  });
+
+  describe("<PinMode />", () => {
+    it("sets pin mode", () => {
+      const p = fakeProps();
+      const wrapper = shallow(<PinMode {...p} />);
+      wrapper.find(FBSelect).simulate("change", { label: "", value: 0 });
+      expect(editStep).toHaveBeenCalled();
+    });
   });
 });
