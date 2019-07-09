61bf5165edb5fbf0e06b9cbacb1f30f9d1917bef
Rick Carlino <rick.carlino@gmail.com>
2019-07-09 16:31:31 -0500

    Blooper cleanup pt I

--- db/views/in_use_tools_v01.sql
+++ db/views/in_use_tools_v01.sql
@@ -1,26 +1,10 @@
-WITH
-  resource_type_node
-  AS
-  (
-    SELECT primary_node_id, kind, value
-    FROM edge_nodes
-    WHERE (kind = 'resource_type')
-  ),
-  resource_id_node
-  AS
-  (
-    SELECT primary_node_id, kind, value, sequence_id
-    FROM edge_nodes
-    WHERE (kind = 'resource_id')
-  )
 SELECT
-  resource_id_node.sequence_id,
-  resource_id_node.primary_node_id,
-  resource_id_node.value::bigint as point_id,
-  resource_type_node.value AS point_type
-FROM
-  resource_id_node
-  INNER JOIN
-  resource_type_node
-  ON
-  resource_id_node.primary_node_id = resource_type_node.primary_node_id;
+  tools.id            as tool_id,
+  tools.name          as tool_name,
+  sequences.name      as sequence_name,
+  sequences.id        as sequence_id,
+  sequences.device_id as device_id
+FROM "edge_nodes"
+INNER JOIN "sequences" ON sequences.id=sequences.id
+INNER JOIN "tools"     ON (edge_nodes.value)::int=tools.id
+WHERE "edge_nodes"."kind" = 'tool_id';
