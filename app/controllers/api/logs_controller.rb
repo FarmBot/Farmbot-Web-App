0a6bbb4bfb477dcc807dbdbfb4f603dbb47ee540
Rick Carlino <rick.carlino@gmail.com>
2019-08-07 08:15:08 -0500

    Ability to search a subset of log fields on Logs#search. Fixes #1354

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -2,21 +2,31 @@ module Api
   class LogsController < Api::AbstractController
     before_action :trim_logs
 
+    SEARCH_FIELDS = {
+      message: :to_s,
+      type: :to_s,
+      verbosity: :to_i,
+      x: :to_i,
+      y: :to_i,
+      z: :to_i,
+    }
+
     def search
-      conf       = current_device.web_app_config
-      mt         = CeleryScriptSettingsBag::ALLOWED_MESSAGE_TYPES
-      query      = mt
-                    .map { |x| "(type = '#{x}' AND verbosity <= ?)" }
-                    .join(" OR ")
-      conditions = mt.map { |x| "#{x}_log" }.map{|x| conf.send(x) }
-      args_      = conditions.unshift(query)
-      limit      = current_device.max_log_count || Device::DEFAULT_MAX_LOGS
+      conf = current_device.web_app_config
+      mt = CeleryScriptSettingsBag::ALLOWED_MESSAGE_TYPES
+      query = mt
+        .map { |x| "(type = '#{x}' AND verbosity <= ?)" }
+        .join(" OR ")
+      conditions = mt.map { |x| "#{x}_log" }.map { |x| conf.send(x) }
+      args_ = conditions.unshift(query)
+      limit = current_device.max_log_count || Device::DEFAULT_MAX_LOGS
 
       render json: current_device
-        .logs
-        .order(created_at: :desc)
-        .where(*args_)
-        .limit(limit)
+               .logs
+               .order(created_at: :desc)
+               .where(*args_)
+               .limit(limit)
+               .where(search_params)
     end
 
     def create
@@ -32,13 +42,23 @@ module Api
       render json: current_device.logs.destroy_all && ""
     end
 
-  private
+    private
 
     def trim_logs
       # WARNING: Calls to `destroy_all` rather than
       #   `delete_all` can be disastrous- this is
       #   a big table! RC
       current_device.excess_logs.delete_all
+    end
+
+    def search_params
+      SEARCH_FIELDS.reduce({}) do |acc, (k, v)|
+        search_term = params[k]
+        if search_term
+          acc[k] = search_term.send(v)
+        end
+        acc
+      end
     end
   end
 end
