510164403af6691954071132b3841afe6d207072
Rick Carlino <rick.carlino@gmail.com>
2019-08-20 11:44:24 -0500

    Extract conditionals into different React components

--- frontend/farm_designer/point_groups/__tests__/actions_test.ts
+++ frontend/farm_designer/point_groups/__tests__/actions_test.ts
@@ -13,13 +13,6 @@ import { initSave } from "../../../api/crud";
 import { history } from "../../../history";
 
 describe("group action creators and thunks", () => {
-  // const all = points.map(x => unpackUUID(x).remoteId);
-  // const point_ids = betterCompact(all);
-  // const group: PointGroup = ({ name: name || UNTITLED, point_ids });
-  // const thunk = initSave("PointGroup", group);
-  // const p: Promise<{}> = thunk(dispatch);
-  // return p.then(() => history.push("/app/designer/groups"));
-
   it("creates groups", async () => {
     const points: string[] =
       ["Point.4.5", "Point.6.7", "Point.8.9"];
---
+++ frontend/farm_designer/point_groups/group_detail_active.tsx
@@ -0,0 +1,166 @@
+import * as React from "react";
+import { Panel } from "../panel_header";
+import { t } from "../../i18next_wrapper";
+import {
+  DesignerPanel,
+  DesignerPanelContent,
+  DesignerPanelHeader
+} from "../plants/designer_panel";
+import { TaggedPointGroup, TaggedPoint, SpecialStatus } from "farmbot";
+import { findByKindAndId } from "../../resources/selectors";
+import { DeleteButton } from "../../controls/pin_form_fields";
+import { svgToUrl, DEFAULT_ICON } from "../../open_farm/icons";
+import { overwrite, save, edit } from "../../api/crud";
+import { Dictionary } from "lodash";
+import { TaggedPlant } from "../map/interfaces";
+import { cachedCrop } from "../../open_farm/cached_crop";
+import { toggleHoveredPlant } from "../actions";
+import { ResourceIndex } from "../../resources/interfaces";
+
+interface GroupDetailActiveProps {
+  dispatch: Function;
+  group: TaggedPointGroup;
+  points: TaggedPoint[];
+}
+
+interface State {
+  icons: Dictionary<string | undefined>
+}
+
+export function fetchGroupFromUrl(index: ResourceIndex) {
+  /** TODO: Write better selectors. */
+  const groupId = parseInt(location.pathname.split("/").pop() || "?", 10);
+  let group: TaggedPointGroup | undefined;
+  try {
+    group =
+      findByKindAndId<TaggedPointGroup>(index, "PointGroup", groupId);
+  } catch (error) {
+    group = undefined;
+  }
+  return group;
+}
+
+export class GroupDetailActive extends React.Component<GroupDetailActiveProps, State> {
+
+  state: State = { icons: {} };
+
+  update = ({ currentTarget }: React.SyntheticEvent<HTMLInputElement>) => {
+    this
+      .props
+      .dispatch(edit(this.props.group, { name: currentTarget.value }));
+  };
+
+  performLookup = (plant: TaggedPlant) => {
+    cachedCrop(plant.body.openfarm_slug)
+      .then(x => {
+        this.setState({
+          icons: {
+            ...this.state.icons,
+            [plant.uuid]: x.svg_icon
+          }
+        });
+      });
+    return DEFAULT_ICON;
+  }
+
+  findIcon = (plant: TaggedPlant) => {
+    const svg = this.state.icons[plant.uuid];
+    return svg ? svgToUrl(svg) : this.performLookup(plant);
+  }
+
+  get name() {
+    const { group } = this.props;
+    return group ? group.body.name : "Group Not found";
+  }
+
+  get icons() {
+    return this
+      .props
+      .points
+      .map(point => {
+        const { body } = point;
+        switch (body.pointer_type) {
+          case "GenericPointer":
+            return <i key={point.uuid} className="fa fa-dot-circle-o" />;
+          case "ToolSlot":
+            return <i key={point.uuid} className="fa fa-leaf" />;
+          case "Plant":
+            const p = point as TaggedPlant;
+            const icon = this.findIcon(p);
+            const plantUUID = point.uuid;
+            return <span
+              onMouseEnter={() => {
+                this.props.dispatch(toggleHoveredPlant(plantUUID, icon));
+              }}
+              onMouseLeave={() => {
+                this.props.dispatch(toggleHoveredPlant(undefined, icon));
+              }}
+              key={plantUUID}
+              onClick={() => this.removePoint(body.id || 0)}>
+              <img
+                src={icon}
+                alt={p.body.name}
+                width={32}
+                height={32} />
+            </span>;
+        }
+      });
+  }
+
+  removePoint = (pointId: number) => {
+    const { group } = this.props;
+    if (group) {
+      type Body = (typeof group)["body"];
+      const nextGroup: Body = { ...group.body };
+      nextGroup.point_ids = nextGroup.point_ids.filter(x => x !== pointId);
+      this.props.dispatch(overwrite(group, nextGroup));
+    }
+  }
+
+  saveGroup = () => {
+    const { group } = this.props;
+    group && this.props.dispatch(save(group.uuid));
+  }
+
+  /** TODO: Add undo feature to ResourceReducer */
+  componentWillUnmount = () => { this.saveGroup(); };
+
+  render() {
+    const { group } = this.props;
+    return <DesignerPanel panelName={"groups"} panelColor={"blue"}>
+      <DesignerPanelHeader
+        panelName={Panel.Groups}
+        panelColor={"blue"}
+        title={t("Edit Group")}
+        backTo={"/app/designer/groups"}>
+        <a
+          className="right-button"
+          title={t("Save Changes to Group")}
+          onClick={this.saveGroup}>
+          {t("Save")}{group.specialStatus === SpecialStatus.SAVED ? "" : "*"}
+        </a>
+      </DesignerPanelHeader>
+      <DesignerPanelContent
+        panelName={"groups"}>
+        <h5>{t("GROUP NAME")}</h5>
+        <input
+          defaultValue={this.name}
+          onChange={this.update}
+        />
+        <h5>{t("GROUP MEMBERS ({{count}})", { count: this.icons.length })}</h5>
+        <p>
+          {t("Click plants in map to add or remove.")}
+        </p>
+        <div>
+          {this.icons}
+        </div>
+        <DeleteButton
+          dispatch={this.props.dispatch}
+          uuid={group.uuid}
+          onDestroy={history.back}>
+          {t("DELETE GROUP")}
+        </DeleteButton>
+      </DesignerPanelContent>
+    </DesignerPanel>;
+  }
+}
