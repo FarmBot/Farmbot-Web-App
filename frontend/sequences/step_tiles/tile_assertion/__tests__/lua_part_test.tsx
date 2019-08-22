a4ca4ba71e681673661375e7182460f01a4ec78f
Rick Carlino <rick.carlino@gmail.com>
2019-08-22 16:05:47 -0500

    WIP.

---
+++ frontend/sequences/step_tiles/tile_assertion/__tests__/lua_part_test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { LuaPart } from "../lua_part";
+import { shallow } from "enzyme";
+import { fakeAssertProps } from "./test_support";
+import { ReduxAction } from "../../../../redux/interfaces";
+import { EditResourceParams } from "../../../../api/interfaces";
+import { Actions } from "../../../../constants";
+import { TaggedSequence } from "farmbot";
+
+describe("<LuaPart/>", () => {
+  it("renders default verbiage and props", () => {
+    const p =
+      fakeAssertProps({ confirmStepDeletion: true });
+    expect(p.confirmStepDeletion).toBe(true);
+    const el = shallow(<LuaPart {...p} />);
+    const fakeEvent =
+      ({ currentTarget: { value: "hello" } });
+    el.find("textarea").first().simulate("blur", fakeEvent);
+    expect(p.dispatch).toHaveBeenCalled();
+    const calledWith: ReduxAction<EditResourceParams> | undefined =
+      (p.dispatch as jest.Mock).mock.calls[0][0];
+    if (calledWith) {
+      expect(calledWith.type).toEqual(Actions.OVERWRITE_RESOURCE);
+      expect(calledWith.payload.uuid).toEqual(p.currentSequence.uuid);
+      const s = calledWith.payload.update as TaggedSequence;
+      expect(s).toBeTruthy();
+      console.log("I STOPPED HERE. I NEED TO FINISH THIS TEST");
+    } else {
+      fail();
+    }
+  });
+});
