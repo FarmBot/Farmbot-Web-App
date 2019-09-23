fa5f9ed1ff7848cde752ec688a6053699d90f005
Rick Carlino <rick.carlino@gmail.com>
2019-09-23 18:34:37 -0500

    PointGroup related additions

---
+++ spec/controllers/api/point_groups/show_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe Api::PointGroupsController do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:device) { user.device }
+  let(:old_point_ids) do rando_points end
+
+  it "shows a single point group" do
+    PointGroup.destroy_all
+    PointGroupItem.destroy_all
+    sign_in device.users.first
+    pg = PointGroups::Create
+      .run!(device: device, point_ids: [], name: "PointGroups#show test")
+    get :show, params: { id: pg.id }
+    expect(response.status).to eq(200)
+    expect(json.fetch(:name)).to eq pg.name
+  end
+end
