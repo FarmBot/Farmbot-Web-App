9a5939571586aab18a986664b4e70f5d9b143196
Rick Carlino <rick.carlino@gmail.com>
2019-09-24 11:05:43 -0500

    Merge conflicts, part II

--- frontend/resources/__tests__/sequence_meta_test.ts
+++ frontend/resources/__tests__/sequence_meta_test.ts
@@ -70,15 +70,7 @@ describe("determineDropdown", () => {
       kind: "parameter_application",
       args: {
         label: "x",
-<<<<<<< HEAD
         data_value: { kind: "every_point", args: { every_point_type: "Plant" } }
-=======
-        data_value: {
-          kind: "every_point",
-          args: { every_point_type: "Plant" }
-          // tslint:disable-next-line:no-any
-        } as any
->>>>>>> a3d73b35c6816f37dd0ad295d5fb9462ee24ed02
       }
     }, buildResourceIndex([]).index);
     expect(r.label).toBe("All plants");
