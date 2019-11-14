784ddda2aaafd13c46022fcc9dbb10a430c444a2
Rick Carlino <rick.carlino@gmail.com>
2019-11-14 10:32:28 -0600

    initPlantGrid() is ready for use.

--- frontend/farm_designer/plants/grid/generate_grid.ts
+++ frontend/farm_designer/plants/grid/generate_grid.ts
@@ -1,6 +1,7 @@
-import { TaggedPlant } from "../../map/interfaces";
 import { PlantGridData } from "./constants";
 import { range } from "lodash";
+import { uuid } from "farmbot";
+import { PlantPointer } from "farmbot/dist/resources/api_resources";
 
 export function vectorGrid(params: PlantGridData): [number, number][] {
   const rows = range(params.startX,
@@ -18,7 +19,24 @@ export function vectorGrid(params: PlantGridData): [number, number][] {
   return results;
 }
 
-export function initPlantGrid(_params: PlantGridData): TaggedPlant[] {
-  const output: TaggedPlant[] = [];
-  return output;
+const createPlant = (p: PlantGridData, openfarm_slug: string, gridId: string) =>
+  (vec: [number, number]): PlantPointer => {
+    const [x, y] = vec;
+    return {
+      name: `${openfarm_slug} ${p.numPlantsH}x${p.numPlantsV}`,
+      radius: 0,
+      z: 0,
+      x,
+      y,
+      openfarm_slug,
+      pointer_type: "Plant",
+      plant_stage: "planted",
+      meta: { gridId }
+    };
+  };
+
+export function initPlantGrid(p: PlantGridData, openfarm_slug: string): PlantPointer[] {
+  const gridId = uuid();
+  const mapper = createPlant(p, openfarm_slug, gridId);
+  return vectorGrid(p).map(mapper);
 }
