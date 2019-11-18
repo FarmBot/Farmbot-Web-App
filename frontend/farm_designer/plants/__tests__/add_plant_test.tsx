ad5b8cca371530caa5b5f5a5acdb515ec22c0b10
Rick Carlino <rick.carlino@gmail.com>
2019-11-18 13:38:23 -0600

    mapStateToProps tests.

--- frontend/farm_designer/plants/__tests__/add_plant_test.tsx
+++ frontend/farm_designer/plants/__tests__/add_plant_test.tsx
@@ -6,12 +6,14 @@ jest.mock("../../../history", () => ({
 
 import * as React from "react";
 import { mount } from "enzyme";
-import { RawAddPlant as AddPlant, AddPlantProps } from "../add_plant";
+import { RawAddPlant as AddPlant, AddPlantProps, mapStateToProps } from "../add_plant";
 import { history } from "../../../history";
 import {
   fakeCropLiveSearchResult
 } from "../../../__test_support__/fake_crop_search_result";
 import { svgToUrl } from "../../../open_farm/icons";
+import { fakeState } from "../../../__test_support__/fake_state";
+import { CropLiveSearchResult } from "../../interfaces";
 
 describe("<AddPlant />", () => {
   const fakeProps = (): AddPlantProps => {
@@ -41,5 +43,28 @@ describe("<AddPlant />", () => {
     doneBtn.simulate("click");
     expect(history.push).toHaveBeenCalledWith(
       "/app/designer/plants/crop_search/mint");
+  });
+});
+
+describe("mapStateToProps", () => {
+  it("maps state to props", () => {
+    const state = fakeState();
+    const crop: CropLiveSearchResult = {
+      crop: {
+        name: "fake",
+        slug: "fake",
+        binomial_name: "fake",
+        common_names: ["fake"],
+        description: "",
+        sun_requirements: "",
+        sowing_method: "",
+        processing_pictures: 0
+      },
+      image: "X"
+    };
+    state.resources.consumers.farm_designer.cropSearchResults = [crop];
+    const results = mapStateToProps(state);
+    expect(results.cropSearchResults).toEqual([crop]);
+    expect(results.xy_swap).toEqual(false);
   });
 });
