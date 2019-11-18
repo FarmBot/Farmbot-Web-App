dbb330efcfc197ca8062d1c83928868cb72a08ea
Rick Carlino <rick.carlino@gmail.com>
2019-11-18 17:57:03 -0600

    stashGrid tests

--- frontend/farm_designer/plants/grid/__tests__/thunks_test.ts
+++ frontend/farm_designer/plants/grid/__tests__/thunks_test.ts
@@ -3,33 +3,43 @@ const mockSaveAllReturnValue = { mock: "yep" };
 jest.mock("../../../../api/crud", () => {
   return {
     // Thunks are hard to test.
-    saveAll: jest.fn(() => mockSaveAllReturnValue)
+    saveAll: jest.fn(() => mockSaveAllReturnValue),
+    destroy: jest.fn()
   };
 });
 
-import { saveGrid } from "../thunks";
+import { saveGrid, stashGrid } from "../thunks";
 import { buildResourceIndex } from "../../../../__test_support__/resource_index_builder";
 import { fakePlant } from "../../../../__test_support__/fake_state/resources";
 import { fakeState } from "../../../../__test_support__/fake_state";
-import { saveAll } from "../../../../api/crud";
+import { saveAll, destroy } from "../../../../api/crud";
 
 const GRID_ID = "1234567";
 const PLANT = fakePlant();
 PLANT.body.meta["gridId"] = GRID_ID;
 
-const fakeResourceIndex = () => buildResourceIndex([PLANT]);
+const fakeResourceIndex = () => {
+  const state = fakeState();
+  state.resources = buildResourceIndex([PLANT]);
+  return state;
+}
 
 describe("saveGrid", () => {
   it("saves a particular grid", () => {
     const thunk = saveGrid(GRID_ID);
-    const state = fakeState();
     const dispatch = jest.fn();
-    state.resources = fakeResourceIndex();
+    const state = fakeResourceIndex();
     thunk(dispatch, jest.fn(() => state));
     expect(saveAll).toHaveBeenLastCalledWith([PLANT]);
     expect(dispatch).toHaveBeenCalledWith(mockSaveAllReturnValue);
   });
 });
 
-// describe("stashGrid", () => {
-// });
+describe("stashGrid", () => {
+  it("removes grids that the user doesn't want", () => {
+    const thunk = stashGrid(GRID_ID);
+    const state = fakeResourceIndex();
+    thunk(jest.fn, jest.fn(() => state));
+    expect(destroy).toHaveBeenLastCalledWith(PLANT.uuid, true);
+  });
+});
