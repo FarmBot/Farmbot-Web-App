4f8e344c63b3deeeb5d13ad6b5a4c367c7a3d52c
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2019-12-02 12:53:19 -0800

    add zero and calibrate to command list

--- frontend/sequences/step_tiles/__tests__/tile_calibrate_test.tsx
+++ frontend/sequences/step_tiles/__tests__/tile_calibrate_test.tsx
@@ -1,26 +1,31 @@
 import * as React from "react";
-import { TileCalibrate } from "../tile_calibrate";
+import { TileCalibrate, CalibrateParams } from "../tile_calibrate";
 import { mount } from "enzyme";
 import { fakeSequence } from "../../../__test_support__/fake_state/resources";
-import { Calibrate } from "farmbot";
 import { emptyState } from "../../../resources/reducer";
-import { StepParams } from "../../interfaces";
+import { HardwareFlags } from "../../interfaces";
+import {
+  fakeHardwareFlags
+} from "../../../__test_support__/sequence_hardware_settings";
 
 describe("<TileCalibrate/>", () => {
-  const currentStep: Calibrate = {
-    kind: "calibrate",
-    args: {
-      axis: "all"
-    }
-  };
-
-  const fakeProps = (): StepParams => ({
+  const fakeProps = (): CalibrateParams => ({
     currentSequence: fakeSequence(),
-    currentStep: currentStep,
+    currentStep: { kind: "calibrate", args: { axis: "all" } },
     dispatch: jest.fn(),
     index: 0,
     resources: emptyState().index,
     confirmStepDeletion: false,
+    hardwareFlags: fakeHardwareFlags(),
+  });
+
+  it("errors with incorrect kind", () => {
+    console.error = jest.fn();
+    const p = fakeProps();
+    // tslint:disable-next-line:no-any
+    p.currentStep.kind = "wrong" as any;
+    expect(() => mount(<TileCalibrate {...p} />))
+      .toThrowError("TileCalibrate expects calibrate");
   });
 
   it("renders inputs", () => {
@@ -32,5 +37,31 @@ describe("<TileCalibrate/>", () => {
     expect(inputs.first().props().placeholder).toEqual("Calibrate");
     expect(labels.at(0).text()).toContain("Calibrate x");
     expect(inputs.at(1).props().value).toEqual("x");
+  });
+
+  const CONFLICT_TEXT_BASE = "Hardware setting conflict";
+
+  it("doesn't render warning", () => {
+    const p = fakeProps();
+    p.currentStep.args.axis = "x";
+    (p.hardwareFlags as HardwareFlags).findHomeEnabled.x = true;
+    const wrapper = mount(<TileCalibrate {...p} />);
+    expect(wrapper.text()).not.toContain(CONFLICT_TEXT_BASE);
+  });
+
+  it("renders warning: all axes", () => {
+    const p = fakeProps();
+    p.currentStep.args.axis = "all";
+    (p.hardwareFlags as HardwareFlags).findHomeEnabled.x = false;
+    const wrapper = mount(<TileCalibrate {...p} />);
+    expect(wrapper.text()).toContain(CONFLICT_TEXT_BASE + ": x");
+  });
+
+  it("renders warning: one axis", () => {
+    const p = fakeProps();
+    p.currentStep.args.axis = "x";
+    (p.hardwareFlags as HardwareFlags).findHomeEnabled.x = false;
+    const wrapper = mount(<TileCalibrate {...p} />);
+    expect(wrapper.text()).toContain(CONFLICT_TEXT_BASE + ": x");
   });
 });
--- frontend/sequences/step_tiles/tile_calibrate.tsx
+++ frontend/sequences/step_tiles/tile_calibrate.tsx
@@ -1,29 +1,93 @@
 import * as React from "react";
-import { StepParams } from "../interfaces";
-import { ToolTips } from "../../constants";
-import { StepWrapper, StepHeader, StepContent } from "../step_ui/index";
+import { StepParams, HardwareFlags } from "../interfaces";
+import { ToolTips, Content } from "../../constants";
+import {
+  StepWrapper, StepHeader, StepContent, conflictsString, StepWarning
+} from "../step_ui/index";
 import { StepRadio } from "../step_ui/step_radio";
 import { t } from "../../i18next_wrapper";
+import { Xyz, Calibrate, TaggedSequence } from "farmbot";
+import { some } from "lodash";
+import { ResourceIndex } from "../../resources/interfaces";
 
 export function TileCalibrate(props: StepParams) {
-  const { dispatch, currentStep, index, currentSequence } = props;
-  const className = "calibrate-step";
-  return <StepWrapper>
-    <StepHeader
-      className={className}
-      helpText={ToolTips.CALIBRATION}
-      currentSequence={currentSequence}
-      currentStep={currentStep}
-      dispatch={dispatch}
-      index={index}
-      confirmStepDeletion={props.confirmStepDeletion} />
-    <StepContent className={className}>
-      <StepRadio
+  if (props.currentStep.kind === "calibrate") {
+    return <InnerTileCalibrate
+      currentStep={props.currentStep}
+      currentSequence={props.currentSequence}
+      dispatch={props.dispatch}
+      index={props.index}
+      resources={props.resources}
+      hardwareFlags={props.hardwareFlags}
+      confirmStepDeletion={props.confirmStepDeletion} />;
+  } else {
+    throw new Error("TileCalibrate expects calibrate");
+  }
+}
+
+export interface CalibrateParams {
+  currentStep: Calibrate;
+  currentSequence: TaggedSequence;
+  dispatch: Function;
+  index: number;
+  resources: ResourceIndex;
+  hardwareFlags: HardwareFlags | undefined;
+  confirmStepDeletion: boolean;
+}
+
+class InnerTileCalibrate extends React.Component<CalibrateParams, {}> {
+  get settingConflicts(): Record<Xyz, boolean> {
+    const conflicts = { x: false, y: false, z: false };
+    if (this.props.hardwareFlags) {
+      const { axis } = this.props.currentStep.args;
+      const { findHomeEnabled } = this.props.hardwareFlags;
+      switch (axis) {
+        case "x":
+        case "y":
+        case "z":
+          conflicts[axis] = !findHomeEnabled[axis];
+          break;
+        case "all":
+          conflicts.x = !findHomeEnabled.x;
+          conflicts.y = !findHomeEnabled.y;
+          conflicts.z = !findHomeEnabled.z;
+          break;
+      }
+    }
+    return conflicts;
+  }
+
+  get settingConflictWarning() {
+    return Content.END_DETECTION_DISABLED
+      + conflictsString(this.settingConflicts);
+  }
+
+  render() {
+
+    const { dispatch, currentStep, index, currentSequence } = this.props;
+    const className = "calibrate-step";
+    return <StepWrapper>
+      <StepHeader
+        className={className}
+        helpText={ToolTips.CALIBRATION}
         currentSequence={currentSequence}
         currentStep={currentStep}
         dispatch={dispatch}
         index={index}
-        label={t("Calibrate")} />
-    </StepContent>
-  </StepWrapper>;
+        confirmStepDeletion={this.props.confirmStepDeletion}>
+        {some(this.settingConflicts) &&
+          <StepWarning
+            warning={this.settingConflictWarning}
+            conflicts={this.settingConflicts} />}
+      </StepHeader>
+      <StepContent className={className}>
+        <StepRadio
+          currentSequence={currentSequence}
+          currentStep={currentStep}
+          dispatch={dispatch}
+          index={index}
+          label={t("Calibrate")} />
+      </StepContent>
+    </StepWrapper>;
+  }
 }
