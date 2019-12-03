fbc63ace3b0e6bb5f1c345717e0542d3e2e13976
Rick Carlino <rick.carlino@gmail.com>
2019-12-03 14:13:57 -0600

    Consolidate specs into single file

--- spec/controllers/api/folders/folders_spec.rb
+++ spec/controllers/api/folders/folders_spec.rb
@@ -4,7 +4,29 @@ describe Api::FoldersController do
   let(:user) { FactoryBot.create(:user) }
   include Devise::Test::ControllerHelpers
 
-  it "updates a folder URL" do
+  it "creates a webcam feed" do
+    sign_in user
+    parent = Folder.create!(name: "parent", color: "red", device: user.device)
+    input = {
+      parent_id: parent.id,
+      color: "blue",
+      name: "child"
+    }
+    b4 = Folder.count
+    expect(user.device.folders.count).to eq(1)
+    post :create, body: input.to_json
+    expect(response.status).to eq(200)
+    expect(Folder.count).to be > b4
+    expect(user.device.folders.count).to eq(2)
+    input.keys.map do |key|
+      expect(input[key]).to eq(json[key])
+    end
+  end
+
+  it "lists all folders" do
+  end
+
+  it "updates a folder" do
     # Create a folder first....
     sign_in user
     old_parent = Folder.create!(name: "B", device: user.device, color: "blue")
