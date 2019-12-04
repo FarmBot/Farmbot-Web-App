66afa66287859f60f4389bfff2aa4f9f6999c12a
Rick Carlino <rick.carlino@gmail.com>
2019-12-04 16:42:27 -0600

    Cruft removal

--- frontend/folders/index.tsx
+++ frontend/folders/index.tsx
@@ -4,63 +4,82 @@ import { FolderUnion, RootFolderNode } from "./constants";
 import { Everything } from "../interfaces";
 import { connect } from "react-redux";
 import { createFolder, deleteFolder, setFolderName } from "./actions";
+import { TaggedSequence } from "farmbot";
+import { selectAllSequences } from "../resources/selectors";
 
-type Props = RootFolderNode;
+interface Props extends RootFolderNode {
+  sequences: Record<string, TaggedSequence>;
+}
+
 type State = {};
 
-export class RawFolders extends React.Component<Props, State> {
-  Node = ({ node }: { node: FolderUnion }) => {
-    const style: React.CSSProperties =
-      { color: node.color, background: "black" };
-    const creates = () => createFolder({ parent_id: node.id });
-    const deletes = () => deleteFolder(node.id);
-    const subfolderBtn = <span>
-      <button onClick={creates}>
-        Subfolder
-      </button>
-    </span>;
+interface FolderNodeProps {
+  node: FolderUnion;
+  sequences: Record<string, TaggedSequence>;
+}
 
-    const deleteBtn = <span>
-      <button onClick={deletes}>
-        Delete
-      </button>
-    </span>;
+const FolderNode = ({ node, sequences }: FolderNodeProps) => {
+  const creates = () => createFolder({ parent_id: node.id });
+  const deletes = () => deleteFolder(node.id);
 
-    const inputBox = <span>
-      <BlurableInput
-        value={node.name}
-        onCommit={({ currentTarget }) => {
-          return setFolderName(node.id, currentTarget.value);
-        }} />
-    </span>;
+  const subfolderBtn = <a onClick={creates}>📁</a>;
+  const deleteBtn = <a onClick={deletes}>🗑️</a>;
+  const toggleBtn = <a onClick={() => { }}> {node.open ? "➕" : "➖"} </a>;
 
-    switch (node.kind) {
-      case "initial":
-        return <div style={style} >
-          {subfolderBtn}
-          {deleteBtn}
-          {inputBox}
-          {node.children.map((n2: FolderUnion) => <this.Node node={n2} key={n2.id} />)}
-        </div>;
-      case "medial":
-        return <div style={{ ...style, marginLeft: "30px" }} >
-          {subfolderBtn}
-          {deleteBtn}
-          {inputBox}
-          {node.children.map((n2: FolderUnion) => <this.Node node={n2} key={n2.id} />)}
-        </ div>;
-      case "terminal":
-        return <div style={{ ...style, marginLeft: "40px" }} >
-          {deleteBtn}
-          {inputBox}
-        </div>;
-    }
+  const inputBox = <BlurableInput
+    value={node.name}
+    onCommit={({ currentTarget }) => {
+      return setFolderName(node.id, currentTarget.value);
+    }} />;
+  const names = node
+    .content
+    .map(x => <li>*{sequences[x].body.name}</li>);
+
+  const children = <ul> {names} </ul>;
+
+  switch (node.kind) {
+    case "initial":
+      return <div>
+        {toggleBtn}
+        {subfolderBtn}
+        {deleteBtn}
+        {inputBox}
+        {children}
+        {node.children.map((n2: FolderUnion) => <FolderNode
+          node={n2}
+          key={n2.id}
+          sequences={sequences} />)}
+      </div>;
+    case "medial":
+      return <div style={{ marginLeft: "30px" }} >
+        {toggleBtn}
+        {subfolderBtn}
+        {deleteBtn}
+        {inputBox}
+        {children}
+        {node.children.map((n2: FolderUnion) => <FolderNode
+          node={n2}
+          key={n2.id}
+          sequences={sequences} />)}
+      </div>;
+    case "terminal":
+      return <div style={{ marginLeft: "40px" }} >
+        {toggleBtn}
+        {deleteBtn}
+        {inputBox}
+        {children}
+      </div>;
   }
+};
 
+export class RawFolders extends React.Component<Props, State> {
   Graph = (_props: {}) => {
     return <div>
       {this.props.folders.map(grandparent => {
-        return <this.Node node={grandparent} key={grandparent.id} />;
+        return <FolderNode
+          node={grandparent}
+          key={grandparent.id}
+          sequences={this.props.sequences} />;
       })}
     </div>;
   }
@@ -71,19 +90,16 @@ export class RawFolders extends React.Component<Props, State> {
         <Row>
           <input placeholder={"Search"} disabled={true} />
           <button onClick={() => createFolder()}>
-            Add Folder
+            ➕Folder
           </button>
           <button>
-            Add Sequence
+            ➕Sequence
           </button>
         </Row>
       </Col>
       <Col xs={12} sm={6} smOffset={3}>
-        <Row> <this.Graph /> </Row>
         <Row>
-          <pre>
-            {JSON.stringify(this.props.folders, undefined, "  ")}
-          </pre>
+          <this.Graph />
         </Row>
       </Col>
     </Page>;
@@ -91,7 +107,13 @@ export class RawFolders extends React.Component<Props, State> {
 }
 
 export function mapStateToProps(props: Everything): Props {
-  return props.resources.index.sequenceFolders;
+  return {
+    ...props.resources.index.sequenceFolders,
+    sequences: selectAllSequences(props.resources.index)
+      .reduce((a: Props["sequences"], b) => {
+        a[b.uuid] = b;
+        return a;
+      }, {})
+  };
 }
-
 export const Folders = connect(mapStateToProps)(RawFolders);
