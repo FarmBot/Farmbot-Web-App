a75a358a964156369b296d9d3a25f4d775ea8180
Rick Carlino <rick.carlino@gmail.com>
2019-12-13 14:04:48 -0600

    Tests for Actions.FOLDER_SEARCH

--- frontend/folders/__tests__/reducer_test.ts
+++ frontend/folders/__tests__/reducer_test.ts
@@ -12,16 +12,12 @@ import { Actions } from "../../constants";
 const f1 = fakeFolder({ name: "@" });
 const f2 = fakeFolder({ name: "#", parent_id: f1.body.id });
 const f3 = fakeFolder({ name: "$", parent_id: f2.body.id });
+const s1 = fakeSequence({ name: "%", folder_id: f1.body.id });
+const s2 = fakeSequence({ name: "^", folder_id: f2.body.id });
+const s3 = fakeSequence({ name: "&", folder_id: f3.body.id });
 
 function initialState(): RestResources {
-  return buildResourceIndex([
-    f1,
-    f2,
-    f3,
-    fakeSequence({ name: "%", folder_id: f1.body.id }),
-    fakeSequence({ name: "^", folder_id: f2.body.id }),
-    fakeSequence({ name: "&", folder_id: f3.body.id })
-  ]);
+  return buildResourceIndex([f1, f2, f3, s1, s2, s3,]);
 }
 
 describe("Actions.FOLDER_TOGGLE", () => {
@@ -64,7 +60,39 @@ describe("Actions.FOLDER_TOGGLE_EDIT", () => {
     const oldedit =
       state.index.sequenceFolders.localMetaAttributes[f1.body.id || 0];
     const nextedit = index.sequenceFolders.localMetaAttributes[f1.body.id || 0];
-
     expect(oldedit.editing).not.toEqual(nextedit.editing);
+  });
+});
+
+describe("Actions.FOLDER_SEARCH", () => {
+  it("searches folders", () => {
+    const state = initialState();
+    const action = { type: Actions.FOLDER_SEARCH, payload: "" };
+    const { index } = resourceReducer(state, action);
+    expect(index.sequenceFolders.filteredFolders).toBeUndefined();
+    expect(index.sequenceFolders.searchTerm).toBe("");
+
+    const action2 = { type: Actions.FOLDER_SEARCH, payload: "" };
+    const index2 = resourceReducer(state, action2).index;
+    expect(index2.sequenceFolders.searchTerm).toBe("");
+    expect(index2.sequenceFolders.filteredFolders).toBeUndefined();
+
+    const action3 = { type: Actions.FOLDER_SEARCH, payload: "&" };
+    const index3 = resourceReducer(state, action3).index;
+    expect(index3.sequenceFolders.searchTerm).toBe("&");
+    expect(index3.sequenceFolders.filteredFolders).not.toBeUndefined();
+    const { filteredFolders } = index3.sequenceFolders;
+    if (filteredFolders) {
+      expect(filteredFolders.noFolder.length).toEqual(0);
+      expect(filteredFolders.folders.length).toEqual(1);
+      const one = filteredFolders.folders[0];
+      const two = one.children[0];
+      const three = two.children[0];
+      const four = three.content[0];
+      expect(one.name).toBe("@");
+      expect(two.name).toBe("#");
+      expect(three.name).toBe("$");
+      expect(four).toBe(s3.uuid);
+    }
   });
 });
