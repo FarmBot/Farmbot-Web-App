bfe4df68a8d2a9a64fa1b954f0287fba2a5df69a
Rick Carlino <rick.carlino@gmail.com>
2020-04-12 10:41:53 -0500

    Fix typo in migration

--- db/migrate/20200412152208_update_max_image_count.rb
+++ db/migrate/20200412152208_update_max_image_count.rb
@@ -1,5 +1,5 @@
 class UpdateMaxImageCount < ActiveRecord::Migration[6.0]
   def change
-    change_column_default(:images, :max_images_count, 450)
+    change_column_default(:devices, :max_images_count, 450)
   end
 end
