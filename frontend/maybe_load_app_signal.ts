9bf06a177ad38585df29dc3cff3d35524b9bac64
Rick Carlino <rick.carlino@gmail.com>
2020-07-03 10:20:21 -0500

    Add loggers maybe

--- frontend/maybe_load_app_signal.ts
+++ frontend/maybe_load_app_signal.ts
@@ -2,16 +2,25 @@ export async function maybeLoadAppSignal() {
   const token = globalConfig["APPSIGNAL_TOKEN"];
 
   if (window.appSignal) {
+    console.log("Already have appSignal loaded.");
     return;
   }
 
   if (token) {
+    console.log("Load appsignal");
     const AppSignal = (await import("@appsignal/javascript")).default;
+    console.log("Load window events plugin");
     const { plugin } = await import("@appsignal/plugin-window-events");
+    console.log("instantiate appsignal");
     const as = new AppSignal({
       key: "YOUR FRONTEND API KEY"
     });
+    console.log("Use plugins");
     as.use(plugin);
+    console.log("Make it global");
     window.appSignal = as;
+    console.log("done");
+  } else {
+    console.log("No appsignal token detected.");
   }
 }
