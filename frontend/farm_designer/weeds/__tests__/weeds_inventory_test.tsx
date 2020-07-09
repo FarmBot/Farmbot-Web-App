19a5a244af7f1efc10529e834bf9a987f11ca93b
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2020-07-08 18:54:12 -0700

    weeds panel updates

--- frontend/farm_designer/weeds/__tests__/weeds_inventory_test.tsx
+++ frontend/farm_designer/weeds/__tests__/weeds_inventory_test.tsx
@@ -1,18 +1,33 @@
+jest.mock("../../../api/crud", () => ({
+  destroy: jest.fn(),
+  edit: jest.fn(),
+  save: jest.fn(),
+}));
+
 import * as React from "react";
 import { mount, shallow } from "enzyme";
 import {
   RawWeeds as Weeds, WeedsProps, mapStateToProps,
+  WeedsSection, WeedsSectionProps,
 } from "../weeds_inventory";
 import { fakeState } from "../../../__test_support__/fake_state";
-import { fakeWeed } from "../../../__test_support__/fake_state/resources";
+import {
+  fakeWeed, fakeWebAppConfig,
+} from "../../../__test_support__/fake_state/resources";
 import { SearchField } from "../../../ui/search_field";
 import { PointSortMenu } from "../../sort_options";
+import {
+  buildResourceIndex,
+} from "../../../__test_support__/resource_index_builder";
+import { BooleanSetting } from "../../../session_keys";
+import { destroy, edit, save } from "../../../api/crud";
 
 describe("<Weeds> />", () => {
   const fakeProps = (): WeedsProps => ({
     weeds: [],
     dispatch: jest.fn(),
     hoveredPoint: undefined,
+    getConfigValue: jest.fn(),
   });
 
   it("renders no points", () => {
@@ -53,6 +68,13 @@ describe("<Weeds> />", () => {
     expect(wrapper.state().searchTerm).toEqual("0");
   });
 
+  it("closes section", () => {
+    const wrapper = shallow<Weeds>(<Weeds {...fakeProps()} />);
+    expect(wrapper.state().pending).toEqual(true);
+    wrapper.instance().toggleOpen("pending")();
+    expect(wrapper.instance().state.pending).toEqual(false);
+  });
+
   it("changes sort term", () => {
     const wrapper = shallow<Weeds>(<Weeds {...fakeProps()} />);
     const menu = wrapper.find(SearchField).props().customLeftIcon;
@@ -80,9 +102,70 @@ describe("<Weeds> />", () => {
 });
 
 describe("mapStateToProps()", () => {
-  it("returns props", () => {
+  it("returns value", () => {
     const state = fakeState();
+    const config = fakeWebAppConfig();
+    config.body.show_weeds = true;
+    state.resources = buildResourceIndex([config]);
     const props = mapStateToProps(state);
-    expect(props.dispatch).toEqual(expect.any(Function));
+    expect(props.getConfigValue(BooleanSetting.show_weeds)).toBeTruthy();
+  });
+});
+
+describe("<WeedsSection />", () => {
+  const fakeProps = (): WeedsSectionProps => ({
+    category: "pending",
+    sectionTitle: "title",
+    emptyStateText: "none",
+    open: false,
+    hoveredPoint: undefined,
+    clickOpen: jest.fn(),
+    items: [fakeWeed(), fakeWeed(), fakeWeed()],
+    dispatch: Function,
+  });
+
+  it("approves all", () => {
+    const p = fakeProps();
+    const wrapper = mount(<WeedsSection {...p} />);
+    wrapper.find(".fb-button.green").first().simulate("click");
+    expect(edit).toHaveBeenCalledTimes(3);
+    expect(edit).toHaveBeenCalledWith(p.items[0], { plant_stage: "active" });
+    expect(save).toHaveBeenCalledTimes(3);
+  });
+
+  it("rejects all", () => {
+    const p = fakeProps();
+    const wrapper = mount(<WeedsSection {...p} />);
+    wrapper.find(".fb-button.red").first().simulate("click");
+    expect(destroy).toHaveBeenCalledWith(p.items[0].uuid, true);
+  });
+
+  it("renders", () => {
+    const wrapper = mount(<WeedsSection {...fakeProps()} />);
+    ["(3)", "title", "all"].map(string =>
+      expect(wrapper.text().toLowerCase()).toContain(string));
+    expect(wrapper.text().toLowerCase()).not.toContain("none");
+  });
+
+  it("toggles layer", () => {
+    const p = fakeProps();
+    const state = fakeState();
+    state.resources = buildResourceIndex([fakeWebAppConfig()]);
+    p.dispatch = jest.fn(x => x(jest.fn(), () => state));
+    p.layerValue = true;
+    p.layerSetting = BooleanSetting.show_weeds;
+    p.layerDisabled = false;
+    const wrapper = mount(<WeedsSection {...p} />);
+    wrapper.find(".fb-toggle-button").first().simulate("click");
+    expect(edit).toHaveBeenCalledWith(expect.any(Object),
+      { [BooleanSetting.show_weeds]: false });
+  });
+
+  it("closes section", () => {
+    const p = fakeProps();
+    p.open = true;
+    const wrapper = mount(<WeedsSection {...p} />);
+    wrapper.find(".fa-caret-up").simulate("click");
+    expect(p.clickOpen).toHaveBeenCalled();
   });
 });
