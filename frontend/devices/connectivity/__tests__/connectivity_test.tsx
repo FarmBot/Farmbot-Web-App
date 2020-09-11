c6d870bda44c3eb1c952ba81f28f1433346c1bae
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2020-09-10 17:59:54 -0700

    add more network info to connectivity

--- frontend/devices/connectivity/__tests__/connectivity_test.tsx
+++ frontend/devices/connectivity/__tests__/connectivity_test.tsx
@@ -1,8 +1,8 @@
 jest.mock("../../../api/crud", () => ({ refresh: jest.fn() }));
 
-import * as React from "react";
+import React from "react";
 import { mount } from "enzyme";
-import { Connectivity, ConnectivityProps } from "../connectivity";
+import { Connectivity, ConnectivityProps, formatMac } from "../connectivity";
 import { bot } from "../../../__test_support__/fake_state/bot";
 import { StatusRowProps } from "../connectivity_row";
 import { fill } from "lodash";
@@ -65,5 +65,30 @@ describe("<Connectivity />", () => {
     p.bot.hardware.informational_settings.controller_version = "1.0.0";
     const wrapper = mount(<Connectivity {...p} />);
     expect(wrapper.text().toLowerCase()).toContain("version: v1.0.0");
+  });
+
+  it("displays more network info", () => {
+    const p = fakeProps();
+    p.bot.hardware.informational_settings.private_ip = "1.0.0.1";
+    p.bot.hardware.informational_settings.node_name = "f-12345678";
+    const wrapper = mount(<Connectivity {...p} />);
+    expect(wrapper.text().toLowerCase()).toContain("1.0.0.1");
+    expect(wrapper.text().toLowerCase()).toContain("12:34:56:78");
+  });
+
+  it("doesn't display more network info", () => {
+    const p = fakeProps();
+    p.bot.hardware.informational_settings.private_ip = undefined;
+    p.bot.hardware.informational_settings.node_name = "---";
+    const wrapper = mount(<Connectivity {...p} />);
+    expect(wrapper.text().toLowerCase()).not.toContain("ip");
+    expect(wrapper.text().toLowerCase()).not.toContain("mac");
+  });
+});
+
+describe("formatMac()", () => {
+  it("returns formatted MAC address", () => {
+    expect(formatMac("farmbot-00000000abcdefgh")).toEqual("ab:cd:ef:gh");
+    expect(formatMac("farmbot-0000000012345678.local")).toEqual("12:34:56:78");
   });
 });
