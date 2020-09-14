6a8a25d94392596a73af69fd11ef69c7264a080c
Rick Carlino <rick.carlino@gmail.com>
2020-09-14 15:32:01 -0500

    Attach terminal_session to `window` object

--- frontend/terminal/index.tsx
+++ frontend/terminal/index.tsx
@@ -4,5 +4,7 @@ import { TerminalSession } from "./terminal_session";
 
 const { password, username, url } = getCredentials();
 const session = new TerminalSession(url, username, password, attachTerminal());
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).terminal_session = session;
 session.connect();
+
