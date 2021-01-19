42dffec210737e3ac73e906a65d6b6b1a64829e9
Rick Carlino <rick.carlino@gmail.com>
2021-01-19 13:28:23 -0600

    Remove legacy stubs from serializers

--- app/serializers/firmware_config_serializer.rb
+++ app/serializers/firmware_config_serializer.rb
@@ -1,11 +1,3 @@
 class FirmwareConfigSerializer < ApplicationSerializer
-  # REFERENCES TO `LEGACY_FIELDS` CAN BE REMOVED after FBOS
-  # v12.2 REACHES EOL.
-  LEGACY_FIELDS = [:firmware_debug_log, :firmware_input_log, :firmware_output_log]
-  LEGACY_FIELDS.map do |field|
-    define_method(field) do
-      false
-    end
-  end
-  attributes *FirmwareConfig.column_names.map(&:to_sym) + LEGACY_FIELDS
+  attributes *FirmwareConfig.column_names.map(&:to_sym)
 end
