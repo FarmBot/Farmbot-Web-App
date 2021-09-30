847e6f786ebeec3adaeac4b76123ab399382e640
Rick Carlino <rick.carlino@gmail.com>
2021-09-30 18:11:42 -0500

    Fix II: Render `nil` instead of `[]` when criteria is not met.

--- app/mutations/sequences/show.rb
+++ app/mutations/sequences/show.rb
@@ -70,32 +70,25 @@ module Sequences
       sequence.copyright || sequence_version&.copyright
     end
 
-    # Heuristic for determining available sequence version
+    # Heuristic for determining available sequence version.
+    #
     def available_version_ids
       # First attempt:
       #   See if the this sequence "owns" is a published upstream publication.
       #   If it is not published, don't show anything to the author.
       #   If it IS published, show the versions to the author.
       my_sp = SequencePublication.find_by(author_sequence_id: sequence.id)
-      if my_sp
-        if my_sp.published
-          return my_sp.sequence_versions.pluck(:id)
-        else
-          return []
-        end
+      if my_sp&.published
+        return my_sp.sequence_versions.pluck(:id)
       end
 
       # Second attempt:
       # The consumer is not the author.
       # The sequence has an upstream sequence_version
       upstream_sp = sequence_version&.sequence_publication
-      if upstream_sp
-        if upstream_sp.published
-          return upstream_sp.sequence_versions.pluck(:id)
-        end
+      if upstream_sp&.published
+        return upstream_sp.sequence_versions.pluck(:id)
       end
-
-      return []
     end
 
     def sequence_version_id
