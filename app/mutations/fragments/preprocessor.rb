1e5f99d25e877d5a16e9e0787813d71da5dba21e
Rick Carlino <rick.carlino@gmail.com>
2021-10-05 11:20:35 -0500

    Edge case: Publish an empty sequence.

--- app/mutations/fragments/preprocessor.rb
+++ app/mutations/fragments/preprocessor.rb
@@ -2,7 +2,7 @@ module Fragments
   class Preprocessor
     Corpus = CeleryScriptSettingsBag::Corpus
 
-    def self.run!(kind:, args:, body:, device:)
+    def self.run!(kind:, args:, body: [], device:)
       canonical = { kind: kind, args: args, body: body }
       slicer = CeleryScript::Slicer.new
       tree = CeleryScript::AstNode.new(**canonical.deep_symbolize_keys)
