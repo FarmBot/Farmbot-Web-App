0f8cda7715d9892a4c5e6725a2d4c0b5cf97c789
Rick Carlino <rick.carlino@gmail.com>
2021-12-10 10:10:49 -0600

    Restrict uniqueness of Farmware ENVs

--- spec/factories/farmware_envs.rb
+++ spec/factories/farmware_envs.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :farmware_env do
     device
-    key   { Faker::Games::Pokemon.move }
+    key { Faker::Games::Pokemon.move + SecureRandom.hex(2) }
     value { Faker::Games::Pokemon.move }
   end
 end
