99b58bd0a860d3218aea3af9453d49b849157918
Rick Carlino <rick.carlino@gmail.com>
2022-01-24 16:20:17 -0600

    Revert changes: Do not allow `nothing` as a `default_value`.

--- app/models/celery_script_settings_bag.rb
+++ app/models/celery_script_settings_bag.rb
@@ -48,8 +48,8 @@ module CeleryScriptSettingsBag
   ALLOWED_SPEC_ACTION = %w(emergency_lock emergency_unlock power_off read_status
                            reboot sync take_photo)
   ALLOWED_SPECIAL_VALUE = %w(current_location safe_height soil_height)
-  ANY_VARIABLE = %i(coordinate identifier nothing numeric point
-                    resource resource_placeholder text tool)
+  ANY_VARIABLE = %i(coordinate identifier numeric point resource
+                    resource_placeholder text tool)
   BAD_ALLOWED_PIN_MODES = '"%s" is not a valid pin_mode. ' \
                           "Allowed values: %s"
   BAD_ASSERTION_TYPE = '"%s" is not a valid assertion type. ' \
