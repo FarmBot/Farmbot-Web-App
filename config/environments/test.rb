9fe29deb041d8a1adc211b8abf1a4338cba72d9c
Rick Carlino <rick.carlino@gmail.com>
2022-01-24 16:08:41 -0600

    Dep updates plus changes for the eventual Rails 7 upgrade

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,16 +1,15 @@
 FarmBot::Application.configure do
   config.action_controller.allow_forgery_protection = false
-  config.action_controller.perform_caching          = false
-  config.action_dispatch.show_exceptions            = false
-  config.action_mailer.delivery_method              = :test
-  config.active_job.queue_adapter                   = :inline
-  config.active_support.deprecation                 = :stderr
-  config.assets.debug                               = true
-  config.cache_classes                              = true
-  config.consider_all_requests_local                = true
-  config.eager_load                                 = true
-  config.log_level                                  = :error
-  config.public_file_server.enabled                 = true
-  config.public_file_server.headers                 = \
-    { 'Cache-Control' => 'public, max-age=3600' }
+  config.action_controller.perform_caching = false
+  config.action_dispatch.show_exceptions = false
+  config.action_mailer.delivery_method = :test
+  config.active_job.queue_adapter = :inline
+  config.active_support.deprecation = :stderr
+  # config.assets.debug                               = true
+  config.cache_classes = true
+  config.consider_all_requests_local = true
+  config.eager_load = true
+  config.log_level = :error
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = { "Cache-Control" => "public, max-age=3600" }
 end
