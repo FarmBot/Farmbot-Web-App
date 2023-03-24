94aa15dddb153107936caede834d7800783f790a
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2023-03-23 18:41:04 -0700

    increase name uniqueness to fix blinky ruby test

--- spec/factories/curves.rb
+++ spec/factories/curves.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :curve do
     device
-    name { Faker::Games::Pokemon.name }
+    name { Faker::Games::Pokemon.name + Faker::Games::Pokemon.name }
     type { "water" }
     data { ({ 1 => 1, 100 => 100 }) }
   end
