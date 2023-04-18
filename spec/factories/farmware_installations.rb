ec41ec3cfb4260b5d7a2402cb3b39e2712aa547a
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2023-04-18 14:23:35 -0700

    fix ruby test url content

--- spec/factories/farmware_installations.rb
+++ spec/factories/farmware_installations.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :farmware_installation do
     device
-    url { Faker::Internet.url + "/manifest.json" }
+    url { Faker::Internet.url host: "example.com", path: "/#{SecureRandom.hex(16)}/manifest.json" }
   end
 end
