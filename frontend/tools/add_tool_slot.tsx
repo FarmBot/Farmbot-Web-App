8dd2079c84ea88a3494f812ce3ee614081263832
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2025-02-18 15:06:38 -0800

    fix add tool navigation bug

--- frontend/tools/add_tool_slot.tsx
+++ frontend/tools/add_tool_slot.tsx
@@ -9,12 +9,12 @@ import { SpecialStatus, TaggedToolSlotPointer } from "farmbot";
 import { init, save, edit, destroy } from "../api/crud";
 import { Panel } from "../farm_designer/panel_header";
 import { ToolPulloutDirection } from "farmbot/dist/resources/api_resources";
-import { useNavigate } from "react-router";
 import { SlotEditRows } from "./tool_slot_edit_components";
 import { hasUTM } from "../settings/firmware/firmware_hardware_support";
 import { mapStateToPropsAdd } from "./state_to_props";
 import { AddToolSlotState, AddToolSlotProps } from "./interfaces";
 import { Path } from "../internal_urls";
+import { NavigationContext } from "../routes_helpers";
 
 export class RawAddToolSlot
   extends React.Component<AddToolSlotProps, AddToolSlotState> {
@@ -54,10 +54,13 @@ export class RawAddToolSlot
     (update: Partial<TaggedToolSlotPointer["body"]>) =>
       this.props.dispatch(edit(toolSlot, update));
 
+  static contextType = NavigationContext;
+  context!: React.ContextType<typeof NavigationContext>;
+  navigate = this.context;
+
   save = () => {
     this.state.uuid && this.props.dispatch(save(this.state.uuid));
-    const navigate = useNavigate();
-    navigate(Path.tools());
+    this.navigate(Path.tools());
   };
 
   render() {
