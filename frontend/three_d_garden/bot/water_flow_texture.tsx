fc10a8e71e9e8628ede096750d9e61d013fe65c9
Rory Aronson <rory@roryaronson.com>
2025-02-22 00:46:40 -0800

    use useMemo instead + test

--- frontend/three_d_garden/bot/water_flow_texture.tsx
+++ frontend/three_d_garden/bot/water_flow_texture.tsx
@@ -1,23 +1,17 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 import { useFrame } from "@react-three/fiber";
 import { TextureLoader, RepeatWrapping } from "three";
 import { ASSETS } from "../constants";
 
 export const useWaterFlowTexture = (waterFlow: boolean) => {
-  const [waterTexture, setWaterTexture] = useState(() => {
+  const waterTexture = useMemo(() => {
     const texture = new TextureLoader().load(ASSETS.textures.water);
     texture.wrapS = texture.wrapT = RepeatWrapping;
     return texture;
-  });
-
-  useEffect(() => {
-    const newTexture = new TextureLoader().load(ASSETS.textures.water);
-    newTexture.wrapS = newTexture.wrapT = RepeatWrapping;
-    setWaterTexture(newTexture);
-  }, [waterFlow]);
+  }, []);
 
   useFrame((_, delta) => {
-    if (waterFlow && waterTexture) {
+    if (waterFlow) {
       waterTexture.offset.x -= delta * 0.5;
     }
   });
