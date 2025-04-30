c310bf53adf7632d7db7199389da29a5bae315cd
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2025-04-30 15:10:57 -0700

    add seeder tests for v1.8 coverage

--- spec/controllers/api/devices/devices_controller_seed_spec.rb
+++ spec/controllers/api/devices/devices_controller_seed_spec.rb
@@ -1664,6 +1664,18 @@ describe Api::DevicesController do
       expect(sequences_mow_all_weeds?(device)).to be_kind_of(Sequence)
     end
 
+    it "seeds accounts when sequence versions not available:  Genesis XL 1.8" do
+      start_tests "genesis_xl_1.8", false
+
+      expect(sequences_mow_all_weeds?(device)).to be_kind_of(Sequence)
+    end
+
+    it "seeds accounts when sequence versions not available:  Genesis 1.8" do
+      start_tests "genesis_1.8", false
+
+      expect(sequences_mow_all_weeds?(device)).to be_kind_of(Sequence)
+    end
+
     it "does not seed accounts" do
       start_tests "none"
 
