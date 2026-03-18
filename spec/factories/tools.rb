58e83a5fcec08372295a9c162f74d39e71d8a018
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2026-03-18 15:30:51 -0700

    improve blinky ruby tests

--- spec/factories/tools.rb
+++ spec/factories/tools.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tool do
     device
-    name { Faker::Games::Pokemon.name + Faker::Games::Pokemon.name  }
+    name { Faker::Games::Pokemon.name + SecureRandom.hex(8)  }
   end
 end
